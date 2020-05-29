99f1db074f1e16ef77fc82dbdbe84eb59b0d4d9e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-05-29 09:51:56 +0900

    Add consume_{id,text}

--- src/main.c
+++ src/main.c
@@ -3,6 +3,16 @@
 
 #include "parser.tab.h"
 
+int consume_id(const char **text) {
+  int token = *(int *)*text;
+  *text += sizeof(int);
+  return token;
+}
+const char *consume_text(const char **text) {
+  const char *head = *text;
+  *text += strlen(*text) + 1;
+  return head;
+}
 void print_seq(yyscan_t scanner) {
   Vector *seq = yyget_extra(scanner);
   const char *text = vector_at(seq, 0);
