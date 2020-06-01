9be8332fc1cf391d93c17a1971434e9c9d57a772
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-01 10:33:53 +0900

    Add print_ast

--- src/main.c
+++ src/main.c
@@ -11,12 +11,29 @@ const char *consume_text(const char *cur, const char **text) {
   *text = cur;
   return cur + strlen(cur) + 1;
 }
+const char *print_ast(const char *cur) {
+  int arity = 0, tag = 0, i = 0;
+  const char *text = nil;
+  cur = consume_int(cur, &arity);
+  cur = consume_int(cur, &tag);
+  printf("[%d:", tag);
+  if (arity == 0) {
+    cur = consume_text(cur, &text);
+    printf("%s", text);
+  } else {
+    for (i = 0; i < arity; ++i) {
+      cur = print_ast(cur);
+    }
+  }
+  printf("]");
+  return cur;
 }
 void print_seq(yyscan_t scanner) {
   Vector *seq = yyget_extra(scanner);
   const char *text = vector_at(seq, 0);
   const char *const end = vector_at(seq, vector_length(seq));
   while (text < end) {
+    text = print_ast(text);
   }
   printf("length: %d\n", vector_length(seq));
   vector_delete(&seq);
