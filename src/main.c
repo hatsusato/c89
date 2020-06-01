4e50454f64436e4517d5ac7a0b302cdb2a1645b2
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-01 10:33:28 +0900

    Remove print_text

--- src/main.c
+++ src/main.c
@@ -13,23 +13,12 @@ const char *consume_text(const char **text) {
   *text += strlen(*text) + 1;
   return head;
 }
-void print_text(const char **text) {
-  int token = consume_id(text);
-  const char *const head = consume_text(text);
-  printf("[%d:%s]", token, head);
-  switch (token) {
-  case PUNCTUATOR_SEMICOLON:
-  case PUNCTUATOR_LEFT_BRACE:
-  case PUNCTUATOR_RIGHT_BRACE:
-    printf("\n");
-  }
 }
 void print_seq(yyscan_t scanner) {
   Vector *seq = yyget_extra(scanner);
   const char *text = vector_at(seq, 0);
   const char *const end = vector_at(seq, vector_length(seq));
   while (text < end) {
-    print_text(&text);
   }
   printf("length: %d\n", vector_length(seq));
   vector_delete(&seq);
