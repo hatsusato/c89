8dd4936d9fb2046456e342ed304441691ae224e4
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-01 12:10:17 +0900

    Add ast_{s,g}et_top

--- src/parser.c
+++ src/parser.c
@@ -1,8 +1,29 @@
 #include "parser.h"
 
+#include <assert.h>
 #include <stdio.h>
 
 void yyerror(const char* msg, yyscan_t scanner) {
   (void)scanner;
   fprintf(stderr, "yyerror: [%s]\n", msg);
+}
+void ast_set_top(Vector* top, Vector* ast) {
+  if (!top) {
+    fprintf(stderr, "empty top\n");
+  } else if (!ast) {
+    fprintf(stderr, "empty ast\n");
+  } else {
+    int leng = vector_length(ast);
+    const char* text = vector_begin(ast);
+    vector_append(top, &leng, sizeof(leng));
+    vector_append(top, text, leng);
+  }
+}
+const char* ast_get_top(const char* top, int* length, const char** text) {
+  assert(top && length && text);
+  *length = *(int*)top;
+  top += sizeof(int);
+  *text = top;
+  top += *length;
+  return top;
 }
--- src/parser.h
+++ src/parser.h
@@ -3,6 +3,8 @@
 
 #include "parser.tab.h"
 
-void yyerror(const char *, yyscan_t);
+void yyerror(const char*, yyscan_t);
+void ast_set_top(Vector* top, Vector* ast);
+const char* ast_get_top(const char* top, int* length, const char** text);
 
 #endif /* INCLUDE_GUARD_CB25BAC5_5782_4FB4_A66E_3FFFB7B099AC */
