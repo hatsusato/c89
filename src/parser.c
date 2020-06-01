ce330f29da0b8d572697b75e02992c3ab6691735
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-01 11:43:39 +0900

    Update yyerror

--- src/parser.c
+++ src/parser.c
@@ -1,6 +1,8 @@
 #include "parser.h"
 
+#include <stdio.h>
+
 void yyerror(const char* msg, yyscan_t scanner) {
   (void)scanner;
-  fprintf(stderr, "%s\n", msg);
+  fprintf(stderr, "yyerror: [%s]\n", msg);
 }
