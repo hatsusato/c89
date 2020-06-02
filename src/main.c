9282f9bb9a5803ade0403f359c3cd7b230331664
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-02 09:33:12 +0900

    Remove print_{ast,text}

--- src/main.c
+++ src/main.c
@@ -12,30 +12,6 @@ const char *consume_text(const char *cur, const char **text) {
   *text = cur;
   return cur + strlen(cur) + 1;
 }
-void print_text(const char *text, int length) {
-  int i = 0;
-  for (i = 0; i < length; ++i) {
-    printf("%c", text[i]);
-  }
-}
-const char *print_ast(const char *cur) {
-  int tag = 0, arity = 0, length = 0;
-  const char *text = nil;
-  cur = ast_get_int(cur, &tag);
-  cur = ast_get_int(cur, &arity);
-  printf("[%d:", tag);
-  if (arity < 0) {
-    cur = ast_get_text(cur, &text, &length);
-    print_text(text, length);
-  } else {
-    int i = 0;
-    for (i = 0; i < arity; ++i) {
-      cur = print_ast(cur);
-    }
-  }
-  printf("]");
-  return cur;
-}
 void print_seq(yyscan_t scanner) {
   Vector *seq = yyget_extra(scanner);
   const char *cur = vector_begin(seq);
