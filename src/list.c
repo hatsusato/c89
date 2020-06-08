57910caf29e809361df01957f4189645f821d47d
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-08 11:14:14 +0900

    Add node_new

--- src/list.c
+++ src/list.c
@@ -1,10 +1,22 @@
 #include "list.h"
 
+#include <stdlib.h>
+
 struct struct_Node;
 typedef struct struct_Node Node;
 struct struct_Node {
+  int tag;
+  void *data;
   Node *next;
 };
 struct struct_List {
   Node node;
 };
+
+Node *node_new(int tag, void *data, Node *next) {
+  Node *node = malloc(sizeof(Node));
+  node->tag = tag;
+  node->data = data;
+  node->next = next;
+  return node;
+}
