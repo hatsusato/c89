082aa145cb4a46c101c9504677bbd5bb986dee61
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-11 10:58:09 +0900

    Add print_{tag,token}

--- src/print.c
+++ src/print.c
@@ -1,8 +1,10 @@
 #include "print.h"
 
+#include <ctype.h>
 #include <stdio.h>
 
 #include "ast.h"
+#include "vector.h"
 
 static void print_begin(void) {
   printf("[");
@@ -12,6 +14,22 @@ static void print_end(void) {
 }
 static void print_newline(void) {
   printf("\n");
+}
+static void print_tag(int tag) {
+  printf("%s:", ast_show(tag));
+}
+static void print_token(Vector *data) {
+  const char *text = vector_begin(data);
+  int leng = vector_length(data);
+  int i = 0;
+  for (i = 0; i < leng; ++i) {
+    char c = text[i];
+    if (isprint(c)) {
+      printf("%c", c);
+    } else {
+      printf("\\x%02x", c);
+    }
+  }
 }
 
 void print_ast(List *list) {
