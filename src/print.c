0732d619d441f36d04a7097d517ecbe5be9a9ac4
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-11 10:57:00 +0900

    Add print_{begin,end,newline}

--- src/print.c
+++ src/print.c
@@ -4,6 +4,16 @@
 
 #include "ast.h"
 
+static void print_begin(void) {
+  printf("[");
+}
+static void print_end(void) {
+  printf("]");
+}
+static void print_newline(void) {
+  printf("\n");
+}
+
 void print_ast(List *list) {
   while (list) {
     int tag = list_tag(list);
