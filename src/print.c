9d2e54e6e36f15fefb9ca64eaa241ec9257da014
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-11 18:04:08 +0900

    Trivial refactoring

--- src/print.c
+++ src/print.c
@@ -64,7 +64,7 @@ static List *print_list(List *list, int indent) {
       list = list_next(list);
       break;
     }
-    list = print_line(list, indent);
+    list = print_line(list, indent + 1);
   }
   print_end();
   return list;
@@ -102,8 +102,8 @@ void print_all(List *list) {
   }
 }
 List *print_ast(List *list, int indent) {
-  int tag = list_tag(list);
-  switch (tag) {
+  switch (list_tag(list)) {
+    /* Lexical elements */
   case AST_IDENTIFIER:
   case AST_FLOATING_CONSTANT:
   case AST_INTEGER_CONSTANT:
@@ -111,12 +111,14 @@ List *print_ast(List *list, int indent) {
   case AST_CHARACTER_CONSTANT:
   case AST_STRING_LITERAL:
     return print_token(list, indent);
+    /* List */
+  case AST_ARGUMENT_EXPRESSION_LIST:
+    return print_list(list, indent);
+    /* Expressions */
   case AST_PRIMARY_EXPRESSION:
     return print_repeat(list, indent, 1, 1);
   case AST_POSTFIX_EXPRESSION:
     return print_postfix(list, indent);
-  case AST_ARGUMENT_EXPRESSION_LIST:
-    return print_list(list, indent);
   case AST_UNARY_EXPRESSION:
     return print_repeat(list, indent, 2, 1);
   case AST_CAST_EXPRESSION:
@@ -131,16 +133,17 @@ List *print_ast(List *list, int indent) {
   case AST_INCLUSIVE_OR_EXPRESSION:
   case AST_LOGICAL_AND_EXPRESSION:
   case AST_LOGICAL_OR_EXPRESSION:
-  case AST_ASSIGNMENT_EXPRESSION:
     return print_repeat(list, indent, 2, 2);
   case AST_CONDITIONAL_EXPRESSION:
     return print_repeat(list, indent, 1, 3);
+  case AST_ASSIGNMENT_EXPRESSION:
+    return print_repeat(list, indent, 2, 2);
   case AST_EXPRESSION:
     return print_expression(list, indent);
   case AST_CONSTANT_EXPRESSION:
     return print_repeat(list, indent, 1, 1);
   default:
-    printf("[%s]", ast_show(tag));
+    printf("[%s]", ast_show(list_tag(list)));
     return list_next(list);
   }
 }
