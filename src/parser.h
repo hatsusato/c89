c66eea22dd9e714e7aa5decb2cbe53f500dacb5f
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-16 12:34:19 +0900

    Remove AST_APPEND[0-4]

--- src/parser.h
+++ src/parser.h
@@ -7,36 +7,6 @@
   do {                                           \
     x0.list = ast_new_token(AST_##tag, scanner); \
   } while (0)
-#define AST_APPEND0(tag, x0)          \
-  do {                                \
-    x0.list = ast_new_tag(AST_##tag); \
-  } while (0)
-#define AST_APPEND1(tag, x0, x1)      \
-  do {                                \
-    x0.list = ast_new_tag(AST_##tag); \
-    list_append(x0.list, x1.list);    \
-  } while (0)
-#define AST_APPEND2(tag, x0, x1, x2)  \
-  do {                                \
-    x0.list = ast_new_tag(AST_##tag); \
-    list_append(x0.list, x1.list);    \
-    list_append(x0.list, x2.list);    \
-  } while (0)
-#define AST_APPEND3(tag, x0, x1, x2, x3) \
-  do {                                   \
-    x0.list = ast_new_tag(AST_##tag);    \
-    list_append(x0.list, x1.list);       \
-    list_append(x0.list, x2.list);       \
-    list_append(x0.list, x3.list);       \
-  } while (0)
-#define AST_APPEND4(tag, x0, x1, x2, x3, x4) \
-  do {                                       \
-    x0.list = ast_new_tag(AST_##tag);        \
-    list_append(x0.list, x1.list);           \
-    list_append(x0.list, x2.list);           \
-    list_append(x0.list, x3.list);           \
-    list_append(x0.list, x4.list);           \
-  } while (0)
 #define AST_TOKEN(tag, x0, token)                   \
   do {                                              \
     x0.list = ast_new_tag(AST_##tag);               \
