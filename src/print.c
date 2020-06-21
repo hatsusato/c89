446ddf2990672bc891fb711ed68c1b1ec57259ef
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-21 13:44:48 +0900

    Refactor case of arity0

--- src/print.c
+++ src/print.c
@@ -6,20 +6,14 @@
 #include "ast.h"
 #include "vector.h"
 
-static void print_begin(int tag, int indent) {
-  if (AST_ARITY0 == tag) {
-    return;
-  }
+static void print_begin(int indent) {
   printf("\n");
   for (; 0 < indent; --indent) {
     printf("  ");
   }
   printf("(");
 }
-static void print_end(int tag) {
-  if (AST_ARITY0 == tag) {
-    return;
-  }
+static void print_end(void) {
   printf(")");
 }
 static void print_data(Vector *data) {
@@ -52,7 +46,10 @@ static List *print_list(List *list, int indent) {
 List *print_ast(List *list, int indent) {
   int tag = list_tag(list);
   list = list_next(list);
-  print_begin(tag, indent);
+  if (AST_ARITY0 == tag) {
+    return print_tag(list);
+  }
+  print_begin(indent);
   list = print_tag(list);
   switch (tag) {
   case AST_TOKEN:
@@ -61,8 +58,6 @@ List *print_ast(List *list, int indent) {
   case AST_LIST:
     list = print_list(list, indent);
     break;
-  case AST_ARITY0:
-    goto case_arity0;
   case AST_ARITY1:
     goto case_arity1;
   case AST_ARITY2:
@@ -79,11 +74,10 @@ List *print_ast(List *list, int indent) {
     list = print_ast(list, indent + 1);
   case_arity1:
     list = print_ast(list, indent + 1);
-  case_arity0:
     break;
   default:
     assert(0);
   }
-  print_end(tag);
+  print_end();
   return list;
 }
