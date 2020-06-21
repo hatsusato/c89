aef6dc3d0558c9220f7cf43429df7f68cdda9ea3
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-21 11:25:42 +0900

    Remove AST_* macros

--- src/parser.h
+++ src/parser.h
@@ -3,47 +3,6 @@
 
 #include "parser.tab.h"
 
-#define AST_INIT(dst) \
-  do {                \
-    dst = ast_init(); \
-  } while (0)
-#define AST_TAG(dst, tag)         \
-  do {                            \
-    dst = ast_new_tag(AST_##tag); \
-  } while (0)
-#define AST_PUSH_TAG(dst, tag)          \
-  do {                                  \
-    dst = ast_push_tag(dst, AST_##tag); \
-  } while (0)
-#define AST_PUSH(dst, src)    \
-  do {                        \
-    dst = ast_push(dst, src); \
-  } while (0)
-#define AST_EMPTY(dst)      \
-  do {                      \
-    AST_INIT(dst);          \
-    AST_PUSH_TAG(dst, NIL); \
-  } while (0)
-#define AST_LIST_CONS(dst, car, cdr) \
-  do {                               \
-    AST_INIT(dst);                   \
-    AST_PUSH(dst, car);              \
-    AST_PUSH(dst, cdr);              \
-  } while (0)
-#define AST_LIST_EMPTY(dst, tag) \
-  do {                           \
-    AST_TAG(dst, LIST);          \
-    AST_PUSH_TAG(dst, tag);      \
-    AST_PUSH_TAG(dst, NIL);      \
-  } while (0)
-#define AST_LIST_EXIST(dst, tag, src) \
-  do {                                \
-    AST_TAG(dst, LIST);               \
-    AST_PUSH_TAG(dst, tag);           \
-    AST_PUSH(dst, src);               \
-    AST_PUSH_TAG(dst, NIL);           \
-  } while (0)
-
 void yyerror(const char *, yyscan_t);
 YYSTYPE ast_init(void);
 YYSTYPE ast_new_token(int, yyscan_t);
