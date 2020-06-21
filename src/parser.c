67836262d48b4e5d68f98569aeb3592ec99e5b70
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-21 11:40:20 +0900

    Remove ast_new_tag

--- src/parser.c
+++ src/parser.c
@@ -17,10 +17,10 @@ static YYSTYPE ast_push(YYSTYPE x0, YYSTYPE x1) {
   return x0;
 }
 static YYSTYPE ast_push_tag(YYSTYPE x0, int tag) {
-  return ast_push(x0, ast_new_tag(tag));
+  return ast_push(x0, ast_new(tag, nil));
 }
 static YYSTYPE ast_arity(int arity, int tag) {
-  YYSTYPE x0 = ast_new_tag(arity);
+  YYSTYPE x0 = ast_new(arity, nil);
   x0 = ast_push_tag(x0, tag);
   return x0;
 }
@@ -39,9 +39,6 @@ YYSTYPE ast_new_token(int tag, yyscan_t scanner) {
   Vector *vec = vector_new(1);
   vector_append(vec, text, leng);
   return ast_new(tag, vec);
-}
-YYSTYPE ast_new_tag(int tag) {
-  return ast_new(tag, nil);
 }
 YYSTYPE ast_append0(int tag) {
   YYSTYPE x0 = ast_arity(AST_ARITY0, tag);
@@ -77,7 +74,7 @@ YYSTYPE ast_binary(YYSTYPE x1, YYSTYPE x2, YYSTYPE x3) {
   return ast_append3(AST_BINARY, x2, x1, x3);
 }
 YYSTYPE ast_list_empty(int tag) {
-  YYSTYPE ret = ast_push_tag(ast_new_tag(AST_LIST), tag);
+  YYSTYPE ret = ast_push_tag(ast_new(AST_LIST, nil), tag);
   List *prev = ret.last;
   ret = ast_push_tag(ret, AST_NIL);
   list_insert(ret.last, prev);
