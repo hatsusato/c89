f16c2a0bd96dc3bda0f8ac12c322d098980b6a1d
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-23 10:44:06 +0900

    Add string_{new,delete}

--- src/string.c
+++ src/string.c
@@ -1,8 +1,26 @@
 #include "string.h"
 
+#include <stdlib.h>
+#include <string.h>
+
 #include "utility.h"
 
 struct struct_String {
   char* data;
   Size size, capacity;
 };
+
+String* string_new(const char* init) {
+  String* s = malloc(sizeof(String));
+  Size cap = strlen(init) + 1;
+  s->data = malloc(cap);
+  s->size = cap - 1;
+  s->capacity = cap;
+  memcpy(s->data, init, s->capacity);
+  return s;
+}
+void string_delete(String* s) {
+  assert(s);
+  free(s->data);
+  free(s);
+}
--- src/string.h
+++ src/string.h
@@ -4,4 +4,7 @@
 struct struct_String;
 typedef struct struct_String String;
 
+String* string_new(const char*);
+void string_delete(String*);
+
 #endif /* INCLUDE_GUARD_08E41EF5_65A1_4E4C_9B5A_FE5896BDA20E */
