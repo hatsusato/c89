5d197efa7aa2fdfe09eabc6d85aa6bd6d7804ea5
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-26 22:28:55 +0900

    Add typedef_{insert,next}

--- src/typedef.c
+++ src/typedef.c
@@ -1,1 +1,16 @@
 #include "typedef.h"
+
+#include "list.h"
+#include "scanner.h"
+#include "string.h"
+#include "utility.h"
+
+static List *typedef_insert(yyscan_t scanner, List *ast) {
+  assert(AST_DATA == list_tag(ast));
+  scanner_insert_symbol(scanner, string_begin(list_data(ast)));
+  return list_next(ast);
+}
+static List *typedef_next(List *ast, int tag) {
+  assert(tag == list_tag(ast));
+  return list_next(ast);
+}
