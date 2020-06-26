a2e9b3e7aa800d157b4bc64aceadc0a42b0bd548
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-26 10:45:04 +0900

    Add set_find

--- src/set.c
+++ src/set.c
@@ -5,13 +5,22 @@
 
 #include "vector.h"
 
+#define SET_ALIGNMENT (sizeof(void *))
+
 struct struct_Set {
   Vector *data;
 };
 
+static void *set_find(const Set *set, const void *key,
+                      int (*cmp)(const void *, const void *)) {
+  const void *const *data = vector_begin(set->data);
+  size_t count = vector_length(set->data);
+  return bsearch(&key, data, count, SET_ALIGNMENT, cmp);
+}
+
 Set *set_new(void) {
   Set *set = malloc(sizeof(Set));
-  set->data = vector_new(sizeof(void *));
+  set->data = vector_new(SET_ALIGNMENT);
   return set;
 }
 void set_delete(Set *set) {
