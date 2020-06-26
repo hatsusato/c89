c966523f085064e0b55c633a472bd1957ae12c62
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-26 16:45:12 +0900

    Add extra_{new,delete}

--- src/scanner.c
+++ src/scanner.c
@@ -6,6 +6,16 @@ typedef struct {
   List *ast;
 } Extra;
 
+static Extra *extra_new(void) {
+  Extra *extra = malloc(sizeof(Extra));
+  extra->ast = nil;
+  return extra;
+}
+static void extra_delete(Extra *extra) {
+  assert(extra);
+  free(extra);
+}
+
 void yyerror(const char *msg, yyscan_t scanner) {
   (void)scanner;
   fprintf(stderr, "yyerror: [%s]\n", msg);
