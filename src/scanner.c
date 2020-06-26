a2cc05cea7b184e5a72b8a1bfb35652d50e4c2b8
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-26 16:47:55 +0900

    Modify extra-type

--- src/scanner.c
+++ src/scanner.c
@@ -22,18 +22,22 @@ void yyerror(const char *msg, yyscan_t scanner) {
 }
 yyscan_t scanner_new(void) {
   yyscan_t scanner = nil;
-  YYSTYPE extra = parser_init();
-  int ret = yylex_init_extra(extra, &scanner);
+  int ret = yylex_init_extra(extra_new(), &scanner);
   assert(ret == 0);
   return scanner;
 }
 void scanner_delete(yyscan_t scanner) {
+  extra_delete(yyget_extra(scanner));
   yylex_destroy(scanner);
 }
 List *scanner_get_ast(yyscan_t scanner) {
-  YY_EXTRA_TYPE extra = yyget_extra(scanner);
-  return extra.list;
+  Extra *extra = yyget_extra(scanner);
+  assert(extra);
+  return extra->ast;
 }
 void scanner_set_ast(yyscan_t scanner, YYSTYPE ast) {
-  yyset_extra(ast, scanner);
+  Extra *extra = yyget_extra(scanner);
+  assert(extra);
+  extra->ast = ast.list;
+  yyset_extra(extra, scanner);
 }
