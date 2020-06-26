3db5b5dfca926c65bf432389810c8b447ebe6094
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-26 22:40:03 +0900

    Add typedef_parse_{direct_,init_,}declarator

--- src/typedef.c
+++ src/typedef.c
@@ -1,10 +1,12 @@
 #include "typedef.h"
 
 #include "list.h"
+#include "parser.h"
 #include "scanner.h"
 #include "string.h"
 #include "utility.h"
 
+static List *typedef_parse_declarator(yyscan_t, List *);
 static List *typedef_insert(yyscan_t scanner, List *ast) {
   assert(AST_DATA == list_tag(ast));
   scanner_insert_symbol(scanner, string_begin(list_data(ast)));
@@ -13,4 +15,55 @@ static List *typedef_insert(yyscan_t scanner, List *ast) {
 static List *typedef_next(List *ast, int tag) {
   assert(tag == list_tag(ast));
   return list_next(ast);
+}
+static List *typedef_parse_direct_declarator(yyscan_t scanner, List *ast) {
+  int tag = list_tag(ast);
+  ast = list_next(ast);
+  switch (tag) {
+  case AST_TOKEN:
+    ast = typedef_next(ast, AST_IDENTIFIER);
+    return typedef_insert(scanner, ast);
+  case AST_ARITY1:
+    ast = typedef_next(ast, AST_DIRECT_DECLARATOR);
+    return typedef_parse_declarator(scanner, ast);
+  case AST_ARITY2:
+    ast = typedef_next(ast, AST_DIRECT_DECLARATOR);
+    ast = typedef_parse_direct_declarator(scanner, ast);
+    return parser_consume(ast);
+  default:
+    assert(0);
+    return ast;
+  }
+}
+static List *typedef_parse_declarator(yyscan_t scanner, List *ast) {
+  int tag = list_tag(ast);
+  ast = list_next(ast);
+  switch (tag) {
+  case AST_ARITY1:
+    ast = typedef_next(ast, AST_DECLARATOR);
+    return typedef_parse_direct_declarator(scanner, ast);
+  case AST_ARITY2:
+    ast = typedef_next(ast, AST_DECLARATOR);
+    ast = parser_consume(ast);
+    return typedef_parse_direct_declarator(scanner, ast);
+  default:
+    assert(0);
+    return ast;
+  }
+}
+static List *typedef_parse_init_declarator(yyscan_t scanner, List *ast) {
+  int tag = list_tag(ast);
+  ast = list_next(ast);
+  switch (tag) {
+  case AST_ARITY1:
+    ast = typedef_next(ast, AST_INIT_DECLARATOR);
+    return typedef_parse_declarator(scanner, ast);
+  case AST_ARITY2:
+    ast = typedef_next(ast, AST_INIT_DECLARATOR);
+    ast = typedef_parse_declarator(scanner, ast);
+    return parser_consume(ast);
+  default:
+    assert(0);
+    return ast;
+  }
 }
