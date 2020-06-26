aeb1731e7113d59f84bfb477f929dee7510efefd
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-26 10:19:02 +0900

    Add set_{new,delete}

--- src/set.c
+++ src/set.c
@@ -1,7 +1,21 @@
 #include "set.h"
 
+#include <assert.h>
+#include <stdlib.h>
+
 #include "vector.h"
 
 struct struct_Set {
   Vector *data;
 };
+
+Set *set_new(void) {
+  Set *set = malloc(sizeof(Set));
+  set->data = vector_new(sizeof(void *));
+  return set;
+}
+void set_delete(Set *set) {
+  assert(set);
+  vector_delete(set->data);
+  free(set);
+}
--- src/set.h
+++ src/set.h
@@ -4,4 +4,7 @@
 struct struct_Set;
 typedef struct struct_Set Set;
 
+Set *set_new(void);
+void set_delete(Set *set);
+
 #endif /* INCLUDE_GUARD_DE6D0AC9_F1A3_4A4E_90C8_FBAA690CD01A */
