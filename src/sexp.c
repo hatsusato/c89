de153c036d6a87050715c1d6e45f983ee19146c9
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-28 08:08:29 +0900

    Add sexp_kind

--- src/sexp.c
+++ src/sexp.c
@@ -4,7 +4,7 @@
 
 #include "string.h"
 
-typedef enum { SEXP_PAIR, SEXP_SYMBOL, SEXP_STRING } SexpKind;
+typedef enum { SEXP_NIL, SEXP_PAIR, SEXP_SYMBOL, SEXP_STRING } SexpKind;
 
 struct struct_Sexp {
   SexpKind kind;
@@ -16,6 +16,10 @@ struct struct_Sexp {
     const String *string;
   } data;
 };
+
+static SexpKind sexp_kind(Sexp *sexp) {
+  return sexp ? sexp->kind : SEXP_NIL;
+}
 
 Sexp *sexp_nil(void) {
   return nil;
@@ -40,7 +44,9 @@ Sexp *sexp_string(const char *text, int leng) {
   return sexp;
 }
 void sexp_delete(Sexp *sexp) {
-  switch (sexp->kind) {
+  switch (sexp_kind(sexp)) {
+  case SEXP_NIL:
+    return;
   case SEXP_PAIR:
     sexp_delete(sexp->data.pair.car);
     sexp_delete(sexp->data.pair.cdr);
@@ -54,13 +60,13 @@ void sexp_delete(Sexp *sexp) {
   free((void *)sexp);
 }
 Bool sexp_is_pair(Sexp *sexp) {
-  return SEXP_PAIR == sexp->kind;
+  return SEXP_PAIR == sexp_kind(sexp);
 }
 Bool sexp_is_symbol(Sexp *sexp) {
-  return SEXP_SYMBOL == sexp->kind;
+  return SEXP_SYMBOL == sexp_kind(sexp);
 }
 Bool sexp_is_string(Sexp *sexp) {
-  return SEXP_STRING == sexp->kind;
+  return SEXP_STRING == sexp_kind(sexp);
 }
 Sexp *sexp_car(Sexp *sexp) {
   assert(sexp_is_pair(sexp));
