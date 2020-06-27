5f21ee7a783d9b18538244e7e530aa34b15d6a20
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-28 07:36:14 +0900

    Add sexp_cons

--- src/sexp.c
+++ src/sexp.c
@@ -1,5 +1,7 @@
 #include "sexp.h"
 
+#include <stdlib.h>
+
 typedef enum { SEXP_PAIR } SexpKind;
 
 struct struct_Sexp {
@@ -10,3 +12,11 @@ struct struct_Sexp {
     } pair;
   } data;
 };
+
+Sexp *sexp_cons(Sexp *car, Sexp *cdr) {
+  Sexp *sexp = malloc(sizeof(Sexp));
+  sexp->kind = SEXP_PAIR;
+  sexp->data.pair.car = car;
+  sexp->data.pair.cdr = cdr;
+  return sexp;
+}
--- src/sexp.h
+++ src/sexp.h
@@ -4,4 +4,6 @@
 struct struct_Sexp;
 typedef struct struct_Sexp Sexp;
 
+Sexp* sexp_cons(Sexp*, Sexp*);
+
 #endif /* INCLUDE_GUARD_3397EB56_FB26_40B7_AD9E_F7B3DA519335 */
