267a75f8f960ba18491d42f606e1363148b949c0
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-27 12:21:14 +0900

    Add compile.sh

---
+++ compile.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+print() {
+  local opts=(-Wall -Wextra -ansi -pedantic -x c -E -P -)
+  local target="$1"
+  shift
+  cat - "$target" <<EOF | gcc "$@" "${opts[@]}"
+#define __attribute__(x)
+#define __asm__(x)
+#define __extension__
+#define __inline
+#define __restrict
+EOF
+}
+
+eflag=
+if [[ "$1" == -E ]]; then
+  eflag=on
+  shift
+fi
+for arg in "$@"; do
+  if test "$eflag"; then
+    print "$arg" -Isrc
+  else
+    print "$arg" -Isrc -fsyntax-only || exit $?
+    print "$arg" -Isrc | ./main.out
+  fi
+done
