9c67d9fab604c2b744398613d4f8fec0edf37e45
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-06-28 16:20:53 +0900

    Add MutableSexp

--- src/sexp.c
+++ src/sexp.c
@@ -17,6 +17,7 @@ struct struct_Sexp {
     const String *string;
   } data;
 };
+typedef struct struct_Sexp MutableSexp;
 
 static SexpKind sexp_kind(Sexp *sexp) {
   return sexp ? sexp->kind : SEXP_NIL;
@@ -26,20 +27,20 @@ Sexp *sexp_nil(void) {
   return nil;
 }
 Sexp *sexp_cons(Sexp *car, Sexp *cdr) {
-  struct struct_Sexp *sexp = malloc(sizeof(Sexp));
+  MutableSexp *sexp = malloc(sizeof(Sexp));
   sexp->kind = SEXP_PAIR;
   sexp->data.pair.car = car;
   sexp->data.pair.cdr = cdr;
   return sexp;
 }
 Sexp *sexp_symbol(const char *symbol) {
-  struct struct_Sexp *sexp = malloc(sizeof(Sexp));
+  MutableSexp *sexp = malloc(sizeof(Sexp));
   sexp->kind = SEXP_SYMBOL;
   sexp->data.symbol = symbol;
   return sexp;
 }
 Sexp *sexp_string(const char *text, int leng) {
-  struct struct_Sexp *sexp = malloc(sizeof(Sexp));
+  MutableSexp *sexp = malloc(sizeof(Sexp));
   sexp->kind = SEXP_STRING;
   sexp->data.string = string_new_s(text, leng);
   return sexp;
