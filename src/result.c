2d1dfba0e9b9c6eefd852e99af9270e091c6910f
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-02 10:23:25 +0900

    Remove Result.ast

--- src/result.c
+++ src/result.c
@@ -3,33 +3,22 @@
 #include <stdlib.h>
 
 #include "scanner.h"
-#include "string.h"
 #include "utility.h"
 
 struct struct_Result {
   Sexp *sexp;
-  List *ast;
   Set *symbols;
 };
-
-static void result_ast_data_free(List *ast) {
-  String *data = list_data(ast);
-  if (data) {
-    string_delete(data);
-  }
-}
 
 Result *result_new(void) {
   Result *result = malloc(sizeof(Result));
   result->sexp = sexp_nil();
-  result->ast = nil;
   result->symbols = set_new();
   return result;
 }
 void result_delete(Result *result) {
   assert(result);
   sexp_delete(result->sexp);
-  list_delete(result->ast, result_ast_data_free);
   set_delete(result->symbols);
   free(result);
 }
@@ -38,14 +27,6 @@ int result_parse(Result *result) {
   int ret = scanner_parse(scanner);
   scanner_delete(scanner);
   return ret;
-}
-List *result_get_ast(Result *result) {
-  assert(result);
-  return result->ast;
-}
-void result_set_ast(Result *result, List *ast) {
-  assert(result);
-  result->ast = ast;
 }
 Set *result_get_symbols(Result *result) {
   assert(result);
--- src/result.h
+++ src/result.h
@@ -1,7 +1,6 @@
 #ifndef INCLUDE_GUARD_9C8CB4C0_BC4E_4938_931F_A41B8FE242C3
 #define INCLUDE_GUARD_9C8CB4C0_BC4E_4938_931F_A41B8FE242C3
 
-#include "list.h"
 #include "set.h"
 #include "sexp.h"
 
@@ -11,8 +10,6 @@ typedef struct struct_Result Result;
 Result *result_new(void);
 void result_delete(Result *);
 int result_parse(Result *);
-List *result_get_ast(Result *);
-void result_set_ast(Result *, List *);
 Set *result_get_symbols(Result *);
 Sexp *result_set_sexp(Result *, Sexp *);
 
