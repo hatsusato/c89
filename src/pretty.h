8b0eb059069762505b9a167626623628e0634d03
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-04 19:24:35 +0900

    Remove List in pretty

--- src/pretty.h
+++ src/pretty.h
@@ -1,10 +1,8 @@
 #ifndef INCLUDE_GUARD_3E3EE0A2_A0DB_4AA6_9626_7E847D8B9044
 #define INCLUDE_GUARD_3E3EE0A2_A0DB_4AA6_9626_7E847D8B9044
 
-#include "list.h"
 #include "sexp.h"
 
-void pretty_print(List *);
 Sexp *pretty_sexp(Sexp *);
 void pretty_print_sexp(Sexp *);
 
