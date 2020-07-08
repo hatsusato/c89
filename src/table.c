b0dd4d00441d11640cdb5bee719be70851ec76bd
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-09 08:01:46 +0900

    Add table_{new,delete}

--- src/table.c
+++ src/table.c
@@ -1,7 +1,19 @@
 #include "table.h"
 
+#include <stdlib.h>
+
 #include "vector.h"
 
 struct struct_SymbolTable {
   Vector *table;
 };
+
+SymbolTable *table_new(void) {
+  SymbolTable *table = malloc(sizeof(SymbolTable));
+  table->table = vector_new();
+  return table;
+}
+void table_delete(SymbolTable *table) {
+  vector_delete(table->table);
+  free(table);
+}
