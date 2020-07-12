60790d26f6dc085cbad8f2b377a4f31690afaaff
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-12 15:07:58 +0900

    Fix symbols

--- src/symbols.c
+++ src/symbols.c
@@ -3,30 +3,17 @@
 #include "set.h"
 #include "utility.h"
 
-typedef struct {
-  const char *symbol;
-  Bool flag;
-} Symbol;
-
-static Symbol *symbol_new(const char *symbol, Bool flag) {
+Symbol *symbol_new(const char *symbol, Bool flag) {
   Symbol *ptr = UTILITY_MALLOC(Symbol);
   ptr->symbol = symbol;
   ptr->flag = flag;
   return ptr;
 }
-static void symbol_delete(Symbol *symbol) {
+void symbol_delete(Symbol *symbol) {
   UTILITY_FREE(symbol);
 }
-static int symbol_compare(const ElemType *lhs, const ElemType *rhs) {
+int symbol_compare(const ElemType *lhs, const ElemType *rhs) {
   const Symbol *l = *lhs;
   const Symbol *r = *rhs;
   return utility_str_cmp(l->symbol, r->symbol);
-}
-
-Set *symbols_new(void) {
-  Set *set = set_new((Destructor)symbol_delete, symbol_compare);
-  return set;
-}
-void symbols_delete(Set *set) {
-  set_delete(set);
 }
--- src/symbols.h
+++ src/symbols.h
@@ -3,7 +3,13 @@
 
 #include "typedef.h"
 
-Set *symbols_new(void);
-void symbols_delete(Set *);
+typedef struct {
+  const char *symbol;
+  Bool flag;
+} Symbol;
+
+Symbol *symbol_new(const char *symbol, Bool flag);
+void symbol_delete(Symbol *symbol);
+int symbol_compare(const ElemType *, const ElemType *);
 
 #endif /* INCLUDE_GUARD_F61F06FD_A1EC_40C7_BDE9_B15A87E95C41 */
