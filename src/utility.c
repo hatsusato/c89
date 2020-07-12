55ec8e54267a8b96eb9cbd7ce1cd1932d0cf37d1
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-12 11:31:49 +0900

    Add utility_{malloc,free,memcpy}

--- src/utility.c
+++ src/utility.c
@@ -1,7 +1,17 @@
 #include "utility.h"
 
+#include <stdlib.h>
 #include <string.h>
 
 Bool utility_str_eq(const char* lhs, const char* rhs) {
   return lhs && rhs && 0 == strcmp(lhs, rhs);
+}
+void* utility_malloc(size_t size) {
+  return malloc(size);
+}
+void utility_free(void* ptr) {
+  free(ptr);
+}
+void utility_memcpy(void* dst, const void* src, size_t count) {
+  memcpy(dst, src, count);
 }
