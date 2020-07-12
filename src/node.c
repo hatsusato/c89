d8a5c41ad61268323ccd65d6d7c91d1d58e8d8d3
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-12 12:29:49 +0900

    Add node_{new,delete}

--- src/node.c
+++ src/node.c
@@ -1,6 +1,21 @@
 #include "node.h"
 
+#include "utility.h"
+
 struct struct_Node {
   ElemType elem;
   Node *next;
 };
+
+Node *node_new(ElemType elem, Node *next) {
+  Node *node = UTILITY_MALLOC(Node);
+  node->elem = elem;
+  node->next = next;
+  return node;
+}
+Node *node_delete(Node *node, Destructor dtor) {
+  Node *next = node->next;
+  dtor(node->elem);
+  UTILITY_FREE(node);
+  return next;
+}
--- src/node.h
+++ src/node.h
@@ -6,4 +6,7 @@
 struct struct_Node;
 typedef struct struct_Node Node;
 
+Node *node_new(ElemType, Node *next);
+Node *node_delete(Node *, Destructor);
+
 #endif /* INCLUDE_GUARD_B708D97B_CAA0_46DF_A61B_61DC00CAC93F */
