c3b53e25b553030987b028f34283455325b266f1
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-12 15:02:48 +0900

    Add table_{new,delete}

--- src/table.c
+++ src/table.c
@@ -1,1 +1,11 @@
 #include "table.h"
+
+#include "set.h"
+#include "vector.h"
+
+Table *table_new(void) {
+  return vector_new((Destructor)set_delete);
+}
+void table_delete(Table *table) {
+  vector_delete(table);
+}
--- src/table.h
+++ src/table.h
@@ -3,4 +3,7 @@
 
 #include "typedef.h"
 
+Table *table_new(void);
+void table_delete(Table *);
+
 #endif /* INCLUDE_GUARD_DD5D0935_0DD4_46BA_8D0E_560243A08759 */
