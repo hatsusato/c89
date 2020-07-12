fd3ea2a1eae1dd7aa52a7734afa7a1b1cc8e924a
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-12 14:28:12 +0900

    Add register_is_typedef

--- src/register.c
+++ src/register.c
@@ -1,10 +1,24 @@
 #include "register.h"
 
 #include "sexp.h"
+#include "utility.h"
+
+static Bool register_check_tag(Sexp *ast, const char *tag) {
+  return sexp_is_pair(ast) && sexp_is_list(ast) && sexp_eq(sexp_car(ast), tag);
+}
 
 void register_foreach(Register *reg, Sexp *ast,
                       void (*map)(Register *, Sexp *)) {
   for (ast = sexp_cdr(ast); sexp_is_pair(ast); ast = sexp_cdr(ast)) {
     map(reg, sexp_car(ast));
+  }
+}
+void register_is_typedef(Register *reg, Sexp *ast) {
+  assert(register_check_tag(ast, "storage-class-specifier") ||
+         register_check_tag(ast, "type-specifier") ||
+         register_check_tag(ast, "type-qualifier"));
+  if (register_check_tag(ast, "storage-class-specifier") &&
+      sexp_eq(sexp_at(ast, 1), "typedef")) {
+    reg->flag = true;
   }
 }
--- src/register.h
+++ src/register.h
@@ -9,5 +9,6 @@ typedef struct {
 } Register;
 
 void register_foreach(Register *, Sexp *, void (*)(Register *, Sexp *));
+void register_is_typedef(Register *, Sexp *);
 
 #endif /* INCLUDE_GUARD_73005BDB_B13D_44DF_81E9_5D8719897410 */
