8cb6ae7ff5e434121bf81b46479597385e4a697e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-12 14:34:04 +0900

    Add register_from_{identifier,{direct_,,init_}declarator}

--- src/register.c
+++ src/register.c
@@ -6,6 +6,36 @@
 static Bool register_check_tag(Sexp *ast, const char *tag) {
   return sexp_is_pair(ast) && sexp_is_list(ast) && sexp_eq(sexp_car(ast), tag);
 }
+static const char *register_from_declarator(Sexp *ast);
+static const char *register_from_identifier(Sexp *ast) {
+  assert(register_check_tag(ast, "identifier"));
+  ast = sexp_at(ast, 1);
+  assert(sexp_is_string(ast));
+  return sexp_get(ast);
+}
+static const char *register_from_direct_declarator(Sexp *ast) {
+  assert(register_check_tag(ast, "direct-declarator"));
+  ast = sexp_cdr(ast);
+  if (register_check_tag(ast, "(")) {
+    return register_from_declarator(sexp_at(ast, 1));
+  } else {
+    ast = sexp_car(ast);
+    if (register_check_tag(ast, "identifier")) {
+      return register_from_identifier(ast);
+    } else {
+      return register_from_direct_declarator(ast);
+    }
+  }
+}
+static const char *register_from_declarator(Sexp *ast) {
+  assert(register_check_tag(ast, "declarator"));
+  ast = sexp_at(ast, sexp_length(ast) - 1);
+  return register_from_direct_declarator(ast);
+}
+static const char *register_from_init_declarator(Sexp *ast) {
+  assert(register_check_tag(ast, "init-declarator"));
+  return register_from_declarator(sexp_at(ast, 1));
+}
 
 void register_foreach(Register *reg, Sexp *ast,
                       void (*map)(Register *, Sexp *)) {
