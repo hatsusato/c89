5d1221c4b8af8fe7a5443319b5f6f7f0fa0bdfc2
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-12 11:51:50 +0900

    Fix utility

--- src/utility.c
+++ src/utility.c
@@ -3,15 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-Bool utility_str_eq(const char* lhs, const char* rhs) {
+Bool utility_str_eq(const char *lhs, const char *rhs) {
   return lhs && rhs && 0 == strcmp(lhs, rhs);
 }
-void* utility_malloc(size_t size) {
+void *utility_malloc(size_t size) {
   return malloc(size);
 }
-void utility_free(void* ptr) {
+void utility_free(void *ptr) {
   free(ptr);
 }
-void utility_memcpy(void* dst, const void* src, size_t count) {
+void utility_memcpy(void *dst, const void *src, size_t count) {
   memcpy(dst, src, count);
 }
--- src/utility.h
+++ src/utility.h
@@ -7,24 +7,24 @@
 
 #define SWAP(type, lhs, rhs) \
   do {                       \
-    type tmp = lhs;          \
-    lhs = rhs;               \
-    rhs = tmp;               \
+    type tmp = (lhs);        \
+    (lhs) = (rhs);           \
+    (rhs) = tmp;             \
   } while (0);
-#define UTILITY_MALLOC(type) (type*)utility_malloc(sizeof(type))
+#define UTILITY_MALLOC(type) (type *)utility_malloc(sizeof(type))
 #define UTILITY_MALLOC_ARRAY(type, count) \
-  (type*)utility_malloc(sizeof(type) * count)
+  (type *)utility_malloc(sizeof(type) * (count))
 #define UTILITY_FREE(var) \
   do {                    \
     utility_free(var);    \
-    var = NULL;           \
+    (var) = NULL;         \
   } while (0)
 #define UTILITY_MEMCPY(type, dst, src, count) \
-  utility_memcpy(dst, src, sizeof(type) * count)
+  utility_memcpy((dst), (src), sizeof(type) * (count))
 
-Bool utility_str_eq(const char*, const char*);
-void* utility_malloc(size_t);
-void utility_free(void*);
-void utility_memcpy(void* dst, const void* src, size_t count);
+Bool utility_str_eq(const char *, const char *);
+void *utility_malloc(size_t);
+void utility_free(void *);
+void utility_memcpy(void *dst, const void *src, size_t count);
 
 #endif /* INCLUDE_GUARD_26FC0E3E_05C7_464D_8860_9F5E5E2F9210 */
