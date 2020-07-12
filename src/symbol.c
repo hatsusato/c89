572b9fbb17327be0043174263f2d9ca36a31a673
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-12 13:06:24 +0900

    Add symbol_{new,delete}

--- src/symbol.c
+++ src/symbol.c
@@ -1,6 +1,18 @@
 #include "symbol.h"
 
+#include "utility.h"
+
 struct struct_Symbol {
-  const char* symbol;
+  const char *symbol;
   Bool flag;
 };
+
+Symbol *symbol_new(const char *symbol, Bool flag) {
+  Symbol *ptr = UTILITY_MALLOC(Symbol);
+  ptr->symbol = symbol;
+  ptr->flag = flag;
+  return ptr;
+}
+void symbol_delete(Symbol *symbol) {
+  UTILITY_FREE(symbol);
+}
--- src/symbol.h
+++ src/symbol.h
@@ -6,4 +6,7 @@
 struct struct_Symbol;
 typedef struct struct_Symbol Symbol;
 
+Symbol *symbol_new(const char *, Bool);
+void symbol_delete(Symbol *);
+
 #endif /* INCLUDE_GUARD_F61F06FD_A1EC_40C7_BDE9_B15A87E95C41 */
