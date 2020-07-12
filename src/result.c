8f88e42c13f1e02e323e9d08ad2b8b8b4f1e4425
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-12 16:15:03 +0900

    Remove Result.symbols

--- src/result.c
+++ src/result.c
@@ -1,32 +1,24 @@
 #include "result.h"
 
 #include "scanner.h"
-#include "set.h"
 #include "sexp.h"
 #include "table.h"
 #include "utility.h"
 
 struct struct_Result {
   Sexp *sexp;
-  Set *symbols;
   Table *table;
 };
-
-static int result_set_compare(const ElemType *lhs, const ElemType *rhs) {
-  return utility_str_cmp(*lhs, *rhs);
-}
 
 Result *result_new(void) {
   Result *result = UTILITY_MALLOC(Result);
   result->sexp = sexp_nil();
-  result->symbols = set_new(NULL, result_set_compare);
   result->table = table_new();
   return result;
 }
 void result_delete(Result *result) {
   assert(result);
   sexp_delete(result->sexp);
-  set_delete(result->symbols);
   table_delete(result->table);
   UTILITY_FREE(result);
 }
