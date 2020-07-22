7a5ae73354c6e093249443898e658a86e853fd8e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-22 11:28:22 +0900

    Add get_tag

--- src/generate.c
+++ src/generate.c
@@ -1,1 +1,14 @@
 #include "generate.h"
+
+#include "ast.h"
+#include "sexp.h"
+#include "utility.h"
+
+static AstTag get_tag(Sexp *ast) {
+  if (sexp_is_number(ast)) {
+    return sexp_get_number(ast);
+  } else if (sexp_is_pair(ast)) {
+    return get_tag(sexp_car(ast));
+  }
+  return AST_NULL;
+}
