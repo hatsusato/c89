5e1200bb10cc4316304ffc3eba983eea5eb10eac
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-22 10:37:08 +0900

    Remove print_seq

--- src/main.c
+++ src/main.c
@@ -1,17 +1,12 @@
-#include "pretty.h"
 #include "print.h"
 #include "result.h"
-
-void print_seq(Result *result) {
-  Sexp *sexp = result_get_sexp(result);
-  pretty_print(stdout, sexp);
-}
 
 int main(void) {
   Result *result = result_new();
   int ret = result_parse(result);
   if (0 == ret) {
-    print_seq(result);
+    Sexp *ast = result_get_sexp(result);
+    print_ast(ast);
   }
   result_delete(result);
   return ret;
