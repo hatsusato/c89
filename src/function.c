4f85b496e5046885f91c85663e93d8deebe78561
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-25 00:31:57 +0900

    Add check_tag

--- src/function.c
+++ src/function.c
@@ -1,11 +1,17 @@
 #include "function.h"
 
+#include "ast.h"
+#include "sexp.h"
 #include "utility.h"
 
 struct struct_Function {
   const char *name;
 };
 
+static Sexp *check_tag(Sexp *ast, AstTag tag) {
+  assert(ast_get(ast) == tag);
+  return sexp_cdr(ast);
+}
 Function *function_new(void) {
   Function *func = UTILITY_MALLOC(Function);
   func->name = NULL;
