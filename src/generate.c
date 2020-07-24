9082422a94c55b800a7334795081b0f3dccda673
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-25 00:37:39 +0900

    Remove unused functions

--- src/generate.c
+++ src/generate.c
@@ -1,57 +1,13 @@
 #include "generate.h"
 
-#include <stdio.h>
-
 #include "ast.h"
 #include "function.h"
 #include "sexp.h"
 #include "utility.h"
 
-static const char *identifier_from_declarator(Sexp *ast);
 static Sexp *check_tag(Sexp *ast, AstTag tag) {
   assert(ast_get(ast) == tag);
   return sexp_cdr(ast);
-}
-static const char *identifier_from_identifier(Sexp *ast) {
-  ast = check_tag(ast, AST_IDENTIFIER);
-  ast = sexp_car(ast);
-  assert(sexp_is_string(ast));
-  return sexp_get_string(ast);
-}
-static const char *identifier_from_direct_declarator(Sexp *ast) {
-  ast = check_tag(ast, AST_DIRECT_DECLARATOR);
-  switch (ast_get(ast)) {
-  case AST_IDENTIFIER:
-    return identifier_from_identifier(sexp_at(ast, 0));
-  case AST_LEFT_PAREN:
-    return identifier_from_declarator(sexp_at(ast, 1));
-  case AST_DIRECT_DECLARATOR:
-    return identifier_from_direct_declarator(sexp_at(ast, 0));
-  default:
-    assert(0);
-    return NULL;
-  }
-}
-static const char *identifier_from_declarator(Sexp *ast) {
-  ast = check_tag(ast, AST_DECLARATOR);
-  switch (sexp_length(ast)) {
-  case 1:
-    return identifier_from_direct_declarator(sexp_at(ast, 0));
-  case 2:
-    return identifier_from_direct_declarator(sexp_at(ast, 1));
-  default:
-    assert(0);
-    return NULL;
-  }
-}
-static void generate_function_definition(Sexp *ast) {
-  const char *name;
-  ast = check_tag(ast, AST_FUNCTION_DEFINITION);
-  assert(4 == sexp_length(ast));
-  name = identifier_from_declarator(sexp_at(ast, 1));
-  printf("define dso_local i32 @%s() {\n", name);
-  printf("  ret i32 0\n");
-  printf("}\n\n");
 }
 void generate_translation_unit(Sexp *ast) {
   ast = check_tag(ast, AST_TRANSLATION_UNIT);
