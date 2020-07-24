68234844e0f214fa426415584537fbcb99d13cb9
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-24 16:40:51 +0900

    Replace get_tag

--- src/generate.c
+++ src/generate.c
@@ -7,16 +7,8 @@
 #include "utility.h"
 
 static const char *identifier_from_declarator(Sexp *ast);
-static AstTag get_tag(Sexp *ast) {
-  if (sexp_is_number(ast)) {
-    return sexp_get_number(ast);
-  } else if (sexp_is_pair(ast)) {
-    return get_tag(sexp_car(ast));
-  }
-  return AST_NULL;
-}
 static Sexp *check_tag(Sexp *ast, AstTag tag) {
-  assert(get_tag(ast) == tag);
+  assert(ast_get(ast) == tag);
   return sexp_cdr(ast);
 }
 static const char *identifier_from_identifier(Sexp *ast) {
@@ -27,7 +19,7 @@ static const char *identifier_from_identifier(Sexp *ast) {
 }
 static const char *identifier_from_direct_declarator(Sexp *ast) {
   ast = check_tag(ast, AST_DIRECT_DECLARATOR);
-  switch (get_tag(ast)) {
+  switch (ast_get(ast)) {
   case AST_IDENTIFIER:
     return identifier_from_identifier(sexp_at(ast, 0));
   case AST_LEFT_PAREN:
