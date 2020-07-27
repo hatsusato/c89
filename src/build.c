5920171d171094a76424b8a4cc0d9cb40be366b9
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-27 21:46:59 +0900

    Add build_function

--- src/build.c
+++ src/build.c
@@ -1,1 +1,10 @@
 #include "build.h"
+
+#include "function.h"
+
+Function *build_function(Sexp *ast) {
+  Function *func = function_new();
+  const char *name = function_name(ast);
+  function_set_name(func, name);
+  return func;
+}
