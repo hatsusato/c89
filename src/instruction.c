384db754bde9f894a6fe33156313e68bca90a769
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-30 14:57:08 +0900

    Add instruction_{new,delete}

--- src/instruction.c
+++ src/instruction.c
@@ -1,5 +1,16 @@
 #include "instruction.h"
 
+#include "utility.h"
+
 struct struct_Instruction {
   InstructionTag tag;
 };
+
+Instruction *instruction_new(InstructionTag tag) {
+  Instruction *instr = UTILITY_MALLOC(Instruction);
+  instr->tag = tag;
+  return instr;
+}
+void instruction_delete(Instruction *instr) {
+  UTILITY_FREE(instr);
+}
--- src/instruction.h
+++ src/instruction.h
@@ -9,4 +9,7 @@ typedef enum {
   INSTRUCTION_RET
 } InstructionTag;
 
+Instruction *instruction_new(InstructionTag);
+void instruction_delete(Instruction *);
+
 #endif /* INCLUDE_GUARD_B22CD5CF_CFA4_4141_958E_3B8F73C9A6F1 */
