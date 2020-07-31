b542407ab990555f71b04154aea91c764e97b796
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-31 22:40:40 +0900

    Add block_insert

--- src/block.c
+++ src/block.c
@@ -1,5 +1,6 @@
 #include "block.h"
 
+#include "instruction.h"
 #include "utility.h"
 #include "vector.h"
 
@@ -9,11 +10,15 @@ struct struct_Block {
 
 Block *block_new(void) {
   Block *block = UTILITY_MALLOC(Block);
-  block->insts = vector_new(NULL);
+  block->insts = vector_new((Destructor)instruction_delete);
   return block;
 }
 void block_delete(Block *block) {
   assert(block);
   vector_delete(block->insts);
   UTILITY_FREE(block);
+}
+void block_insert(Block *block, Instruction *instr) {
+  assert(block);
+  vector_push(block->insts, instr);
 }
--- src/block.h
+++ src/block.h
@@ -5,5 +5,6 @@
 
 Block *block_new(void);
 void block_delete(Block *);
+void block_insert(Block *, Instruction *);
 
 #endif /* INCLUDE_GUARD_C5893AD5_9E36_4B47_A5BD_7EDC9894A05B */
