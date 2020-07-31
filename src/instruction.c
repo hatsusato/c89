cb7e94890d9515a01a44ae5570cb05a6086dd094
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-31 22:37:41 +0900

    Add Instruction.operands

--- src/instruction.c
+++ src/instruction.c
@@ -1,16 +1,22 @@
 #include "instruction.h"
 
 #include "utility.h"
+#include "value.h"
+#include "vector.h"
 
 struct struct_Instruction {
   InstructionTag tag;
+  Vector *operands;
 };
 
 Instruction *instruction_new(InstructionTag tag) {
   Instruction *instr = UTILITY_MALLOC(Instruction);
   instr->tag = tag;
+  instr->operands = vector_new((Destructor)value_delete);
   return instr;
 }
 void instruction_delete(Instruction *instr) {
+  assert(instr);
+  vector_delete(instr->operands);
   UTILITY_FREE(instr);
 }
