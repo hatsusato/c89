c8bb09e1cec2708f186efedefdcf7811fa2b4fa2
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-08-01 12:22:31 +0900

    Add block_print

--- src/block.c
+++ src/block.c
@@ -22,3 +22,10 @@ void block_insert(Block *block, Instruction *instr) {
   assert(block);
   vector_push(block->insts, instr);
 }
+void block_print(FILE *fp, Block *block) {
+  ElemType *it;
+  assert(block);
+  for (it = vector_begin(block->insts); it != vector_end(block->insts); ++it) {
+    instruction_print(fp, (Instruction *)*it);
+  }
+}
--- src/block.h
+++ src/block.h
@@ -1,10 +1,13 @@
 #ifndef INCLUDE_GUARD_C5893AD5_9E36_4B47_A5BD_7EDC9894A05B
 #define INCLUDE_GUARD_C5893AD5_9E36_4B47_A5BD_7EDC9894A05B
 
+#include <stdio.h>
+
 #include "typedef.h"
 
 Block *block_new(void);
 void block_delete(Block *);
 void block_insert(Block *, Instruction *);
+void block_print(FILE *, Block *);
 
 #endif /* INCLUDE_GUARD_C5893AD5_9E36_4B47_A5BD_7EDC9894A05B */
