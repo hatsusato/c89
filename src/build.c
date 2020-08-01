24d57d65b5f248da7f1fb03b7342874d264d630a
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-08-01 12:33:01 +0900

    Update build_function

--- src/build.c
+++ src/build.c
@@ -1,10 +1,20 @@
 #include "build.h"
 
+#include "block.h"
 #include "function.h"
+#include "instruction.h"
+#include "value.h"
 
 Function *build_function(Sexp *ast) {
-  Function *func = function_new();
-  const char *name = function_name(ast);
-  function_set_name(func, name);
+  Instruction *instr;
+  Block *block;
+  Function *func;
+  instr = instruction_new(INSTRUCTION_RET);
+  instruction_insert(instr, value_new_integer(0));
+  block = block_new();
+  block_insert(block, instr);
+  func = function_new();
+  function_set_name(func, function_name(ast));
+  function_insert(func, block);
   return func;
 }
