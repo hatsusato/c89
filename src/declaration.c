5501a2b11b968976c7ddf96f0426d69515b4261a
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-08-02 14:17:53 +0900

    Add declaration_new_function

--- src/declaration.c
+++ src/declaration.c
@@ -1,17 +1,27 @@
 #include "declaration.h"
 
+#include "function.h"
 #include "utility.h"
 
 struct struct_Declaration {
   DeclarationTag tag;
+  void *data;
 };
 
 static Declaration *declaration_new(DeclarationTag tag) {
   Declaration *decl = UTILITY_MALLOC(Declaration);
   decl->tag = tag;
+  decl->data = NULL;
   return decl;
 }
 
+Declaration *declaration_new_function(Function *func) {
+  Declaration *decl = declaration_new(DECLARATION_FUNCTION);
+  decl->data = func;
+  return decl;
+}
 void declaration_delete(Declaration *decl) {
+  assert(decl);
+  function_delete(decl->data);
   UTILITY_FREE(decl);
 }
--- src/declaration.h
+++ src/declaration.h
@@ -5,6 +5,7 @@
 
 typedef enum { DECLARATION_FUNCTION } DeclarationTag;
 
+Declaration *declaration_new_function(Function *);
 void declaration_delete(Declaration *);
 
 #endif /* INCLUDE_GUARD_0DCFEB82_40C6_4B2A_A620_2FA21E8F81A8 */
