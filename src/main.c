b17206dd3a5b4d25922f3f59b562885627184514
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-08-02 15:43:37 +0900

    Update main

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "generate.h"
+#include "module.h"
 #include "print.h"
 #include "result.h"
 
@@ -7,8 +8,11 @@ int main(void) {
   int ret = result_parse(result);
   if (0 == ret) {
     Sexp *ast = result_get_sexp(result);
+    Module *module = module_build(ast);
     print_ast(ast);
     generate_translation_unit(ast);
+    module_print(stdout, module);
+    module_delete(module);
   }
   result_delete(result);
   return ret;
