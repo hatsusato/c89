7cef41716aa0abe0ac4b0f9301ca58a91b001469
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-08-02 14:11:12 +0900

    Add declaration_{new,delete}

--- src/declaration.c
+++ src/declaration.c
@@ -1,5 +1,17 @@
 #include "declaration.h"
 
+#include "utility.h"
+
 struct struct_Declaration {
   DeclarationTag tag;
 };
+
+static Declaration *declaration_new(DeclarationTag tag) {
+  Declaration *decl = UTILITY_MALLOC(Declaration);
+  decl->tag = tag;
+  return decl;
+}
+
+void declaration_delete(Declaration *decl) {
+  UTILITY_FREE(decl);
+}
--- src/declaration.h
+++ src/declaration.h
@@ -5,4 +5,6 @@
 
 typedef enum { DECLARATION_FUNCTION } DeclarationTag;
 
+void declaration_delete(Declaration *);
+
 #endif /* INCLUDE_GUARD_0DCFEB82_40C6_4B2A_A620_2FA21E8F81A8 */
