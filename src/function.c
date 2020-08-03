e165b3500eb8db639bf823ac766030074aa8d7fe
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-08-03 20:39:30 +0900

    Remove function_set*

--- src/function.c
+++ src/function.c
@@ -14,49 +14,6 @@ struct struct_Function {
   Vector *blocks;
 };
 
-static void function_set_declarator(Function *, Sexp *);
-static void function_set_identifier(Function *func, Sexp *ast) {
-  ast = sexp_next(ast, AST_IDENTIFIER);
-  ast = sexp_car(ast);
-  assert(sexp_is_string(ast));
-  func->name = sexp_get_string(ast);
-}
-static void function_set_direct_declarator(Function *func, Sexp *ast) {
-  ast = sexp_next(ast, AST_DIRECT_DECLARATOR);
-  switch (ast_get(ast)) {
-  case AST_IDENTIFIER:
-    function_set_identifier(func, sexp_at(ast, 0));
-    break;
-  case AST_LEFT_PAREN:
-    function_set_declarator(func, sexp_at(ast, 1));
-    break;
-  case AST_DIRECT_DECLARATOR:
-    function_set_direct_declarator(func, sexp_at(ast, 0));
-    break;
-  default:
-    assert(0);
-  }
-}
-static void function_set_declarator(Function *func, Sexp *ast) {
-  ast = sexp_next(ast, AST_DECLARATOR);
-  switch (sexp_length(ast)) {
-  case 1:
-    function_set_direct_declarator(func, sexp_at(ast, 0));
-    break;
-  case 2:
-    function_set_direct_declarator(func, sexp_at(ast, 1));
-    break;
-  default:
-    assert(0);
-  }
-}
-
-static Function *function_new(void) {
-  Function *func = UTILITY_MALLOC(Function);
-  func->name = NULL;
-  func->blocks = vector_new((Destructor)block_delete);
-  return func;
-}
 static const char *function_name_from_declarator(Sexp *);
 static const char *function_name_from_identifier(Sexp *ast) {
   ast = sexp_next(ast, AST_IDENTIFIER);
@@ -102,10 +59,11 @@ static const char *function_name(Sexp *ast) {
     return NULL;
   }
 }
-static void function_set(Function *func, Sexp *ast) {
-  ast = sexp_next(ast, AST_FUNCTION_DEFINITION);
-  assert(4 == sexp_length(ast));
-  function_set_declarator(func, sexp_at(ast, 1));
+static Function *function_new(void) {
+  Function *func = UTILITY_MALLOC(Function);
+  func->name = NULL;
+  func->blocks = vector_new((Destructor)block_delete);
+  return func;
 }
 static void function_insert(Function *func, Block *block) {
   assert(func);
