6f152e78e01a8dd920ec80d1f0fdaa64764b1c18
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-08-09 12:18:13 +0900

    Add builder_function

--- src/builder.c
+++ src/builder.c
@@ -5,18 +5,25 @@
 
 struct struct_Builder {
   Module *module;
+  Function *func;
 };
 
 Builder *builder_new(Module *module) {
   Builder *builder = UTILITY_MALLOC(Builder);
   builder->module = module;
+  builder->func = NULL;
   return builder;
 }
 void builder_delete(Builder *builder) {
   assert(builder);
+  assert(!builder->func);
   UTILITY_FREE(builder);
 }
 Module *builder_module(Builder *builder) {
   assert(builder);
   return builder->module;
+}
+Function *builder_function(Builder *builder) {
+  assert(builder);
+  return builder->func;
 }
--- src/builder.h
+++ src/builder.h
@@ -6,5 +6,6 @@
 Builder *builder_new(Module *);
 void builder_delete(Builder *);
 Module *builder_module(Builder *);
+Function *builder_function(Builder *);
 
 #endif /* INCLUDE_GUARD_B9CC5D68_6094_4DEA_88B4_20E85C99FEFC */
