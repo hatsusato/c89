b1f8507cce2fd1f57637abad38b38798da4cec3d
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-21 00:48:35 +0900

    Add scanner_{new,delete}

--- include/scanner.h
+++ include/scanner.h
@@ -1,3 +1,9 @@
 #ifndef INCLUDE_GUARD_29982745_2BB0_454B_9528_9FABEC206FF9
 #define INCLUDE_GUARD_29982745_2BB0_454B_9528_9FABEC206FF9
+
+#include "scanner_type.h"
+
+Scanner *scanner_new(void);
+void scanner_delete(Scanner *);
+
 #endif /* INCLUDE_GUARD_29982745_2BB0_454B_9528_9FABEC206FF9 */
--- src/scanner.c
+++ src/scanner.c
@@ -1,7 +1,21 @@
 #include "scanner.h"
 
-#include "scanner_type.h"
+#include "parser.tab.h"
+#include "utility.h"
 
 struct struct_Scanner {
   yyscan_t yyscan;
 };
+
+Scanner *scanner_new(void) {
+  Scanner *scanner = UTILITY_MALLOC(Scanner);
+  int ret = yylex_init(&scanner->yyscan);
+  assert(0 == ret);
+  (void)ret;
+  yyset_extra(scanner, scanner->yyscan);
+  return scanner;
+}
+void scanner_delete(Scanner *scanner) {
+  yylex_destroy(scanner->yyscan);
+  UTILITY_FREE(scanner);
+}
