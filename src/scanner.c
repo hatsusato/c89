16f1d33d3d064026ab375e3728bbcce71920c991
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-21 01:26:24 +0900

    Add Scanner.table

--- src/scanner.c
+++ src/scanner.c
@@ -2,11 +2,13 @@
 
 #include "parser.tab.h"
 #include "sexp.h"
+#include "table.h"
 #include "utility.h"
 
 struct struct_Scanner {
   yyscan_t yyscan;
   Sexp *ast;
+  Table *table;
 };
 
 Scanner *scanner_new(void) {
@@ -15,10 +17,12 @@ Scanner *scanner_new(void) {
   assert(0 == ret);
   (void)ret;
   yyset_extra(scanner, scanner->yyscan);
-  scanner->ast = NULL;
+  scanner->ast = sexp_nil();
+  scanner->table = table_new();
   return scanner;
 }
 void scanner_delete(Scanner *scanner) {
+  table_delete(scanner->table);
   sexp_delete(scanner->ast);
   yylex_destroy(scanner->yyscan);
   UTILITY_FREE(scanner);
@@ -27,6 +31,7 @@ int scanner_parse(Scanner *scanner) {
   return yyparse(scanner->yyscan);
 }
 void scanner_set_ast(Scanner *scanner, Sexp *ast) {
+  sexp_delete(scanner->ast);
   scanner->ast = ast;
 }
 Sexp *scanner_get_ast(Scanner *scanner) {
