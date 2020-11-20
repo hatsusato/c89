bbdad6872721be0ca5446b1390919936eb17fafc
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-21 00:58:59 +0900

    Add scanner_[sg]et_ast

--- include/scanner.h
+++ include/scanner.h
@@ -2,9 +2,12 @@
 #define INCLUDE_GUARD_29982745_2BB0_454B_9528_9FABEC206FF9
 
 #include "scanner_type.h"
+#include "sexp_type.h"
 
 Scanner *scanner_new(void);
 void scanner_delete(Scanner *);
 int scanner_parse(Scanner *);
+void scanner_set_ast(Scanner *, Sexp *);
+Sexp *scanner_get_ast(Scanner *);
 
 #endif /* INCLUDE_GUARD_29982745_2BB0_454B_9528_9FABEC206FF9 */
--- src/scanner.c
+++ src/scanner.c
@@ -1,10 +1,12 @@
 #include "scanner.h"
 
 #include "parser.tab.h"
+#include "sexp.h"
 #include "utility.h"
 
 struct struct_Scanner {
   yyscan_t yyscan;
+  Sexp *ast;
 };
 
 Scanner *scanner_new(void) {
@@ -13,12 +15,20 @@ Scanner *scanner_new(void) {
   assert(0 == ret);
   (void)ret;
   yyset_extra(scanner, scanner->yyscan);
+  scanner->ast = NULL;
   return scanner;
 }
 void scanner_delete(Scanner *scanner) {
+  sexp_delete(scanner->ast);
   yylex_destroy(scanner->yyscan);
   UTILITY_FREE(scanner);
 }
 int scanner_parse(Scanner *scanner) {
   return yyparse(scanner->yyscan);
+}
+void scanner_set_ast(Scanner *scanner, Sexp *ast) {
+  scanner->ast = ast;
+}
+Sexp *scanner_get_ast(Scanner *scanner) {
+  return scanner->ast;
 }
