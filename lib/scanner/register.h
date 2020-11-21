8145a15c356c3124cf0b032dd75c3e6de643c6e9
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-22 01:32:42 +0900

    Remove scanner_type.h

--- lib/scanner/register.h
+++ lib/scanner/register.h
@@ -1,7 +1,7 @@
 #ifndef INCLUDE_GUARD_2F3411B7_BDBD_4EB1_815C_B08161816E03
 #define INCLUDE_GUARD_2F3411B7_BDBD_4EB1_815C_B08161816E03
 
-#include "scanner_type.h"
+#include "lib/scanner.h"
 #include "sexp_type.h"
 
 void register_declaration(Scanner *, Sexp *);
