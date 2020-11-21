0380ec5069aaff9f369f39ea84af11b6622c37c7
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-21 16:06:29 +0900

    Remove reg_declaration

--- include/register.h
+++ include/register.h
@@ -3,9 +3,7 @@
 
 #include "scanner_type.h"
 #include "sexp_type.h"
-#include "symbol.h"
 
-void reg_declaration(SymbolSet *, Sexp *);
 void register_declaration(Scanner *, Sexp *);
 
 #endif /* INCLUDE_GUARD_2F3411B7_BDBD_4EB1_815C_B08161816E03 */
