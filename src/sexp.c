1061ad016c4b4e0ca81bc2360f7388f2e88c0595
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-21 19:07:47 +0900

    Remove unused sexp_*

--- src/sexp.c
+++ src/sexp.c
@@ -30,6 +30,11 @@ static MutableSexp *sexp_new(SexpKind kind) {
 static void sexp_free(MutableSexp *sexp) {
   UTILITY_FREE(sexp);
 }
+static Sexp *sexp_set_cdr(Sexp *sexp, Sexp *cdr) {
+  Sexp *tmp = sexp_cdr(sexp);
+  ((MutableSexp *)sexp)->data.pair.cdr = cdr;
+  return tmp;
+}
 
 Sexp *sexp_nil(void) {
   static MutableSexp sexp = {SEXP_NIL, {NULL}};
@@ -52,18 +57,6 @@ Sexp *sexp_snoc(Sexp *xs, Sexp *x) {
   }
   assert(sexp_is_nil(xs));
   return sexp_cons(x, xs);
-}
-Sexp *sexp_append(Sexp *lhs, Sexp *rhs) {
-  Sexp *sexp = lhs;
-  assert(sexp_is_list(lhs));
-  for (; sexp_is_pair(sexp); sexp = sexp_cdr(sexp)) {
-    if (sexp_is_nil(sexp_cdr(sexp))) {
-      sexp_set_cdr(sexp, rhs);
-      return lhs;
-    }
-  }
-  assert(sexp_is_nil(lhs));
-  return rhs;
 }
 Sexp *sexp_string(const char *text, int leng) {
   MutableSexp *sexp = sexp_new(SEXP_STRING);
@@ -101,32 +94,6 @@ void sexp_delete(Sexp *sexp) {
   }
   sexp_free((MutableSexp *)sexp);
 }
-Sexp *sexp_set_car(Sexp *sexp, Sexp *car) {
-  Sexp *tmp = sexp_car(sexp);
-  ((MutableSexp *)sexp)->data.pair.car = car;
-  return tmp;
-}
-Sexp *sexp_set_cdr(Sexp *sexp, Sexp *cdr) {
-  Sexp *tmp = sexp_cdr(sexp);
-  ((MutableSexp *)sexp)->data.pair.cdr = cdr;
-  return tmp;
-}
-void sexp_set_caar(Sexp *sexp, Sexp *caar) {
-  assert(sexp_is_pair(sexp));
-  sexp_set_car(sexp, sexp_cons(caar, sexp_car(sexp)));
-}
-void sexp_set_cadr(Sexp *sexp, Sexp *cadr) {
-  assert(sexp_is_pair(sexp));
-  sexp_set_car(sexp, sexp_cons(sexp_car(sexp), cadr));
-}
-void sexp_set_cdar(Sexp *sexp, Sexp *cdar) {
-  assert(sexp_is_pair(sexp));
-  sexp_set_cdr(sexp, sexp_cons(cdar, sexp_cdr(sexp)));
-}
-void sexp_set_cddr(Sexp *sexp, Sexp *cddr) {
-  assert(sexp_is_pair(sexp));
-  sexp_set_cdr(sexp, sexp_cons(sexp_cdr(sexp), cddr));
-}
 Bool sexp_is_nil(Sexp *sexp) {
   return SEXP_NIL == sexp_kind(sexp);
 }
@@ -160,14 +127,6 @@ Sexp *sexp_at(Sexp *sexp, int index) {
   } else {
     return sexp_car(sexp);
   }
-}
-Sexp *sexp_next(Sexp *sexp, int n) {
-  Sexp *tmp = sexp;
-  assert(sexp_is_pair(sexp));
-  sexp = sexp_car(sexp);
-  assert(sexp_is_number(sexp));
-  assert(sexp_get_number(sexp) == n);
-  return sexp_cdr(tmp);
 }
 const char *sexp_get_string(Sexp *sexp) {
   return sexp_is_string(sexp) ? str_begin(sexp->data.string) : NULL;
