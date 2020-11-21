778991164c0ca1b8ceb70dbaef18a8d1162812d1
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-21 15:13:06 +0900

    Add register_*

--- include/register.h
+++ include/register.h
@@ -1,9 +1,11 @@
 #ifndef INCLUDE_GUARD_2F3411B7_BDBD_4EB1_815C_B08161816E03
 #define INCLUDE_GUARD_2F3411B7_BDBD_4EB1_815C_B08161816E03
 
+#include "scanner_type.h"
 #include "sexp_type.h"
 #include "symbol.h"
 
 void reg_declaration(SymbolSet *, Sexp *);
+void register_declaration(Scanner *, Sexp *);
 
 #endif /* INCLUDE_GUARD_2F3411B7_BDBD_4EB1_815C_B08161816E03 */
--- src/register.c
+++ src/register.c
@@ -1,6 +1,7 @@
 #include "register.h"
 
 #include "ast.h"
+#include "scanner.h"
 #include "sexp.h"
 #include "utility.h"
 
@@ -105,8 +106,60 @@ static Bool is_typedef_declaration_specifiers(Sexp *ast) {
   }
   return false;
 }
-Bool is_typedef_declaration(Sexp *ast) {
+static Bool is_typedef_declaration(Sexp *ast) {
   assert(check_tag(ast, AST_DECLARATION));
   ast = sexp_at(ast, 1);
   return is_typedef_declaration_specifiers(ast);
+}
+
+static void register_identifier(Scanner *scanner, Sexp *ast, Bool flag) {
+  assert(check_tag(ast, AST_IDENTIFIER));
+  ast = sexp_at(ast, 1);
+  assert(sexp_is_string(ast));
+  scanner_register(scanner, sexp_get_string(ast), flag);
+}
+static void register_declarator(Scanner *, Sexp *, Bool);
+static void register_direct_declarator(Scanner *scanner, Sexp *ast, Bool flag) {
+  Sexp *next;
+  assert(check_tag(ast, AST_DIRECT_DECLARATOR));
+  next = sexp_at(ast, 1);
+  if (check_tag(next, AST_LEFT_PAREN)) {
+    next = sexp_at(ast, 2);
+    register_declarator(scanner, next, flag);
+  } else if (check_tag(next, AST_IDENTIFIER)) {
+    register_identifier(scanner, next, flag);
+  } else {
+    register_direct_declarator(scanner, next, flag);
+  }
+}
+static void register_declarator(Scanner *scanner, Sexp *ast, Bool flag) {
+  Sexp *next;
+  assert(check_tag(ast, AST_DECLARATOR));
+  next = sexp_at(ast, 1);
+  if (check_tag(next, AST_POINTER)) {
+    next = sexp_at(ast, 2);
+  }
+  register_direct_declarator(scanner, next, flag);
+}
+static void register_init_declarator(Scanner *scanner, Sexp *ast, Bool flag) {
+  assert(check_tag(ast, AST_INIT_DECLARATOR));
+  ast = sexp_at(ast, 1);
+  register_declarator(scanner, ast, flag);
+}
+static void register_init_declarator_list(Scanner *scanner, Sexp *ast,
+                                          Bool flag) {
+  assert(check_tag(ast, AST_INIT_DECLARATOR_LIST));
+  for (ast = sexp_cdr(ast); sexp_is_pair(ast); ast = sexp_cdr(ast)) {
+    register_init_declarator(scanner, sexp_car(ast), flag);
+  }
+}
+void register_declaration(Scanner *scanner, Sexp *ast) {
+  assert(check_tag(ast, AST_DECLARATION));
+  if (is_typedef_declaration(ast)) {
+    ast = sexp_at(ast, 2);
+    register_init_declarator_list(scanner, ast, true);
+  } else {
+    ast = sexp_at(ast, 2);
+    register_init_declarator_list(scanner, ast, false);
+  }
 }
