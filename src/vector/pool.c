17f4427abff49e72eb413e8f15c98cc44beec79c
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-23 17:09:15 +0900

    Add pool_find

--- src/vector/pool.c
+++ src/vector/pool.c
@@ -24,6 +24,28 @@ static void entry_destructor(ElemType elem) {
   UTILITY_FREE(buf);
   UTILITY_FREE(elem);
 }
+static int entry_compare(const ElemType *l, const ElemType *r) {
+  Entry *lhs = *l;
+  Entry *rhs = *r;
+  int diff = lhs->size - rhs->size;
+  int ret = memcmp(lhs->buf, rhs->buf, 0 < diff ? lhs->size : rhs->size);
+  return 0 == ret ? diff : ret;
+}
+static const ElemType *pool_find(Pool *pool, const void *buf, Size size) {
+  const ElemType *it;
+  const ElemType *begin = vector_begin(pool->pool);
+  const ElemType *end = vector_end(pool->pool);
+  Entry entry;
+  ElemType key = &entry;
+  entry.size = size;
+  entry.buf = buf;
+  for (it = begin; it != end; ++it) {
+    if (entry_compare(it, &key)) {
+      return it;
+    }
+  }
+  return NULL;
+}
 
 Pool *pool_new(void) {
   Pool *pool = UTILITY_MALLOC(Pool);
@@ -35,7 +57,13 @@ void pool_delete(Pool *pool) {
   UTILITY_FREE(pool);
 }
 const void *pool_insert(Pool *pool, const void *buf, Size size) {
-  Entry *entry = entry_constructor(buf, size);
-  vector_push(pool->pool, entry);
+  const ElemType *found = pool_find(pool, buf, size);
+  Entry *entry;
+  if (found) {
+    entry = *found;
+  } else {
+    entry = entry_constructor(buf, size);
+    vector_push(pool->pool, entry);
+  }
   return entry->buf;
 }
