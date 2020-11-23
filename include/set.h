842d548bee1ee54e319ea1eadac31e82d314120d
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-23 15:47:05 +0900

    Remove pool

--- include/set.h
+++ include/set.h
@@ -5,7 +5,6 @@
 
 struct struct_Set;
 typedef struct struct_Set Set;
-typedef Set Pool;
 typedef int (*Compare)(const ElemType *, const ElemType *);
 
 Set *set_new(Destructor, Compare);
@@ -15,11 +14,5 @@ Bool set_contains(const Set *, ElemType);
 const ElemType *set_find(const Set *, ElemType);
 const ElemType *set_begin(const Set *);
 const ElemType *set_end(const Set *);
-
-Pool *pool_new(Bool);
-void pool_delete(Pool *);
-const char *pool_construct(const char *, Size);
-const char *pool_insert(Pool *, const char *);
-Bool pool_contains(Pool *, const char *);
 
 #endif /* INCLUDE_GUARD_DE6D0AC9_F1A3_4A4E_90C8_FBAA690CD01A */
