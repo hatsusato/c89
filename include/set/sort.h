e980c8c59aad1afead05992b4060715eed6055af
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-23 21:51:42 +0900

    Add quick_sort

--- include/set/sort.h
+++ include/set/sort.h
@@ -5,5 +5,6 @@
 #include "types.h"
 
 void insert_sort(ElemType *, ElemType *, Compare *);
+void quick_sort(ElemType *, ElemType *, Compare *);
 
 #endif /* INCLUDE_GUARD_0C8B0910_CE56_48A5_8BFB_257BD9BDAB76 */
--- src/set/sort.c
+++ src/set/sort.c
@@ -1,5 +1,11 @@
 #include "set/sort.h"
 
+#include "utility.h"
+
+static Size sort_threshold(void) {
+  return 32;
+}
+
 void insert_sort(ElemType *begin, ElemType *end, Compare *cmp) {
   ElemType *cur = begin;
   while (++cur < end) {
@@ -9,4 +15,38 @@ void insert_sort(ElemType *begin, ElemType *end, Compare *cmp) {
     }
     *dst = val;
   }
+}
+void quick_sort(ElemType *begin, ElemType *end, Compare *cmp) {
+  ElemType pivot;
+  Size size = end - begin;
+  ElemType *lo = begin, *hi = end - 1;
+  ElemType *mid = begin + size / 2;
+  if (size < sort_threshold()) {
+    insert_sort(begin, end, cmp);
+    return;
+  }
+  if (0 < compare(cmp, *lo, *hi)) {
+    UTILITY_SWAP(ElemType, *lo, *hi);
+  }
+  if (0 < compare(cmp, *lo, *mid)) {
+    UTILITY_SWAP(ElemType, *lo, *mid);
+  } else if (0 < compare(cmp, *mid, *hi)) {
+    UTILITY_SWAP(ElemType, *mid, *hi);
+  }
+  pivot = *mid;
+  while (lo < hi) {
+    ++lo;
+    --hi;
+    while (compare(cmp, *lo, pivot) < 0) {
+      ++lo;
+    }
+    while (compare(cmp, pivot, *hi) < 0) {
+      --hi;
+    }
+    if (lo < hi) {
+      UTILITY_SWAP(ElemType, *lo, *hi);
+    }
+  }
+  quick_sort(begin, lo, cmp);
+  quick_sort(lo, end, cmp);
 }
