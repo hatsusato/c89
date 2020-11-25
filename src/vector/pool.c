9a092023269a47b376a823ecb5fbda6faea7f5c8
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-26 03:49:11 +0900

    Rename Pool.pool

--- src/vector/pool.c
+++ src/vector/pool.c
@@ -6,33 +6,33 @@
 #include "utility.h"
 
 struct struct_Pool {
-  Vector *pool;
+  Vector *vec;
   Compare *cmp;
 };
 
 static void pool_sort(Pool *pool) {
-  ElemType *begin = vector_begin(pool->pool);
-  ElemType *end = vector_end(pool->pool);
+  ElemType *begin = vector_begin(pool->vec);
+  ElemType *end = vector_end(pool->vec);
   quick_sort(begin, end, pool->cmp);
 }
 
 Pool *pool_new(Vector *vec, Compare *cmp) {
   Pool *pool = UTILITY_MALLOC(Pool);
-  pool->pool = vec;
+  pool->vec = vec;
   pool->cmp = cmp;
   return pool;
 }
 void pool_delete(Pool *pool) {
   compare_delete(pool->cmp);
-  vector_delete(pool->pool);
+  vector_delete(pool->vec);
   UTILITY_FREE(pool);
 }
 void pool_insert(Pool *pool, ElemType elem) {
-  vector_push(pool->pool, elem);
+  vector_push(pool->vec, elem);
   pool_sort(pool);
 }
 const ElemType *pool_find(Pool *pool, ElemType key) {
-  ElemType *begin = vector_begin(pool->pool);
-  ElemType *end = vector_end(pool->pool);
+  ElemType *begin = vector_begin(pool->vec);
+  ElemType *end = vector_end(pool->vec);
   return binary_search(key, begin, end, pool->cmp);
 }
