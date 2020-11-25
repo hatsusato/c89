859440216d9de898a90f080062e0efaeb49429b4
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-26 04:21:57 +0900

    Replace type of Ast.symbols

--- src/ast/ast.c
+++ src/ast/ast.c
@@ -1,13 +1,13 @@
 #include "ast.h"
 
 #include "compare.h"
-#include "set/pool.h"
+#include "set.h"
 #include "sexp.h"
 #include "utility.h"
 #include "vector.h"
 
 struct struct_Ast {
-  Pool *symbols;
+  Set *symbols;
   Sexp *sexp;
 };
 
@@ -29,13 +29,13 @@ Ast *ast_new(void) {
   Ast *ast = UTILITY_MALLOC(Ast);
   Vector *vec = vector_new(symbol_free);
   Compare *cmp = compare_new(symbol_compare);
-  ast->symbols = pool_new(vec, cmp);
+  ast->symbols = set_new(vec, cmp);
   ast->sexp = sexp_nil();
   return ast;
 }
 void ast_delete(Ast *ast) {
   sexp_delete(ast->sexp);
-  pool_delete(ast->symbols);
+  set_delete(ast->symbols);
   UTILITY_FREE(ast);
 }
 Sexp *ast_get(Ast *ast) {
@@ -48,12 +48,12 @@ void ast_set(Ast *ast, Sexp *sexp) {
 const char *ast_symbol(Ast *ast, const char *text, Size leng) {
   const ElemType *found;
   assert('\0' == text[leng]);
-  found = pool_find(ast->symbols, (ElemType)text);
+  found = set_find(ast->symbols, (ElemType)text);
   if (found) {
     return *found;
   } else {
     ElemType elem = (ElemType)symbol_new(text, leng);
-    pool_insert(ast->symbols, elem);
+    set_insert(ast->symbols, elem);
     return elem;
   }
 }
