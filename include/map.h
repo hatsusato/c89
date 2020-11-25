a4fbebda348646a517590cfb13faf376563fc838
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-25 12:59:09 +0900

    Fix Map

--- include/map.h
+++ include/map.h
@@ -1,9 +1,10 @@
 #ifndef INCLUDE_GUARD_0A0BAD8D_B420_4D3D_B9E5_6504EE1C3925
 #define INCLUDE_GUARD_0A0BAD8D_B420_4D3D_B9E5_6504EE1C3925
 
+#include "compare_type.h"
 #include "map/map_type.h"
 
-Map *map_new(void);
+Map *map_new(Compare *);
 void map_delete(Map *);
 
 #endif /* INCLUDE_GUARD_0A0BAD8D_B420_4D3D_B9E5_6504EE1C3925 */
--- src/map/map.c
+++ src/map/map.c
@@ -1,16 +1,30 @@
 #include "map.h"
 
+#include "compare.h"
 #include "set.h"
+#include "types.h"
 #include "utility.h"
 
 struct struct_Map {
   Set *set;
 };
+typedef struct {
+  ElemType key, val;
+} Pair;
 
-Map *map_new(void) {
+static int map_compare(ElemType lhs, ElemType rhs, CompareExtra extra) {
+  const Pair *l = lhs, *r = rhs;
+  return compare_cmp(extra, l->key, r->key);
+}
+
+Map *map_new(Compare *keycmp) {
   Map *map = UTILITY_MALLOC(Map);
+  Compare *compare = compare_new(map_compare);
+  compare_set_extra(compare, keycmp, (Destructor)compare_delete);
+  map->set = set_new(compare);
   return map;
 }
 void map_delete(Map *map) {
+  set_delete(map->set);
   UTILITY_FREE(map);
 }
