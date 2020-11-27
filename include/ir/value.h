3297cbb3b7fcaf789b9be325b59c750f1f9dd227
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-27 17:26:11 +0900

    Hide value_compare

--- include/ir/value.h
+++ include/ir/value.h
@@ -1,7 +1,6 @@
 #ifndef INCLUDE_GUARD_FE2AA6D9_B675_4B2E_839A_2B085264192A
 #define INCLUDE_GUARD_FE2AA6D9_B675_4B2E_839A_2B085264192A
 
-#include "compare.h"
 #include "ir/value_type.h"
 #include "set/set_type.h"
 #include "sexp_type.h"
@@ -11,6 +10,5 @@ void value_pool_delete(Set *);
 Value *value_register(void);
 Value *value_integer_constant(Sexp *);
 void value_delete(Value *);
-int value_compare(ElemType, ElemType, CompareExtra);
 
 #endif /* INCLUDE_GUARD_FE2AA6D9_B675_4B2E_839A_2B085264192A */
--- src/ir/value.c
+++ src/ir/value.c
@@ -1,6 +1,7 @@
 #include "ir/value.h"
 
 #include "ast/ast_tag.h"
+#include "compare.h"
 #include "set.h"
 #include "sexp.h"
 #include "utility.h"
@@ -19,9 +20,25 @@ static Value *value_new(ValueKind kind) {
   value->kind = kind;
   return value;
 }
+static int value_compare(Value *lhs, Value *rhs, CompareExtra extra) {
+  UTILITY_UNUSED(extra);
+  if (lhs->kind == rhs->kind) {
+    switch (lhs->kind) {
+    case VALUE_REGISTER:
+      return utility_intcmp(lhs->value.reg, rhs->value.reg);
+    case VALUE_INTEGER_CONSTANT:
+      return utility_strcmp(lhs->value.integer, rhs->value.integer);
+    default:
+      assert(0);
+      return 0;
+    }
+  } else {
+    return lhs->kind < rhs->kind ? -1 : 1;
+  }
+}
 
 Set *value_pool_new(void) {
-  Compare *cmp = compare_new(value_compare);
+  Compare *cmp = compare_new((Cmp)value_compare);
   Set *pool = set_new((Destructor)value_delete, cmp);
   return pool;
 }
@@ -43,21 +60,4 @@ Value *value_integer_constant(Sexp *ast) {
 }
 void value_delete(Value *value) {
   UTILITY_FREE(value);
-}
-int value_compare(ElemType l, ElemType r, CompareExtra extra) {
-  Value *lhs = l, *rhs = r;
-  UTILITY_UNUSED(extra);
-  if (lhs->kind == rhs->kind) {
-    switch (lhs->kind) {
-    case VALUE_REGISTER:
-      return utility_intcmp(lhs->value.reg, rhs->value.reg);
-    case VALUE_INTEGER_CONSTANT:
-      return utility_strcmp(lhs->value.integer, rhs->value.integer);
-    default:
-      assert(0);
-      return 0;
-    }
-  } else {
-    return lhs->kind < rhs->kind ? -1 : 1;
-  }
 }
