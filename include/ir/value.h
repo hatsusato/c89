c132f81e68600c69557bc26a88f4d88c8240671f
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-27 16:51:51 +0900

    Add value_integer_constant

--- include/ir/value.h
+++ include/ir/value.h
@@ -2,7 +2,9 @@
 #define INCLUDE_GUARD_FE2AA6D9_B675_4B2E_839A_2B085264192A
 
 #include "ir/value_type.h"
+#include "sexp_type.h"
 
 Value *value_register(void);
+Value *value_integer_constant(Sexp *);
 
 #endif /* INCLUDE_GUARD_FE2AA6D9_B675_4B2E_839A_2B085264192A */
--- src/ir/value.c
+++ src/ir/value.c
@@ -1,5 +1,7 @@
 #include "ir/value.h"
 
+#include "ast/ast_tag.h"
+#include "sexp.h"
 #include "utility.h"
 
 typedef enum { VALUE_REGISTER, VALUE_INTEGER_CONSTANT } ValueKind;
@@ -7,6 +9,7 @@ struct struct_Value {
   ValueKind kind;
   union {
     int reg;
+    const char *integer;
   } value;
 };
 
@@ -19,5 +22,13 @@ static Value *value_new(ValueKind kind) {
 Value *value_register(void) {
   Value *value = value_new(VALUE_REGISTER);
   value->value.reg = 0;
+  return value;
+}
+Value *value_integer_constant(Sexp *ast) {
+  Value *value = value_new(VALUE_INTEGER_CONSTANT);
+  assert(AST_INTEGER_CONSTANT == sexp_get_tag(ast));
+  ast = sexp_at(ast, 1);
+  assert(sexp_is_symbol(ast));
+  value->value.integer = sexp_get_symbol(ast);
   return value;
 }
