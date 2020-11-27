f5e03b948916b007f21ff7bcc5cef3250c33c383
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-27 18:25:24 +0900

    Add instruction_binary

--- include/ir/instruction.h
+++ include/ir/instruction.h
@@ -1,6 +1,10 @@
 #ifndef INCLUDE_GUARD_09C7931B_C0EE_4947_8485_E45CB53C286A
 #define INCLUDE_GUARD_09C7931B_C0EE_4947_8485_E45CB53C286A
 
+#include "ir/builder_type.h"
 #include "ir/instruction_type.h"
+#include "ir/value_type.h"
+
+Instruction *instruction_binary(Builder *, Value *, Value *);
 
 #endif /* INCLUDE_GUARD_09C7931B_C0EE_4947_8485_E45CB53C286A */
--- src/ir/instruction.c
+++ src/ir/instruction.c
@@ -1,1 +1,18 @@
 #include "ir/instruction.h"
+
+#include "builder.h"
+#include "ir/register_type.h"
+#include "utility.h"
+
+struct struct_Instruction {
+  Register *dst;
+  Value *lhs, *rhs;
+};
+
+Instruction *instruction_binary(Builder *builder, Value *lhs, Value *rhs) {
+  Instruction *instr = UTILITY_MALLOC(Instruction);
+  instr->dst = builder_register(builder);
+  instr->lhs = lhs;
+  instr->rhs = rhs;
+  return instr;
+}
