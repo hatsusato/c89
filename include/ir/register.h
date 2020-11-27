04502d26b157e56957a594d3e496f5cdcc219140
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-27 17:37:07 +0900

    Add register_pool_{new,delete}

--- include/ir/register.h
+++ include/ir/register.h
@@ -1,3 +1,10 @@
 #ifndef INCLUDE_GUARD_CEF76A36_FC1B_4A04_BA5B_8E1A29C43165
 #define INCLUDE_GUARD_CEF76A36_FC1B_4A04_BA5B_8E1A29C43165
+
+#include "register_type.h"
+#include "vector.h"
+
+Vector *register_pool_new(void);
+void register_pool_delete(Vector *);
+
 #endif /* INCLUDE_GUARD_CEF76A36_FC1B_4A04_BA5B_8E1A29C43165 */
--- src/ir/register.c
+++ src/ir/register.c
@@ -1,5 +1,19 @@
 #include "ir/register.h"
 
+#include "utility.h"
+#include "vector.h"
+
 struct struct_Register {
   int id;
 };
+
+static void register_delete(Register *reg) {
+  UTILITY_FREE(reg);
+}
+
+Vector *register_pool_new(void) {
+  return vector_new((Destructor)register_delete);
+}
+void register_pool_delete(Vector *pool) {
+  vector_delete(pool);
+}
