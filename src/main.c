0a97753b9402b065098a4eddd0c8e237cb506bdd
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-27 14:22:10 +0900

    Use builder

--- src/main.c
+++ src/main.c
@@ -1,13 +1,21 @@
 #include "ast.h"
+#include "builder.h"
 #include "print.h"
 #include "scanner.h"
+
+static void build(Sexp *ast) {
+  Builder *builder = builder_new();
+  builder_build(builder, ast);
+  builder_delete(builder);
+}
 
 int main(void) {
   yyscan_t scanner = scanner_new();
   int ret = scanner_parse(scanner);
   if (0 == ret) {
-    Ast *ast = scanner_get(scanner);
-    print_ast(ast_get(ast));
+    Sexp *ast = ast_get(scanner_get(scanner));
+    print_ast(ast);
+    build(ast);
   }
   scanner_delete(scanner);
   return ret;
