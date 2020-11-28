28227154dfdb69d524ed76a6bae1eccc1da887fe
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-28 11:38:10 +0900

    Add value_set_id

--- include/ir/value.h
+++ include/ir/value.h
@@ -1,11 +1,13 @@
 #ifndef INCLUDE_GUARD_FE2AA6D9_B675_4B2E_839A_2B085264192A
 #define INCLUDE_GUARD_FE2AA6D9_B675_4B2E_839A_2B085264192A
 
+#include "ir/builder_type.h"
 #include "ir/value_kind.h"
 #include "ir/value_type.h"
 #include "sexp_type.h"
 
 ValueKind value_kind(Value *);
 Value *value_integer_constant(Sexp *);
+void value_set_id(Builder *, Value *);
 
 #endif /* INCLUDE_GUARD_FE2AA6D9_B675_4B2E_839A_2B085264192A */
--- src/ir/value.c
+++ src/ir/value.c
@@ -1,18 +1,21 @@
 #include "ir/value.h"
 
 #include "ast/ast_tag.h"
+#include "builder.h"
 #include "ir/value_kind.h"
 #include "sexp.h"
 #include "utility.h"
 
 struct struct_Value {
   ValueKind kind;
+  int id;
   const void *value;
 };
 
 static Value *value_new(ValueKind kind, const void *value) {
   Value *v = UTILITY_MALLOC(Value);
   v->kind = kind;
+  v->id = 0;
   v->value = value;
   return v;
 }
@@ -25,4 +28,7 @@ Value *value_integer_constant(Sexp *ast) {
   ast = sexp_at(ast, 1);
   assert(sexp_is_symbol(ast));
   return value_new(VALUE_INTEGER_CONSTANT, sexp_get_symbol(ast));
+}
+void value_set_id(Builder *builder, Value *value) {
+  value->id = builder_fresh_id(builder);
 }
