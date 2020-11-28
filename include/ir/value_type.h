7b1e6fd3faf8c52ecd957661f4c7d0668bc174b6
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-28 12:50:04 +0900

    Add ValueHeader

--- include/ir/value_type.h
+++ include/ir/value_type.h
@@ -1,7 +1,15 @@
 #ifndef INCLUDE_GUARD_78E99201_C3B9_4B05_88E4_2BD5F30C2F6C
 #define INCLUDE_GUARD_78E99201_C3B9_4B05_88E4_2BD5F30C2F6C
 
+#include "ir/value_kind.h"
+
 struct struct_Value;
 typedef struct struct_Value Value;
+
+struct struct_ValueHeader {
+  ValueKind kind;
+  int id;
+};
+typedef struct struct_ValueHeader ValueHeader;
 
 #endif /* INCLUDE_GUARD_78E99201_C3B9_4B05_88E4_2BD5F30C2F6C */
