d233d4b762dd0a4fd34407257d467a31c49c4070
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-28 11:46:45 +0900

    Add instruction_print

--- include/ir/instruction.h
+++ include/ir/instruction.h
@@ -5,5 +5,6 @@
 #include "ir/value_type.h"
 
 Value *instruction_binary(Builder *, Value *, Value *);
+void instruction_print(Value *);
 
 #endif /* INCLUDE_GUARD_09C7931B_C0EE_4947_8485_E45CB53C286A */
--- src/ir/instruction.c
+++ src/ir/instruction.c
@@ -1,6 +1,9 @@
 #include "ir/instruction.h"
 
+#include <stdio.h>
+
 #include "builder.h"
+#include "ir/value.h"
 #include "ir/value_kind.h"
 #include "utility.h"
 
@@ -19,4 +22,10 @@ Value *instruction_binary(Builder *builder, Value *lhs, Value *rhs) {
   instr->lhs = lhs;
   instr->rhs = rhs;
   return (Value *)instr;
+}
+void instruction_print(Value *value) {
+  Instruction *instr = (Instruction *)value;
+  assert(VALUE_INSTRUCTION == value_kind(value));
+  printf("  %%%d = add i32 %%%d, %%%d\n", value_get_id(instr),
+         value_get_id(instr->lhs), value_get_id(instr->rhs));
 }
