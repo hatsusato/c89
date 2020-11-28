89a941b3730e4c9d3eae1a1a27aad290a4257000
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-11-28 13:51:03 +0900

    Add pool_{new,delete}

--- include/ir/pool.h
+++ include/ir/pool.h
@@ -1,3 +1,9 @@
 #ifndef INCLUDE_GUARD_3328774C_9C13_4B31_8826_622558B21C09
 #define INCLUDE_GUARD_3328774C_9C13_4B31_8826_622558B21C09
+
+#include "ir/pool_type.h"
+
+Pool *pool_new(void);
+void pool_delete(Pool *);
+
 #endif /* INCLUDE_GUARD_3328774C_9C13_4B31_8826_622558B21C09 */
--- src/ir/pool.c
+++ src/ir/pool.c
@@ -1,7 +1,18 @@
 #include "ir/pool.h"
 
-#include "vector/vector_type.h"
+#include "utility.h"
+#include "vector.h"
 
 struct struct_Pool {
   Vector *pool;
 };
+
+Pool *pool_new(void) {
+  Pool *pool = UTILITY_MALLOC(Pool);
+  pool->pool = vector_new(utility_free);
+  return pool;
+}
+void pool_delete(Pool *pool) {
+  vector_delete(pool->pool);
+  UTILITY_FREE(pool);
+}
