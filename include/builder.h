67254ef1c1fe4975b88608a09a6bd209a2a1ada1
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-02 21:41:20 +0900

    Add builder_{new,delete}

--- include/builder.h
+++ include/builder.h
@@ -1,3 +1,9 @@
 #ifndef INCLUDE_GUARD_5D831CA9_8557_453C_9E51_10E9C0569E95
 #define INCLUDE_GUARD_5D831CA9_8557_453C_9E51_10E9C0569E95
+
+#include "builder_type.h"
+
+Builder *builder_new(void);
+void builder_delete(Builder *);
+
 #endif /* INCLUDE_GUARD_5D831CA9_8557_453C_9E51_10E9C0569E95 */
--- src/ir/builder.c
+++ src/ir/builder.c
@@ -1,1 +1,18 @@
 #include "builder.h"
+
+#include "ir/stack.h"
+#include "utility.h"
+
+struct struct_Builder {
+  Stack *stack;
+};
+
+Builder *builder_new(void) {
+  Builder *builder = UTILITY_MALLOC(Builder);
+  builder->stack = stack_new();
+  return builder;
+}
+void builder_delete(Builder *builder) {
+  stack_delete(builder->stack);
+  UTILITY_FREE(builder);
+}
