2e6e30bb7d59469988950e88659c7632a10043dd
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-02 15:51:25 +0900

    Remove builder_br_cond

--- src/ir/statement.c
+++ src/ir/statement.c
@@ -34,27 +34,30 @@ static void builder_new_integer_constant(Builder *builder,
   builder_stack_new_value(builder, VALUE_INTEGER_CONSTANT);
   builder_stack_set_symbol(builder, integer);
 }
-static void builder_br_cond(Builder *builder, Sexp *ast) {
-  builder_ast(builder, ast);
-  builder_new_integer_constant(builder, "0");
-  builder_instruction_icmp_ne(builder);
-  builder_stack_new_value(builder, VALUE_BLOCK);
-  builder_stack_new_value(builder, VALUE_BLOCK);
-  builder_instruction_br_cond(builder);
-  builder_stack_swap(builder);
-}
 void builder_selection_statement(Builder *builder, Sexp *ast) {
   assert(AST_SELECTION_STATEMENT == sexp_get_tag(ast));
   assert(AST_IF == sexp_get_number(sexp_at(ast, 1)));
   switch (sexp_length(ast)) {
   case 6:
-    builder_br_cond(builder, sexp_at(ast, 3));
+    builder_ast(builder, sexp_at(ast, 3));
+    builder_new_integer_constant(builder, "0");
+    builder_instruction_icmp_ne(builder);
+    builder_stack_new_value(builder, VALUE_BLOCK);
+    builder_stack_new_value(builder, VALUE_BLOCK);
+    builder_instruction_br_cond(builder);
+    builder_stack_swap(builder);
     builder_stack_pop_block(builder);
     builder_ast(builder, sexp_at(ast, 5));
     builder_stack_pop_block(builder);
     break;
   case 8:
-    builder_br_cond(builder, sexp_at(ast, 3));
+    builder_ast(builder, sexp_at(ast, 3));
+    builder_new_integer_constant(builder, "0");
+    builder_instruction_icmp_ne(builder);
+    builder_stack_new_value(builder, VALUE_BLOCK);
+    builder_stack_new_value(builder, VALUE_BLOCK);
+    builder_instruction_br_cond(builder);
+    builder_stack_swap(builder);
     builder_stack_pop_block(builder);
     builder_stack_new_value(builder, VALUE_BLOCK);
     builder_ast(builder, sexp_at(ast, 5));
