f97944faf09026de93a6aac7c1b3fb1522c3497f
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-02 14:44:36 +0900

    Add builder_instruction_unary

--- src/ir/instruction.c
+++ src/ir/instruction.c
@@ -3,17 +3,20 @@
 #include "ir/builder_impl.h"
 #include "ir/value_kind.h"
 
-void builder_instruction_ret(Builder *builder) {
-  Value *first = builder_stack_pop(builder);
-  builder_stack_new_value(builder, VALUE_INSTRUCTION_RET);
+static void builder_instruction_unary(Builder *builder, ValueKind kind,
+                                      Value *first) {
+  builder_stack_new_value(builder, kind);
   builder_stack_add(builder, first);
   builder_stack_register(builder);
 }
+
+void builder_instruction_ret(Builder *builder) {
+  Value *first = builder_stack_pop(builder);
+  builder_instruction_unary(builder, VALUE_INSTRUCTION_RET, first);
+}
 void builder_instruction_br(Builder *builder) {
   Value *first = builder_stack_top(builder);
-  builder_stack_new_value(builder, VALUE_INSTRUCTION_BR);
-  builder_stack_add(builder, first);
-  builder_stack_register(builder);
+  builder_instruction_unary(builder, VALUE_INSTRUCTION_BR, first);
 }
 void builder_instruction_br_cond(Builder *builder) {
   Value *third = builder_stack_pop(builder);
@@ -42,9 +45,7 @@ void builder_instruction_alloca(Builder *builder, const char *symbol) {
 }
 void builder_instruction_load(Builder *builder) {
   Value *first = builder_stack_pop(builder);
-  builder_stack_new_value(builder, VALUE_INSTRUCTION_LOAD);
-  builder_stack_add(builder, first);
-  builder_stack_register(builder);
+  builder_instruction_unary(builder, VALUE_INSTRUCTION_LOAD, first);
 }
 void builder_instruction_store(Builder *builder) {
   Value *second = builder_stack_pop(builder);
