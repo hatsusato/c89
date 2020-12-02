1d1260d7565663da53331c98bbbe37482585ccdf
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-02 22:01:48 +0900

    Add builder_ast

--- src/ir/builder.c
+++ src/ir/builder.c
@@ -1,8 +1,10 @@
 #include "builder.h"
 
+#include "ast/ast_tag.h"
 #include "ir/pool.h"
 #include "ir/stack.h"
 #include "ir/value_kind.h"
+#include "sexp.h"
 #include "utility.h"
 
 struct struct_Builder {
@@ -23,4 +25,23 @@ void builder_build(Builder *builder, Sexp *ast) {
   stack_build(stack, ast);
   stack_print(stack);
   stack_delete(stack);
+}
+static builder_ast_map(Builder *builder, Sexp *ast) {
+  for (ast = sexp_cdr(ast); sexp_is_pair(ast); ast = sexp_cdr(ast)) {
+    builder_ast(builder, sexp_car(ast));
+  }
+}
+void builder_ast(Builder *builder, Sexp *ast) {
+  switch (sexp_get_tag(ast)) {
+  case AST_TRANSLATION_UNIT:
+    builder_ast_map(builder, ast);
+    break;
+  case AST_EXTERNAL_DECLARATION:
+    break;
+  case AST_FUNCTION_DEFINITION:
+    break;
+  default:
+    assert(0);
+    break;
+  }
 }
