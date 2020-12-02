3995693978e19de21666ef5d8967d92bf83142bd
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-02 21:57:27 +0900

    Remove Builder.stack

--- src/ir/builder.c
+++ src/ir/builder.c
@@ -2,25 +2,25 @@
 
 #include "ir/pool.h"
 #include "ir/stack.h"
+#include "ir/value_kind.h"
 #include "utility.h"
 
 struct struct_Builder {
   Pool *pool;
-  Stack *stack;
 };
 
 Builder *builder_new(void) {
   Builder *builder = UTILITY_MALLOC(Builder);
   builder->pool = pool_new();
-  builder->stack = stack_new(builder->pool);
   return builder;
 }
 void builder_delete(Builder *builder) {
-  stack_delete(builder->stack);
   pool_delete(builder->pool);
   UTILITY_FREE(builder);
 }
 void builder_build(Builder *builder, Sexp *ast) {
-  stack_build(builder->stack, ast);
-  stack_print(builder->stack);
+  Stack *stack = stack_new(builder->pool);
+  stack_build(stack, ast);
+  stack_print(stack);
+  stack_delete(stack);
 }
