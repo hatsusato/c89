17f2f65123c99637d7934dc7864c7867c014203d
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-02 11:08:29 +0900

    Add value_prepend

--- src/ir/value.c
+++ src/ir/value.c
@@ -32,6 +32,21 @@ void value_delete(Value *value) {
 void value_insert(Value *value, Value *elem) {
   vector_push(value->vec, elem);
 }
+void value_prepend(Value *dst, const Value *src) {
+  Vector *tmp = vector_new(NULL);
+  ElemType *begin = vector_begin(src->vec);
+  ElemType *end = vector_end(src->vec);
+  while (begin < end) {
+    vector_push(tmp, *begin++);
+  }
+  begin = vector_begin(dst->vec);
+  end = vector_end(dst->vec);
+  while (begin < end) {
+    vector_push(tmp, *begin++);
+  }
+  UTILITY_SWAP(Vector *, dst->vec, tmp);
+  vector_delete(tmp);
+}
 void value_set_value(Value *value, const void *val) {
   value->value = val;
 }
