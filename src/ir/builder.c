924f47ceafb089a730c7406df4b40ffcfff575e7
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-02 22:21:59 +0900

    Add builder_function_definition

--- src/ir/builder.c
+++ src/ir/builder.c
@@ -27,26 +27,23 @@ void builder_build(Builder *builder, Sexp *ast) {
   builder_ast(builder, ast);
   value_pretty(builder->module);
 }
-static void builder_ast_map(Builder *builder, Sexp *ast) {
-  for (ast = sexp_cdr(ast); sexp_is_pair(ast); ast = sexp_cdr(ast)) {
-    builder_ast(builder, sexp_car(ast));
-  }
+static void builder_function_definition(Builder *builder, Sexp *ast) {
+  Stack *stack = stack_new(builder->pool);
+  assert(AST_FUNCTION_DEFINITION == sexp_get_tag(ast));
+  value_insert(builder->module, stack_build(stack, ast));
+  stack_delete(stack);
 }
 void builder_ast(Builder *builder, Sexp *ast) {
-  Stack *stack;
-  switch (sexp_get_tag(ast)) {
-  case AST_TRANSLATION_UNIT:
-    builder_ast_map(builder, ast);
-    break;
-  case AST_EXTERNAL_DECLARATION:
-    break;
-  case AST_FUNCTION_DEFINITION:
-    stack = stack_new(builder->pool);
-    value_insert(builder->module, stack_build(stack, ast));
-    stack_delete(stack);
-    break;
-  default:
-    assert(0);
-    break;
+  for (ast = sexp_cdr(ast); sexp_is_pair(ast); ast = sexp_cdr(ast)) {
+    switch (sexp_get_tag(sexp_car(ast))) {
+    case AST_EXTERNAL_DECLARATION:
+      break;
+    case AST_FUNCTION_DEFINITION:
+      builder_function_definition(builder, sexp_car(ast));
+      break;
+    default:
+      assert(0);
+      break;
+    }
   }
 }
