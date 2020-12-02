11302b1884fbc00b2bdc0b658d3af506e64a08d5
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-02 23:36:49 +0900

    Add function_{new,delete}

--- include/ir/function.h
+++ include/ir/function.h
@@ -1,3 +1,10 @@
 #ifndef INCLUDE_GUARD_B55311C7_57F5_41B3_92DD_0759ECC346E7
 #define INCLUDE_GUARD_B55311C7_57F5_41B3_92DD_0759ECC346E7
+
+#include "ir/function_type.h"
+#include "ir/pool.h"
+
+Function *function_new(Pool *);
+void function_delete(Function *);
+
 #endif /* INCLUDE_GUARD_B55311C7_57F5_41B3_92DD_0759ECC346E7 */
--- src/ir/function.c
+++ src/ir/function.c
@@ -1,7 +1,22 @@
 #include "ir/function.h"
 
+#include "ir/pool.h"
 #include "ir/value.h"
+#include "ir/value_kind.h"
+#include "utility.h"
 
 struct struct_Function {
-  Value* func;
+  Value *func, *allocs;
+  Value *current, *next, *ret;
 };
+
+Function *function_new(Pool *pool) {
+  Function *func = UTILITY_MALLOC(Function);
+  func->func = pool_alloc(pool, VALUE_FUNCTION);
+  func->allocs = pool_alloc(pool, VALUE_BLOCK);
+  func->current = func->next = func->ret = NULL;
+  return func;
+}
+void function_delete(Function *func) {
+  UTILITY_FREE(func);
+}
