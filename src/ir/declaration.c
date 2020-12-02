287f59a66671d3f63f5bb947f457efce80f3490a
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-02 10:18:27 +0900

    Add builder_init_declarator

--- src/ir/declaration.c
+++ src/ir/declaration.c
@@ -16,7 +16,6 @@ static void builder_direct_declarator(Builder *builder, Sexp *ast) {
     builder_stack_new_value(builder, VALUE_INSTRUCTION_ALLOCA);
     builder_stack_init(builder, ast);
     builder_stack_register(builder);
-    builder_stack_pop(builder);
     break;
   case 4:
     ast = sexp_at(ast, 2);
@@ -37,12 +36,23 @@ static void builder_declarator(Builder *builder, Sexp *ast) {
   ast = sexp_at(ast, sexp_length(ast) - 1);
   builder_direct_declarator(builder, ast);
 }
+static void builder_init_declarator(Builder *builder, Sexp *ast) {
+  assert(AST_INIT_DECLARATOR == sexp_get_tag(ast));
+  builder_declarator(builder, sexp_at(ast, 1));
+  if (4 == sexp_length(ast)) {
+    builder_stack_new_value(builder, VALUE_INSTRUCTION_STORE);
+    builder_ast(builder, sexp_at(ast, 3));
+    builder_stack_pop_insert(builder);
+    builder_stack_swap(builder);
+    builder_stack_pop_insert(builder);
+    builder_stack_register(builder);
+  }
+  builder_stack_pop(builder);
+}
 void builder_declaration(Builder *builder, Sexp *ast) {
   assert(AST_DECLARATION == sexp_get_tag(ast));
   ast = sexp_at(ast, 2);
   assert(AST_INIT_DECLARATOR_LIST == sexp_get_tag(ast));
   ast = sexp_at(ast, 1);
-  assert(AST_INIT_DECLARATOR == sexp_get_tag(ast));
-  ast = sexp_at(ast, 1);
-  builder_declarator(builder, ast);
+  builder_init_declarator(builder, ast);
 }
