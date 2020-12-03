7ee0982b61d25313514c70406dc39a7e9c395345
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-03 22:21:05 +0900

    Rename builder_ast

--- include/builder.h
+++ include/builder.h
@@ -7,6 +7,5 @@
 Builder *builder_new(void);
 void builder_delete(Builder *);
 void builder_build(Builder *, Sexp *);
-void builder_ast(Builder *, Sexp *);
 
 #endif /* INCLUDE_GUARD_5D831CA9_8557_453C_9E51_10E9C0569E95 */
--- src/ir/builder.c
+++ src/ir/builder.c
@@ -13,27 +13,13 @@ struct struct_Builder {
   Value *module;
 };
 
-Builder *builder_new(void) {
-  Builder *builder = UTILITY_MALLOC(Builder);
-  builder->pool = pool_new();
-  builder->module = pool_alloc(builder->pool, VALUE_MODULE);
-  return builder;
-}
-void builder_delete(Builder *builder) {
-  pool_delete(builder->pool);
-  UTILITY_FREE(builder);
-}
-void builder_build(Builder *builder, Sexp *ast) {
-  builder_ast(builder, ast);
-  value_pretty(builder->module);
-}
 static void builder_function_definition(Builder *builder, Sexp *ast) {
   Stack *stack = stack_new(builder->pool);
   assert(AST_FUNCTION_DEFINITION == sexp_get_tag(ast));
   value_insert(builder->module, stack_build(stack, ast));
   stack_delete(stack);
 }
-void builder_ast(Builder *builder, Sexp *ast) {
+static void builder_translation_unit(Builder *builder, Sexp *ast) {
   for (ast = sexp_cdr(ast); sexp_is_pair(ast); ast = sexp_cdr(ast)) {
     switch (sexp_get_tag(sexp_car(ast))) {
     case AST_EXTERNAL_DECLARATION:
@@ -46,4 +32,19 @@ void builder_ast(Builder *builder, Sexp *ast) {
       break;
     }
   }
+}
+
+Builder *builder_new(void) {
+  Builder *builder = UTILITY_MALLOC(Builder);
+  builder->pool = pool_new();
+  builder->module = pool_alloc(builder->pool, VALUE_MODULE);
+  return builder;
+}
+void builder_delete(Builder *builder) {
+  pool_delete(builder->pool);
+  UTILITY_FREE(builder);
+}
+void builder_build(Builder *builder, Sexp *ast) {
+  builder_translation_unit(builder, ast);
+  value_pretty(builder->module);
 }
