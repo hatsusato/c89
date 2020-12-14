062bd6ff092b609fe82f2c08c1499ef15d4d9b4d
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-14 14:28:53 +0900

    Add stack_identifier_store

--- include/ir/lexical.h
+++ include/ir/lexical.h
@@ -6,6 +6,7 @@
 
 void stack_identifier(Stack *, Sexp *);
 void stack_identifier_alloca(Stack *, Sexp *);
+void stack_identifier_store(Stack *, Sexp *);
 void stack_integer_constant(Stack *, Sexp *);
 
 #endif /* INCLUDE_GUARD_E38EAC74_338A_4A0A_A889_55ABA2B7FBE8 */
--- src/ir/expression.c
+++ src/ir/expression.c
@@ -17,17 +17,10 @@ void stack_additive_expression(Stack *stack, Sexp *ast) {
   }
 }
 void stack_assignment_expression(Stack *stack, Sexp *ast) {
-  Sexp *lhs, *op, *rhs;
   assert(AST_ASSIGNMENT_EXPRESSION == sexp_get_tag(ast));
-  lhs = sexp_at(ast, 1);
-  op = sexp_at(ast, 2);
-  rhs = sexp_at(ast, 3);
-  assert(AST_IDENTIFIER == sexp_get_tag(lhs));
-  assert(sexp_is_number(op) && AST_ASSIGN == sexp_get_number(op));
-  lhs = sexp_at(lhs, 1);
-  assert(sexp_is_symbol(lhs));
-  stack_ast(stack, rhs);
-  stack_store_to_symbol(stack, sexp_get_symbol(lhs));
+  assert(AST_ASSIGN == sexp_get_tag(sexp_at(ast, 2)));
+  stack_ast(stack, sexp_at(ast, 3));
+  stack_identifier_store(stack, sexp_at(ast, 1));
 }
 void stack_constant_expression(Stack *stack, Sexp *ast) {
   assert(AST_CONSTANT_EXPRESSION == sexp_get_tag(ast));
