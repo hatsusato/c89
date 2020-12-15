6cd204e734681053d5fd703b4b842183cdd330b6
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-15 18:08:17 +0900

    Remove stack_identifier_store

--- src/ir/expression.c
+++ src/ir/expression.c
@@ -24,10 +24,15 @@ void stack_additive_expression(Stack *stack, Sexp *ast) {
   }
 }
 void stack_assignment_expression(Stack *stack, Sexp *ast) {
+  const char *symbol;
+  Value *lhs, *rhs;
   assert(AST_ASSIGNMENT_EXPRESSION == sexp_get_tag(ast));
   assert(AST_ASSIGN == sexp_get_tag(sexp_at(ast, 2)));
+  symbol = stack_identifier_symbol(sexp_at(ast, 1));
+  lhs = stack_find_alloca(stack, symbol);
   stack_ast(stack, sexp_at(ast, 3));
-  stack_identifier_store(stack, sexp_at(ast, 1));
+  rhs = stack_get_prev(stack);
+  stack_instruction_store(stack, rhs, lhs);
 }
 void stack_constant_expression(Stack *stack, Sexp *ast) {
   assert(AST_CONSTANT_EXPRESSION == sexp_get_tag(ast));
--- src/ir/lexical.c
+++ src/ir/lexical.c
@@ -14,10 +14,6 @@ void stack_identifier(Stack *stack, Sexp *ast) {
 void stack_identifier_alloca(Stack *stack, Sexp *ast) {
   stack_alloca(stack, stack_identifier_symbol(ast));
 }
-void stack_identifier_store(Stack *stack, Sexp *ast) {
-  Value *src = stack_get_prev(stack);
-  stack_store_to_symbol(stack, src, stack_identifier_symbol(ast));
-}
 void stack_integer_constant(Stack *stack, Sexp *ast) {
   Value *value;
   assert(AST_INTEGER_CONSTANT == sexp_get_tag(ast));
