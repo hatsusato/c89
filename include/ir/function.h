1cd5b9865a41fc96627db7cd327bdf53d854d82b
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-16 17:56:24 +0900

    Add function_{new,delete}

--- include/ir/function.h
+++ include/ir/function.h
@@ -3,4 +3,7 @@
 
 #include "ir/function_type.h"
 
+Function *function_new(void);
+void function_delete(Function *);
+
 #endif /* INCLUDE_GUARD_9CEF8276_25CA_4457_A042_1492E261359B */
--- src/ir/function.c
+++ src/ir/function.c
@@ -1,1 +1,26 @@
 #include "ir/function.h"
+
+#include "ir/register.h"
+#include "ir/value_kind.h"
+#include "utility.h"
+#include "vector.h"
+
+struct struct_Function {
+  ValueKind kind;
+  Register reg;
+  Vector *vec;
+  const void *value;
+};
+
+Function *function_new(void) {
+  Function *func = UTILITY_MALLOC(Function);
+  func->kind = VALUE_FUNCTION;
+  register_init(&func->reg);
+  func->vec = vector_new(NULL);
+  func->value = NULL;
+  return func;
+}
+void function_delete(Function *func) {
+  vector_delete(func->vec);
+  UTILITY_FREE(func);
+}
