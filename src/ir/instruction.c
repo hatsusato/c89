a1a22ec5760d2fa97c354cd114ec0a4c675e3434
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-16 23:39:05 +0900

    Update return type of instruction_new

--- src/ir/instruction.c
+++ src/ir/instruction.c
@@ -12,39 +12,39 @@ struct struct_Instruction {
   const void *value;
 };
 
-static Value *instruction_new(Stack *stack, ValueKind kind) {
+static Instruction *instruction_new(Stack *stack, ValueKind kind) {
   Block *current = stack_get_next(stack, STACK_NEXT_CURRENT);
   Instruction *instr = UTILITY_MALLOC(Instruction);
   instr->kind = kind;
   register_init(&instr->reg);
   instr->vec = vector_new(NULL);
   instr->value = NULL;
-  block_insert(current, instr);
+  block_insert(current, value_of(instr));
   return instr;
 }
 
 void stack_instruction_ret(Stack *stack, Value *expr) {
-  Value *instr = instruction_new(stack, VALUE_INSTRUCTION_RET);
-  value_insert(instr, expr);
+  Instruction *instr = instruction_new(stack, VALUE_INSTRUCTION_RET);
+  value_insert(value_of(instr), expr);
 }
 void stack_instruction_br(Stack *stack, Block *label) {
   assert(label);
   if (!stack_last_terminator(stack)) {
-    Value *instr = instruction_new(stack, VALUE_INSTRUCTION_BR);
-    value_insert_block(instr, label);
+    Instruction *instr = instruction_new(stack, VALUE_INSTRUCTION_BR);
+    value_insert_block(value_of(instr), label);
   }
 }
 void stack_instruction_br_cond(Stack *stack, Value *expr, Block *then_label,
                                Block *else_label) {
-  Value *instr = instruction_new(stack, VALUE_INSTRUCTION_BR_COND);
-  value_insert(instr, expr);
-  value_insert_block(instr, then_label);
-  value_insert_block(instr, else_label);
+  Instruction *instr = instruction_new(stack, VALUE_INSTRUCTION_BR_COND);
+  value_insert(value_of(instr), expr);
+  value_insert_block(value_of(instr), then_label);
+  value_insert_block(value_of(instr), else_label);
 }
 Value *stack_instruction_switch(Stack *stack, Value *expr) {
-  Value *instr = instruction_new(stack, VALUE_INSTRUCTION_SWITCH);
-  value_insert(instr, expr);
-  return instr;
+  Instruction *instr = instruction_new(stack, VALUE_INSTRUCTION_SWITCH);
+  value_insert(value_of(instr), expr);
+  return value_of(instr);
 }
 void stack_instruction_switch_finish(Stack *stack, Value *instr) {
   Block *default_label = stack_get_next(stack, STACK_NEXT_DEFAULT);
@@ -65,31 +65,31 @@ void stack_instruction_switch_case(Stack *stack, Value *constant,
   block_insert(cases, value_of(label));
 }
 Value *stack_instruction_add(Stack *stack, Value *lhs, Value *rhs) {
-  Value *instr = instruction_new(stack, VALUE_INSTRUCTION_ADD);
-  value_insert(instr, lhs);
-  value_insert(instr, rhs);
-  return instr;
+  Instruction *instr = instruction_new(stack, VALUE_INSTRUCTION_ADD);
+  value_insert(value_of(instr), lhs);
+  value_insert(value_of(instr), rhs);
+  return value_of(instr);
 }
 Value *stack_instruction_sub(Stack *stack, Value *lhs, Value *rhs) {
-  Value *instr = instruction_new(stack, VALUE_INSTRUCTION_SUB);
-  value_insert(instr, lhs);
-  value_insert(instr, rhs);
-  return instr;
+  Instruction *instr = instruction_new(stack, VALUE_INSTRUCTION_SUB);
+  value_insert(value_of(instr), lhs);
+  value_insert(value_of(instr), rhs);
+  return value_of(instr);
 }
 Value *stack_instruction_load(Stack *stack, Value *src) {
-  Value *instr = instruction_new(stack, VALUE_INSTRUCTION_LOAD);
-  value_insert(instr, src);
-  return instr;
+  Instruction *instr = instruction_new(stack, VALUE_INSTRUCTION_LOAD);
+  value_insert(value_of(instr), src);
+  return value_of(instr);
 }
 Value *stack_instruction_store(Stack *stack, Value *src, Value *dst) {
-  Value *instr = instruction_new(stack, VALUE_INSTRUCTION_STORE);
-  value_insert(instr, src);
-  value_insert(instr, dst);
-  return instr;
+  Instruction *instr = instruction_new(stack, VALUE_INSTRUCTION_STORE);
+  value_insert(value_of(instr), src);
+  value_insert(value_of(instr), dst);
+  return value_of(instr);
 }
 Value *stack_instruction_icmp_ne(Stack *stack, Value *lhs, Value *rhs) {
-  Value *instr = instruction_new(stack, VALUE_INSTRUCTION_ICMP_NE);
-  value_insert(instr, lhs);
-  value_insert(instr, rhs);
-  return instr;
+  Instruction *instr = instruction_new(stack, VALUE_INSTRUCTION_ICMP_NE);
+  value_insert(value_of(instr), lhs);
+  value_insert(value_of(instr), rhs);
+  return value_of(instr);
 }
