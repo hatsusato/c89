223f7eb91eb112e2da2f3afb109bccc6932cb8cd
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-18 14:50:13 +0900

    Add constant_{integer,new,delete}

--- include/ir/constant.h
+++ include/ir/constant.h
@@ -3,4 +3,7 @@
 
 #include "ir/constant_type.h"
 
+Constant *constant_integer(const char *);
+void constant_delete(Constant *);
+
 #endif /* INCLUDE_GUARD_847C4618_BC3A_4B3B_AB91_0840BEF9BDE2 */
--- src/ir/constant.c
+++ src/ir/constant.c
@@ -1,1 +1,32 @@
 #include "ir/constant.h"
+
+#include "ir/register.h"
+#include "ir/value_kind.h"
+#include "utility.h"
+#include "vector.h"
+
+struct struct_Constant {
+  ValueKind kind;
+  Register reg;
+  Vector *vec;
+  const void *value;
+};
+
+static Constant *constant_new(void) {
+  Constant *constant = UTILITY_MALLOC(Constant);
+  constant->kind = VALUE_INTEGER_CONSTANT;
+  register_init(&constant->reg);
+  constant->value = NULL;
+  constant->vec = vector_new(NULL);
+  return constant;
+}
+
+Constant *constant_integer(const char *integer) {
+  Constant *constant = constant_new();
+  constant->value = integer;
+  return constant;
+}
+void constant_delete(Constant *constant) {
+  vector_delete(constant->vec);
+  UTILITY_FREE(constant);
+}
