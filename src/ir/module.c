9cb6e7ffe55d947fad9dcaeb84806c7982cde9dd
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-20 14:30:02 +0900

    Reduce Module.{reg,value}

--- src/ir/module.c
+++ src/ir/module.c
@@ -3,24 +3,19 @@
 #include <stdio.h>
 
 #include "ir/function.h"
-#include "ir/register.h"
 #include "ir/value_kind.h"
 #include "utility.h"
 #include "vector.h"
 
 struct struct_Module {
   ValueKind kind;
-  Register reg;
   Vector *vec;
-  const void *value;
 };
 
 Module *module_new(void) {
   Module *module = UTILITY_MALLOC(Module);
   module->kind = VALUE_MODULE;
-  register_init(&module->reg);
   module->vec = vector_new(NULL);
-  module->value = NULL;
   return module;
 }
 void module_delete(Module *module) {
