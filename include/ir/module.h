916ef897a3907965bcf7e275913dc76da4d73664
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-20 19:58:17 +0900

    Add module_new_{function,block,instruction,integer}

--- include/ir/module.h
+++ include/ir/module.h
@@ -1,11 +1,19 @@
 #ifndef INCLUDE_GUARD_2066624D_9BD2_42A1_8E82_3B559F049976
 #define INCLUDE_GUARD_2066624D_9BD2_42A1_8E82_3B559F049976
 
+#include "ir/block_type.h"
+#include "ir/constant_type.h"
 #include "ir/function_type.h"
+#include "ir/instruction_kind.h"
+#include "ir/instruction_type.h"
 #include "ir/module_type.h"
 
 Module *module_new(void);
 void module_delete(Module *);
+Function *module_new_function(Module *);
+Block *module_new_block(Module *);
+Instruction *module_new_instruction(Module *, InstructionKind);
+Constant *module_new_integer(Module *, const char *);
 void module_insert(Module *, Function *);
 void module_pretty(Module *);
 void *module_get(Module *);
--- src/ir/module.c
+++ src/ir/module.c
@@ -2,7 +2,10 @@
 
 #include <stdio.h>
 
+#include "ir/block.h"
+#include "ir/constant.h"
 #include "ir/function.h"
+#include "ir/instruction.h"
 #include "ir/pool.h"
 #include "utility.h"
 #include "vector.h"
@@ -22,6 +25,26 @@ void module_delete(Module *module) {
   vector_delete(module->vec);
   pool_delete(module->pool);
   UTILITY_FREE(module);
+}
+Function *module_new_function(Module *module) {
+  Function *func = function_new();
+  pool_insert_function(module->pool, func);
+  return func;
+}
+Block *module_new_block(Module *module) {
+  Block *block = block_new();
+  pool_insert_block(module->pool, block);
+  return block;
+}
+Instruction *module_new_instruction(Module *module, InstructionKind kind) {
+  Instruction *instr = instruction_new(kind);
+  pool_insert_instruction(module->pool, instr);
+  return instr;
+}
+Constant *module_new_integer(Module *module, const char *integer) {
+  Constant *constant = constant_integer(integer);
+  pool_insert_constant(module->pool, constant);
+  return constant;
 }
 void module_insert(Module *module, Function *func) {
   vector_push(module->vec, func);
