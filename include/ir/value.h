29c184588569b158fd5584198c75a7f1d48df3d1
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-20 17:55:37 +0900

    Add value_as_*

--- include/ir/value.h
+++ include/ir/value.h
@@ -1,8 +1,16 @@
 #ifndef INCLUDE_GUARD_FE2AA6D9_B675_4B2E_839A_2B085264192A
 #define INCLUDE_GUARD_FE2AA6D9_B675_4B2E_839A_2B085264192A
 
+#include "ir/block_type.h"
+#include "ir/constant_type.h"
+#include "ir/function_type.h"
+#include "ir/instruction_type.h"
 #include "ir/value_type.h"
 
+Function *value_as_function(Value *);
+Block *value_as_block(Value *);
+Instruction *value_as_instruction(Value *);
+Constant *value_as_constant(Value *);
 void value_print(Value *);
 
 #endif /* INCLUDE_GUARD_FE2AA6D9_B675_4B2E_839A_2B085264192A */
--- src/ir/value.c
+++ src/ir/value.c
@@ -10,16 +10,32 @@ struct struct_Value {
   ValueKind kind;
 };
 
+Function *value_as_function(Value *value) {
+  assert(VALUE_FUNCTION == value->kind);
+  return (Function *)value;
+}
+Block *value_as_block(Value *value) {
+  assert(VALUE_BLOCK == value->kind);
+  return (Block *)value;
+}
+Instruction *value_as_instruction(Value *value) {
+  assert(VALUE_INSTRUCTION == value->kind);
+  return (Instruction *)value;
+}
+Constant *value_as_constant(Value *value) {
+  assert(VALUE_CONSTANT == value->kind);
+  return (Constant *)value;
+}
 void value_print(Value *value) {
   switch (value->kind) {
   case VALUE_BLOCK:
-    block_print((Block *)value);
+    block_print(value_as_block(value));
     break;
   case VALUE_INSTRUCTION:
-    instruction_print((Instruction *)value);
+    instruction_print(value_as_instruction(value));
     break;
   case VALUE_CONSTANT:
-    constant_print((Constant *)value);
+    constant_print(value_as_constant(value));
     break;
   default:
     assert(0);
