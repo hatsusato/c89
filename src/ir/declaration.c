fa09be91db259a6ceb705fab4b09e536ca608767
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-22 03:08:47 +0900

    Refactor in declaration

--- src/ir/declaration.c
+++ src/ir/declaration.c
@@ -13,16 +13,17 @@ Value *builder_declaration(Builder *builder, Sexp *ast) {
   return builder_ast(builder, sexp_at(ast, 2));
 }
 Value *builder_init_declarator(Builder *builder, Sexp *ast) {
+  Value *src, *dst;
+  Instruction *instr;
   assert(AST_INIT_DECLARATOR == sexp_get_tag(ast));
   switch (sexp_length(ast)) {
   case 2:
     return builder_declarator(builder, sexp_at(ast, 1));
-  case 4: {
-    Value *src = builder_ast(builder, sexp_at(ast, 3));
-    Value *dst = builder_declarator(builder, sexp_at(ast, 1));
-    Instruction *instr = builder_instruction_store(builder, src, dst);
+  case 4:
+    src = builder_ast(builder, sexp_at(ast, 3));
+    dst = builder_declarator(builder, sexp_at(ast, 1));
+    instr = builder_instruction_store(builder, src, dst);
     return instruction_as_value(instr);
-  }
   default:
     assert(0);
     return NULL;
@@ -41,13 +42,14 @@ Value *builder_declarator(Builder *builder, Sexp *ast) {
   }
 }
 Value *builder_direct_declarator(Builder *builder, Sexp *ast) {
+  const char *symbol;
+  Instruction *instr;
   assert(AST_DIRECT_DECLARATOR == sexp_get_tag(ast));
   switch (sexp_length(ast)) {
-  case 2: {
-    const char *symbol = builder_identifier_symbol(sexp_at(ast, 1));
-    Instruction *instr = builder_instruction_alloca(builder, symbol);
+  case 2:
+    symbol = builder_identifier_symbol(sexp_at(ast, 1));
+    instr = builder_instruction_alloca(builder, symbol);
     return instruction_as_value(instr);
-  }
   case 4:
     return builder_declarator(builder, sexp_at(ast, 2));
   case 5:
