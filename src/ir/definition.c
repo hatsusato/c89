53fdf2f2a2ee684f8dd23f9e5806e53a0b2f1f8d
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-22 03:11:12 +0900

    Add builder_function_return

--- src/ir/definition.c
+++ src/ir/definition.c
@@ -10,19 +10,21 @@
 #include "sexp.h"
 #include "utility.h"
 
+static void builder_function_return(Builder *builder) {
+  Value *expr = builder_find_alloca(builder, "$retval");
+  Instruction *instr = builder_instruction_load(builder, expr);
+  builder_instruction_ret(builder, instruction_as_value(instr));
+}
+
 Value *builder_function_definition(Builder *builder, Sexp *ast) {
   Block *ret = builder_function_init(builder, ast);
   assert(AST_FUNCTION_DEFINITION == sexp_get_tag(ast));
   assert(5 == sexp_length(ast));
   builder_ast(builder, sexp_at(ast, 4));
   if (ret) {
-    Value *expr;
-    Instruction *instr;
     builder_instruction_br(builder, ret);
     builder_jump_block(builder, ret);
-    expr = builder_find_alloca(builder, "$retval");
-    instr = builder_instruction_load(builder, expr);
-    builder_instruction_ret(builder, instruction_as_value(instr));
+    builder_function_return(builder);
   }
   builder_function_finish(builder);
   return NULL;
