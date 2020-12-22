a344ad497720c025b68111a0fd2b0433caba1eec
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-22 15:58:59 +0900

    Update header

--- include/set/sort.h
+++ include/set/sort.h
@@ -1,7 +1,7 @@
 #ifndef INCLUDE_GUARD_0C8B0910_CE56_48A5_8BFB_257BD9BDAB76
 #define INCLUDE_GUARD_0C8B0910_CE56_48A5_8BFB_257BD9BDAB76
 
-#include "compare.h"
+#include "compare/type.h"
 #include "types.h"
 
 void insert_sort(ElemType *, ElemType *, Compare *);
