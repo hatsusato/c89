ba8c93dcd862f4a5b245fc42c0c95a9acb5e4313
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-24 23:27:10 +0900

    Fix builder_{,find_}alloca

--- src/builder/builder.c
+++ src/builder/builder.c
@@ -93,19 +93,10 @@ Block *builder_label(Builder *builder, const char *label) {
 }
 void builder_alloca(Builder *builder, const char *symbol, Instruction *instr) {
   UTILITY_ASSERT(INSTRUCTION_ALLOCA == instruction_kind(instr));
-  if (symbol && '$' == *symbol) {
-    table_builtin_insert(builder->table, symbol, instr);
-  } else {
-    table_insert(builder->table, symbol, instr);
-  }
+  table_insert(builder->table, symbol, instr);
 }
 Instruction *builder_find_alloca(Builder *builder, const char *symbol) {
-  Instruction *instr;
-  if (symbol && '$' == *symbol) {
-    instr = table_builtin_find(builder->table, symbol);
-  } else {
-    instr = table_find(builder->table, symbol);
-  }
+  Instruction *instr = table_find(builder->table, symbol);
   UTILITY_ASSERT(INSTRUCTION_ALLOCA == instruction_kind(instr));
   return instr;
 }
