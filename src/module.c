731c593f2a7b7c2fb8dcf6528a32423bb8c23d12
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-26 01:04:30 +0900

    Add module_pretty_prior

--- src/module.c
+++ src/module.c
@@ -41,26 +41,24 @@ static void module_delete_global(ElemType global) {
 static void module_delete_function(ElemType function) {
   function_delete(function);
 }
+static void module_pretty_prior(Module *module) {
+  ElemType *begin = vector_begin(module->global);
+  ElemType *end = vector_end(module->global);
+  while (begin < end) {
+    Global *global = *begin++;
+    if (global_get_prior(global)) {
+      global_pretty(global);
+    }
+  }
+}
 static void module_pretty_global(Module *module) {
   ElemType *begin = vector_begin(module->global);
   ElemType *end = vector_end(module->global);
-  if (begin != end) {
-    Vector *decl = vector_new(NULL);
-    printf("\n");
-    while (begin < end) {
-      Global *global = *begin++;
-      if (global_get_init(global)) {
-        global_pretty(global);
-      } else {
-        vector_push(decl, global);
-      }
+  while (begin < end) {
+    Global *global = *begin++;
+    if (!global_get_prior(global)) {
+      global_pretty(global);
     }
-    begin = vector_begin(decl);
-    end = vector_end(decl);
-    while (begin < end) {
-      global_pretty(*begin++);
-    }
-    vector_delete(decl);
   }
 }
 static void module_pretty_function(Module *module) {
@@ -117,6 +115,10 @@ void module_build(Module *module, Sexp *ast) {
 }
 void module_pretty(Module *module) {
   printf("target triple = \"x86_64-unknown-linux-gnu\"\n");
-  module_pretty_global(module);
+  if (!vector_empty(module->global)) {
+    printf("\n");
+    module_pretty_prior(module);
+    module_pretty_global(module);
+  }
   module_pretty_function(module);
 }
