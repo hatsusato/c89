0f08f7f21b3a3dc218af01342d3670c28c4d2d0c
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-25 21:38:20 +0900

    Add builder_external_*

--- src/builder/definition.c
+++ src/builder/definition.c
@@ -2,11 +2,33 @@
 
 #include "ast/tag.h"
 #include "builder.h"
+#include "global.h"
 #include "instruction.h"
 #include "sexp.h"
 #include "utility.h"
 #include "value.h"
 
+static Global *builder_external_identifier(Builder *builder, Sexp *ast) {
+  UTILITY_ASSERT(AST_IDENTIFIER == sexp_get_tag(ast));
+  ast = sexp_at(ast, 1);
+  UTILITY_ASSERT(sexp_is_symbol(ast));
+  return builder_new_global(builder, sexp_get_symbol(ast));
+}
+static Global *builder_exteral_direct_declarator(Builder *builder, Sexp *ast) {
+  UTILITY_ASSERT(AST_DIRECT_DECLARATOR == sexp_get_tag(ast));
+  UTILITY_ASSERT(2 == sexp_length(ast));
+  return builder_external_identifier(builder, sexp_at(ast, 1));
+}
+static Global *builder_external_declarator(Builder *builder, Sexp *ast) {
+  UTILITY_ASSERT(AST_DECLARATOR == sexp_get_tag(ast));
+  UTILITY_ASSERT(2 == sexp_length(ast));
+  return builder_exteral_direct_declarator(builder, sexp_at(ast, 1));
+}
+static void builder_external_init_declarator(Builder *builder, Sexp *ast) {
+  UTILITY_ASSERT(AST_INIT_DECLARATOR == sexp_get_tag(ast));
+  UTILITY_ASSERT(2 == sexp_length(ast));
+  builder_external_declarator(builder, sexp_at(ast, 1));
+}
 static void builder_function_return(Builder *builder) {
   Instruction *retval = builder_get_retval(builder);
   Instruction *instr =
@@ -21,6 +43,7 @@ Value *builder_external_declaration(Builder *builder, Sexp *ast) {
   ast = sexp_at(ast, 2);
   UTILITY_ASSERT(AST_INIT_DECLARATOR_LIST == sexp_get_tag(ast));
   for (ast = sexp_cdr(ast); sexp_is_pair(ast); ast = sexp_cdr(ast)) {
+    builder_external_init_declarator(builder, sexp_car(ast));
   }
   return NULL;
 }
