40e62cfb058ce343cbb47357ef40c1020eef3867
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-26 02:07:31 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,21 +5,24 @@ set -eu
 cd $(dirname "$BASH_SOURCE")
 make &>/dev/null
 
-comp=./compile.sh
-emit=./emit-llvm.sh
-target=
-diff_flag=
-
 error() {
   echo "ERROR: $*"
   exit 1
 }
-
-test -x "$comp" || error "$comp not found"
-test -x "$emit" || error "$emit not found"
-
+comp() {
+  local sh=./compile.sh
+  test -x "$sh" || error "$sh not found"
+  "$sh" -S "$1"
+}
+emit() {
+  local sh=./emit-llvm.sh
+  test -x "$sh" || error "$sh not found"
+  "$sh" "$1"
+}
 compare() {
-  diff "$@" <("$emit" "$target") <("$comp" -S "$target")
+  local target=$1
+  shift
+  diff "$@" <(emit "$target") <(comp "$target")
 }
 check() {
   local e=$'\e'
@@ -27,27 +30,31 @@ check() {
   local green=$e[32m
   local red=$e[31m
   local normal=$e[0m
-  "$comp" -S "$target" >/dev/null || exit
-  if compare >/dev/null; then
-    echo "$bold${green}OK$normal: $1"
+  local name=${1##*/}
+  name=${name%.c}
+  comp "$1" >/dev/null || exit
+  if compare "$1" >/dev/null; then
+    echo "$bold${green}OK$normal: $name"
   else
-    echo "$bold${red}NG$normal: $1"
-    test "$diff_flag" && compare -y || :
+    echo "$bold${red}NG$normal: $name"
+    compare "$1" -y
     return 1
   fi
 }
 
+files=()
 if (($#)); then
-  diff_flag=on
+  for i in "$@"; do
+    files+=("test/$i.c")
+  done
 else
-  for f in $(ls test/); do
-    set -- "$@" "${f%.c}"
+  for f in $(find test/ -name '*.c' | sort); do
+    files+=("$f")
   done
 fi
 count=0
-for i in "$@"; do
-  target=test/$i.c
-  test -f "$target" || error "$target not found"
-  check "$i" || ((++count))
+for f in "${files[@]}"; do
+  test -f "$f" || error "$f not found"
+  check "$f" || ((++count))
 done
 exit $count
