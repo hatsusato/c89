29a145305aa27e938da53d230eb0ffa63595a54b
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-31 01:49:44 +0900

    Add statement/common

---
+++ src/builder/statement/common.c
@@ -0,0 +1,17 @@
+#include "common.h"
+
+#include "../builder.h"
+#include "../constant.h"
+#include "../instruction.h"
+
+void builder_guard_statement(Builder *builder, Sexp *expr, Block *then_block,
+                             Block *else_block) {
+  Value *lhs, *rhs, *icmp;
+  builder_ast(builder, expr);
+  lhs = builder_get_value(builder);
+  builder_new_integer(builder, "0");
+  rhs = builder_get_value(builder);
+  builder_instruction_icmp_ne(builder, lhs, rhs);
+  icmp = builder_get_value(builder);
+  builder_instruction_br_cond(builder, icmp, then_block, else_block);
+}
---
+++ src/builder/statement/common.h
@@ -0,0 +1,9 @@
+#ifndef INCLUDE_GUARD_D8BD14A4_C3CA_4380_9AFB_EC7026A30DD7
+#define INCLUDE_GUARD_D8BD14A4_C3CA_4380_9AFB_EC7026A30DD7
+
+#include "builder/types.h"
+#include "sexp/types.h"
+
+void builder_guard_statement(Builder *, Sexp *, Block *, Block *);
+
+#endif /* INCLUDE_GUARD_D8BD14A4_C3CA_4380_9AFB_EC7026A30DD7 */
