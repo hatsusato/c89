e054cce0e593a2f9953813dfb4b69ba95102ba1d
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-31 18:51:06 +0900

    Update map_{clear,insert,find}

--- src/map.c
+++ src/map.c
@@ -30,14 +30,16 @@ void map_delete(Map *map) {
   UTILITY_FREE(map);
 }
 void map_clear(Map *map) {
+  pool_clear(map->pool);
   set_clear(map->set);
 }
 void map_insert(Map *map, ElemType key, ElemType val) {
+  pool_insert(map->pool, pair_new(key, val));
   set_insert(map->set, pair_new(key, val));
 }
 ElemType *map_find(Map *map, ElemType key) {
   Pair pair = pair_dummy(key);
-  const ElemType *found = set_find(map->set, &pair);
+  const ElemType *found = pool_find(map->pool, &pair);
   return found ? pair_val(*found) : NULL;
 }
 Bool map_contains(const Map *map, ElemType key) {
