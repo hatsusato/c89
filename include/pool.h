6bbfcb18ed6439b21aaef1ce9af57e3abc0c8b0c
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-31 18:30:40 +0900

    Add pool_{new,delete}

--- include/pool.h
+++ include/pool.h
@@ -1,6 +1,11 @@
 #ifndef INCLUDE_GUARD_E5CBD615_F933_4AAA_8E0A_D8BDF23D4954
 #define INCLUDE_GUARD_E5CBD615_F933_4AAA_8E0A_D8BDF23D4954
 
+#include "compare/types.h"
 #include "pool/types.h"
+#include "utility/types.h"
+
+Pool *pool_new(Destructor, Compare *);
+void pool_delete(Pool *);
 
 #endif /* INCLUDE_GUARD_E5CBD615_F933_4AAA_8E0A_D8BDF23D4954 */
--- src/pool.c
+++ src/pool.c
@@ -1,9 +1,22 @@
 #include "pool.h"
 
 #include "set.h"
+#include "utility.h"
 #include "vector.h"
 
 struct struct_Pool {
   Vector *vec;
   Set *set;
 };
+
+Pool *pool_new(Destructor dtor, Compare *compare) {
+  Pool *pool = UTILITY_MALLOC(Pool);
+  pool->vec = vector_new(dtor);
+  pool->set = set_new(NULL, compare);
+  return pool;
+}
+void pool_delete(Pool *pool) {
+  set_delete(pool->set);
+  vector_delete(pool->vec);
+  UTILITY_FREE(pool);
+}
