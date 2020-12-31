2fb679cf8c375530dbecd982919123144eacfdf7
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-12-31 18:51:44 +0900

    Remove map_contains

--- src/map.c
+++ src/map.c
@@ -42,7 +42,3 @@ ElemType *map_find(Map *map, ElemType key) {
   const ElemType *found = pool_find(map->pool, &pair);
   return found ? pair_val(*found) : NULL;
 }
-Bool map_contains(const Map *map, ElemType key) {
-  Pair pair = pair_dummy(key);
-  return set_contains(map->set, &pair);
-}
