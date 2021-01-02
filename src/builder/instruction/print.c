a4dda99755e5e017248bddbb5aa7fe832bcb1489
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-01-02 14:52:52 +0900

    Remove print_assign

--- src/builder/instruction/print.c
+++ src/builder/instruction/print.c
@@ -17,24 +17,36 @@ static void print_indent(void) {
   print_space();
   print_space();
 }
-static void print_assign(void) {
-  print_space();
-  printf("=");
-  print_space();
-}
 static void print_align(void) {
   printf(", align");
   print_space();
   printf("4");
 }
 static void instruction_print_name(Instruction *instr) {
-  const char *names[] = {
-#define DO_HANDLE(name, str) str,
+  switch (instr->ikind) {
+#define DO_HANDLE(name, str) \
+  case name:                 \
+    break;
+#include "terminator.def"
+#undef DO_HANDLE
+  case INSTRUCTION_STORE:
+    break;
+  default:
+    instruction_print(instr);
+    printf(" = ");
+    break;
+  }
+  switch (instr->ikind) {
+#define DO_HANDLE(name, str) \
+  case name:                 \
+    printf("%s ", str);      \
+    break;
 #include "instruction.def"
 #undef DO_HANDLE
-      "kind-count"};
-  printf("%s", names[instr->ikind]);
-  print_space();
+  default:
+    UTILITY_ASSERT(0);
+    break;
+  }
 }
 
 static void instruction_pretty_ret(Instruction *instr) {
@@ -63,31 +75,23 @@ static void instruction_pretty_switch(Instruction *instr) {
   printf("]");
 }
 static void instruction_pretty_add(Instruction *instr) {
-  instruction_print(instr);
-  print_assign();
   instruction_print_name(instr);
   value_print_with_type(instr->operands[0], false);
   value_print(instr->operands[1], true);
 }
 static void instruction_pretty_sub(Instruction *instr) {
-  instruction_print(instr);
-  print_assign();
   instruction_print_name(instr);
   value_print_with_type(instr->operands[0], false);
   value_print(instr->operands[1], true);
 }
 static void instruction_pretty_alloca(Instruction *instr) {
   Type *type = value_type(instruction_as_value(instr));
-  instruction_print(instr);
-  print_assign();
   instruction_print_name(instr);
   type_print(type_get_elem(type));
   print_align();
 }
 static void instruction_pretty_load(Instruction *instr) {
   Type *type = value_type(instruction_as_value(instr));
-  instruction_print(instr);
-  print_assign();
   instruction_print_name(instr);
   type_print(type);
   value_print_with_type(instr->operands[0], true);
@@ -100,8 +104,6 @@ static void instruction_pretty_store(Instruction *instr) {
   print_align();
 }
 static void instruction_pretty_icmp_ne(Instruction *instr) {
-  instruction_print(instr);
-  print_assign();
   instruction_print_name(instr);
   value_print_with_type(instr->operands[0], false);
   value_print(instr->operands[1], true);
