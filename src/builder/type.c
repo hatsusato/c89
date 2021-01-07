f3de5898189fe4b34029752052f0c78455c24187
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-01-07 11:42:37 +0900

    Add builder_type_int

--- src/builder/type.c
+++ src/builder/type.c
@@ -73,6 +73,21 @@ Type *builder_type_void(Builder *builder) {
   UTILITY_UNUSED(builder);
   return type_void();
 }
+Type *builder_type_int(Builder *builder) {
+  Module *module = builder_get_module(builder);
+  TypeSpec *spec = type_spec_new();
+  Type key, *type;
+  type_spec_set(spec, TYPE_SPEC_INT);
+  type_init_spec(&key, spec);
+  type_spec_delete(spec);
+  type = module_find_type(module, &key);
+  if (!type) {
+    type = type_new();
+    type_init_spec(type, spec);
+    module_insert_type(module, type);
+  }
+  return type;
+}
 Type *builder_type_integer(Builder *builder, int size) {
   Module *module = builder_get_module(builder);
   TypePool *pool = module_get_type(module);
