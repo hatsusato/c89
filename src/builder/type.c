32e5cc81935894bd327cb8c24bf7557cbf7b30d2
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-01-07 12:58:12 +0900

    Rename type_tmp_integer

--- src/builder/type.c
+++ src/builder/type.c
@@ -51,7 +51,7 @@ static void type_init_spec(Type *type, TypeSpec *spec) {
     }
   }
 }
-static Type type_tmp_integer(int size) {
+static Type type_integer(int size) {
   Type type;
   type.kind = TYPE_INTEGER;
   type.data.size = size;
@@ -117,26 +117,26 @@ Type *builder_type(Builder *builder, TypeSpec *spec) {
   return builder_new_type(builder, &type);
 }
 Type *builder_type_void(Builder *builder) {
-  Type tmp = type_tmp_integer(0);
-  return builder_new_type(builder, &tmp);
+  Type type = type_integer(0);
+  return builder_new_type(builder, &type);
 }
 Type *builder_type_bool(Builder *builder) {
-  Type tmp = type_tmp_integer(1);
-  return builder_new_type(builder, &tmp);
+  Type type = type_integer(1);
+  return builder_new_type(builder, &type);
 }
 Type *builder_type_int(Builder *builder) {
-  Type tmp = type_tmp_integer(32);
-  return builder_new_type(builder, &tmp);
+  Type type = type_integer(32);
+  return builder_new_type(builder, &type);
 }
 Type *builder_type_pointer(Builder *builder, Type *base) {
-  Type tmp;
-  tmp.kind = TYPE_POINTER;
-  tmp.data.type = base;
-  return builder_new_type(builder, &tmp);
+  Type type;
+  type.kind = TYPE_POINTER;
+  type.data.type = base;
+  return builder_new_type(builder, &type);
 }
 Type *builder_type_label(Builder *builder) {
-  Type tmp;
-  tmp.kind = TYPE_LABEL;
-  tmp.data.size = 0;
-  return builder_new_type(builder, &tmp);
+  Type type;
+  type.kind = TYPE_LABEL;
+  type.data.size = 0;
+  return builder_new_type(builder, &type);
 }
