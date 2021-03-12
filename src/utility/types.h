d427142f583f31321bc7bc35cebe607967a34978
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-03-12 13:06:00 +0900

    Add utility/{bool,stddef}.h

--- src/utility/types.h
+++ src/utility/types.h
@@ -1,14 +1,9 @@
 #ifndef INCLUDE_GUARD_ADFC65B2_9360_4BBE_84CD_607E1F3D622C
 #define INCLUDE_GUARD_ADFC65B2_9360_4BBE_84CD_607E1F3D622C
 
-#include <stddef.h>
-
 #include "opaque/opaque.h"
-
-#define true (1)
-#define false (0)
-typedef int Bool, Size, Index;
-typedef unsigned char Byte;
+#include "utility/bool.h"
+#include "utility/stddef.h"
 
 typedef void *ElemType;
 typedef void (*Destructor)(ElemType);
