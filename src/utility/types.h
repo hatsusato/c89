816d34553150771c567ffbc19e16ad5f92729b48
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-03-16 16:15:36 +0900

    Merge utility/bool.h

--- src/utility/types.h
+++ src/utility/types.h
@@ -2,7 +2,6 @@
 #define INCLUDE_GUARD_ADFC65B2_9360_4BBE_84CD_607E1F3D622C
 
 #include "opaque/opaque.h"
-#include "utility/bool.h"
 #include "utility/stddef.h"
 
 #endif /* INCLUDE_GUARD_ADFC65B2_9360_4BBE_84CD_607E1F3D622C */
