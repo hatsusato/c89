9037a03cd7517edb1c955767be8ee1481bad7e9f
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-04-01 18:10:04 +0900

    Add printer_{new,delete}

--- src/printer/printer.c
+++ src/printer/printer.c
@@ -2,6 +2,17 @@
 
 #include <stdio.h>
 
+#include "utility/utility.h"
+
 struct struct_Printer {
   FILE *fp;
 };
+
+Printer *printer_new(void) {
+  Printer *printer = UTILITY_MALLOC(Printer);
+  printer->fp = NULL;
+  return printer;
+}
+void printer_delete(Printer *printer) {
+  UTILITY_FREE(printer);
+}
--- src/printer/printer.h
+++ src/printer/printer.h
@@ -1,3 +1,9 @@
 #ifndef INCLUDE_GUARD_DBC01AAE_EE94_4490_B009_30C1CDA3E9FF
 #define INCLUDE_GUARD_DBC01AAE_EE94_4490_B009_30C1CDA3E9FF
+
+#include "utility/types.h"
+
+Printer *printer_new(void);
+void printer_delete(Printer *);
+
 #endif /* INCLUDE_GUARD_DBC01AAE_EE94_4490_B009_30C1CDA3E9FF */
