39810d35559a663fc9c92737ad78f1e86a3cf286
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-04-01 18:26:18 +0900

    Add printer_indent

--- src/printer/printer.c
+++ src/printer/printer.c
@@ -7,11 +7,20 @@
 
 struct struct_Printer {
   FILE *fp;
+  int indent;
 };
+
+static void printer_print_indent(Printer *printer) {
+  int i;
+  for (i = 0; i < printer->indent; ++i) {
+    fprintf(printer->fp, " ");
+  }
+}
 
 Printer *printer_new(PrinterFile fp) {
   Printer *printer = UTILITY_MALLOC(Printer);
   printer->fp = fp;
+  printer->indent = 0;
   return printer;
 }
 void printer_delete(Printer *printer) {
@@ -21,7 +30,11 @@ void printer_print(Printer *printer, const char *format, ...) {
   if (printer->fp) {
     va_list args;
     va_start(args, format);
+    printer_print_indent(printer);
     vfprintf(printer->fp, format, args);
     va_end(args);
   }
+}
+void printer_indent(Printer *printer, int indent) {
+  printer->indent += indent;
 }
--- src/printer/printer.h
+++ src/printer/printer.h
@@ -7,5 +7,6 @@
 Printer *printer_new(PrinterFile);
 void printer_delete(Printer *);
 void printer_print(Printer *, const char *, ...);
+void printer_indent(Printer *, int);
 
 #endif /* INCLUDE_GUARD_DBC01AAE_EE94_4490_B009_30C1CDA3E9FF */
