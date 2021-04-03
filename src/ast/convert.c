c25881d1673cb9a56f061726a0156638e96be1f7
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-04-03 22:54:54 +0900

    Add ast_convert_list

--- src/ast/convert.c
+++ src/ast/convert.c
@@ -1,6 +1,17 @@
 #include "convert.h"
 
 #include "sexp/sexp.h"
+#include "utility/utility.h"
+
+Sexp *ast_convert_list(Sexp *sexp) {
+  Sexp *ret = sexp_nil();
+  UTILITY_ASSERT(sexp_is_list(sexp));
+  for (; sexp_is_pair(sexp); sexp = sexp_cdr(sexp)) {
+    ret = sexp_snoc(ret, ast_convert(sexp_car(sexp)));
+  }
+  return ret;
+}
+
 Sexp *ast_convert(Sexp *sexp) {
   switch (sexp_get_tag(sexp)) {
   default:
