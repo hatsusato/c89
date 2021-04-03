b681e2e94112933a3d72f108d3b844ec86a5cf61
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-04-03 21:12:01 +0900

    Update build and print

--- src/main.c
+++ src/main.c
@@ -8,13 +8,13 @@
 #include "sexp/sexp.h"
 #include "utility/utility.h"
 
-static void build(Module *module, Sexp *ast) {
+static void build(Module *module, Ast *ast) {
   Builder *builder = builder_new(module);
-  builder_ast(builder, ast);
+  builder_ast(builder, ast_get(ast));
   builder_delete(builder);
 }
-static void print(Sexp *ast, Printer *printer) {
-  sexp_print(ast, printer);
+static void print(Ast *ast, Printer *printer) {
+  sexp_print(ast_get(ast), printer);
   printer_newline(printer);
 }
 static Bool is_debug(int argc, char *argv[]) {
@@ -31,17 +31,16 @@ int main(int argc, char *argv[]) {
   Ast *ast = ast_new();
   int ret = scanner_parse(ast);
   if (0 == ret) {
-    Sexp *sexp = ast_get(ast);
-    Module *module = module_new();
     Printer *printer = printer_new(stdout);
     if (is_debug(argc, argv)) {
-      print(sexp, printer);
+      print(ast, printer);
     } else {
-      build(module, sexp);
+      Module *module = module_new();
+      build(module, ast);
       module_pretty(module, printer);
+      module_delete(module);
     }
     printer_delete(printer);
-    module_delete(module);
   }
   ast_delete(ast);
   return ret;
