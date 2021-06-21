6da7871498eabbd74a40027be1f216da78d3ba87
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-21 10:53:11 +0900

    add vector_span

---
+++ src/vector/span.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_GUARD_55419819_A196_4816_9A82_349ADA502BAA
+#define INCLUDE_GUARD_55419819_A196_4816_9A82_349ADA502BAA
+
+#include <stddef.h>
+
+typedef unsigned char byte;
+
+struct vector_span {
+  byte *begin, *end;
+  size_t size;
+};
+
+#endif /* INCLUDE_GUARD_55419819_A196_4816_9A82_349ADA502BAA */
