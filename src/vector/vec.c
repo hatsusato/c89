58c0ef5bcbb67229a9ea9677ea40fa46bac39801
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-21 13:08:09 +0900

    Add vector_aligned_size

--- src/vector/vec.c
+++ src/vector/vec.c
@@ -3,7 +3,16 @@
 #include "span.h"
 #include "utility/buffer.h"
 
+static size_t vector_aligned_size(size_t size) {
+  enum { vector_align = 8 };
+  size += vector_align - 1;
+  size /= vector_align;
+  size *= vector_align;
+  return size;
+}
+
 void vec_init(struct vec *vec, size_t size, struct buffer *buf) {
+  size = vector_aligned_size(size);
   vector_span_init(&vec->span, buf->ptr, size);
   vec->capacity = buf->size;
 }
