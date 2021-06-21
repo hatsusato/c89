0606ac59fb10f3c667e7c3ff08d77c84b1bf75a3
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-21 13:23:48 +0900

    Add vector_{malloc,free}_data

--- src/vector/vec.c
+++ src/vector/vec.c
@@ -10,6 +10,18 @@ static size_t vector_aligned_size(size_t size) {
   size *= vector_align;
   return size;
 }
+void vector_malloc_data(struct vec *vec, size_t size, size_t count) {
+  struct buffer buf;
+  buf.size = size * count;
+  buffer_malloc(&buf);
+  vec_init(vec, size, &buf);
+}
+void vector_free_data(struct vec *vec) {
+  struct buffer buf;
+  buf.ptr = vec->span.begin;
+  buf.size = vec->capacity;
+  buffer_free(&buf);
+}
 
 void vec_init(struct vec *vec, size_t size, struct buffer *buf) {
   size = vector_aligned_size(size);
