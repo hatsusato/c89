f4a1e6f2044989ed93d110906278bb914738f8ed
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-21 11:18:18 +0900

    Add vector_span_init

--- src/vector/span.c
+++ src/vector/span.c
@@ -1,1 +1,14 @@
 #include "span.h"
+
+static size_t vector_span_aligned_size(size_t size) {
+  enum { vector_span_align = 8 };
+  size += vector_span_align - 1;
+  size /= vector_span_align;
+  size *= vector_span_align;
+  return size;
+}
+
+void vector_span_init(struct vector_span *span, byte_t *begin, size_t size) {
+  span->begin = span->end = begin;
+  span->size = vector_span_aligned_size(size);
+}
--- src/vector/span.h
+++ src/vector/span.h
@@ -8,4 +8,6 @@ struct vector_span {
   size_t size;
 };
 
+void vector_span_init(struct vector_span *, byte_t *, size_t);
+
 #endif /* INCLUDE_GUARD_55419819_A196_4816_9A82_349ADA502BAA */
