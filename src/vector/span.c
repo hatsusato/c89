3057e451a32fc74ac6325c1fb53ecb1544e0b83e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-21 12:07:55 +0900

    Update vector_span_push_{back,front}

--- src/vector/span.c
+++ src/vector/span.c
@@ -1,6 +1,8 @@
 #include "span.h"
 
-#include <string.h>
+#include <assert.h>
+
+#include "buffer.h"
 
 static size_t vector_span_aligned_size(size_t size) {
   enum { vector_span_align = 8 };
@@ -23,19 +25,21 @@ void vector_span_init(struct vector_span *span, byte_t *begin, size_t size) {
 size_t vector_span_length(struct vector_span *span) {
   return (span->end - span->begin) / span->size;
 }
-void vector_span_push_back(struct vector_span *span, byte_t *data,
-                           size_t count) {
-  memcpy(span->end, data, count);
+void vector_span_push_back(struct vector_span *span,
+                           const struct vector_buffer *buf) {
+  assert(span->size >= buf->size);
+  vector_span_memcpy(span->end, buf);
   span->end += span->size;
 }
 byte_t *vector_span_pop_back(struct vector_span *span) {
   span->end -= span->size;
   return span->end;
 }
-void vector_span_push_front(struct vector_span *span, byte_t *data,
-                            size_t count) {
+void vector_span_push_front(struct vector_span *span,
+                            const struct vector_buffer *buf) {
+  assert(span->size >= buf->size);
   span->begin -= span->size;
-  memcpy(span->begin, data, count);
+  vector_span_memcpy(span->begin, buf);
 }
 byte_t *vector_span_pop_front(struct vector_span *span) {
   byte_t *ptr = span->begin;
