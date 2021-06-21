785032e967fb273bc6da5908c34e559c89c477e6
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-21 10:55:02 +0900

    Add utility/type.h

--- src/vector/span.h
+++ src/vector/span.h
@@ -1,12 +1,10 @@
 #ifndef INCLUDE_GUARD_55419819_A196_4816_9A82_349ADA502BAA
 #define INCLUDE_GUARD_55419819_A196_4816_9A82_349ADA502BAA
 
-#include <stddef.h>
-
-typedef unsigned char byte;
+#include "utility/type.h"
 
 struct vector_span {
-  byte *begin, *end;
+  byte_t *begin, *end;
   size_t size;
 };
 
