b543fa3a5dd6018f7fcf43be817bd5a4b811e556
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-21 13:05:59 +0900

    Add vec_init

--- src/vector/vec.c
+++ src/vector/vec.c
@@ -1,3 +1,9 @@
 #include "vec.h"
 
-typedef int dummy;
+#include "span.h"
+#include "utility/buffer.h"
+
+void vec_init(struct vec *vec, size_t size, struct buffer *buf) {
+  vector_span_init(&vec->span, buf->ptr, size);
+  vec->capacity = buf->size;
+}
--- src/vector/vec.h
+++ src/vector/vec.h
@@ -3,9 +3,12 @@
 
 #include "span.h"
 
+struct buffer;
 struct vec {
   struct vector_span span;
   size_t capacity;
 };
+
+void vec_init(struct vec *, size_t, struct buffer *);
 
 #endif /* INCLUDE_GUARD_C9808F6F_7A34_4226_A18A_9B7E94B05A1D */
