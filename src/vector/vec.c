e380bfe985c5297c3f9658671d92b93a738147d0
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-21 14:55:05 +0900

    Alpha conversion

--- src/vector/vec.c
+++ src/vector/vec.c
@@ -10,14 +10,14 @@ static size_t vector_aligned_size(size_t size) {
   size *= vector_align;
   return size;
 }
-static void vector_malloc_data(struct vec *vec, size_t size, size_t count) {
+static void vector_malloc_data(struct vec *self, size_t size, size_t count) {
   struct buffer buf;
   buffer_malloc(&buf, size * count);
-  vec_init(vec, size, &buf);
+  vec_init(self, size, &buf);
 }
-static void vector_free_data(struct vec *vec) {
+static void vector_free_data(struct vec *self) {
   struct buffer buf;
-  buffer_init(&buf, vec->span.begin, vec->capacity);
+  buffer_init(&buf, self->span.begin, self->capacity);
   buffer_free(&buf);
 }
 static struct vec *vector_malloc(void) {
@@ -25,44 +25,44 @@ static struct vec *vector_malloc(void) {
   BUFFER_MALLOC(&buf, struct vec);
   return (struct vec *)buf.ptr;
 }
-static void vector_free(struct vec *vec) {
+static void vector_free(struct vec *self) {
   struct buffer buf;
-  BUFFER_INIT(&buf, vec);
+  BUFFER_INIT(&buf, self);
   buffer_free(&buf);
 }
 
-void vec_init(struct vec *vec, size_t size, struct buffer *buf) {
+void vec_init(struct vec *self, size_t size, struct buffer *buf) {
   size = vector_aligned_size(size);
-  vector_span_init(&vec->span, buf->ptr, size);
-  vec->capacity = buf->size;
+  vector_span_init(&self->span, buf->ptr, size);
+  self->capacity = buf->size;
 }
 struct vec *vec_create(size_t size) {
   enum { vec_initial_count = 8 };
-  struct vec *vec;
+  struct vec *self;
   size = vector_aligned_size(size);
-  vec = vector_malloc();
-  vector_malloc_data(vec, size, vec_initial_count);
-  return vec;
+  self = vector_malloc();
+  vector_malloc_data(self, size, vec_initial_count);
+  return self;
 }
-void vec_delete(struct vec *vec) {
-  vector_free_data(vec);
-  vector_free(vec);
+void vec_delete(struct vec *self) {
+  vector_free_data(self);
+  vector_free(self);
 }
-size_t vec_capacity(const struct vec *vec) {
-  return vec->capacity / vec->span.size;
+size_t vec_capacity(const struct vec *self) {
+  return self->capacity / self->span.size;
 }
-size_t vec_length(const struct vec *vec) {
-  return vector_span_length(&vec->span);
+size_t vec_length(const struct vec *self) {
+  return vector_span_length(&self->span);
 }
-bool_t vec_empty(const struct vec *vec) {
-  return vec_length(vec) == 0;
+bool_t vec_empty(const struct vec *self) {
+  return vec_length(self) == 0;
 }
-void *vec_begin(struct vec *vec) {
-  return vec->span.begin;
+void *vec_begin(struct vec *self) {
+  return self->span.begin;
 }
-void *vec_end(struct vec *vec) {
-  return vec->span.end;
+void *vec_end(struct vec *self) {
+  return self->span.end;
 }
-void *vec_at(struct vec *vec, index_t i) {
-  return vector_span_at(&vec->span, i);
+void *vec_at(struct vec *self, index_t i) {
+  return vector_span_at(&self->span, i);
 }
