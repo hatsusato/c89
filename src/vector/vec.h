33038bc1d151f6cec65ef67883afe6ea07193198
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-21 12:52:47 +0900

    Add struct vec

--- src/vector/vec.h
+++ src/vector/vec.h
@@ -1,3 +1,11 @@
 #ifndef INCLUDE_GUARD_C9808F6F_7A34_4226_A18A_9B7E94B05A1D
 #define INCLUDE_GUARD_C9808F6F_7A34_4226_A18A_9B7E94B05A1D
+
+#include "span.h"
+
+struct vec {
+  struct vector_span span;
+  size_t capacity;
+};
+
 #endif /* INCLUDE_GUARD_C9808F6F_7A34_4226_A18A_9B7E94B05A1D */
