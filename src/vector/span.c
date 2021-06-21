ff5dbcd0b18e2dd56855795607fe37c2ea488339
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-21 14:21:47 +0900

    Update vector_span_{push,pop}_{back,front}

--- src/vector/span.c
+++ src/vector/span.c
@@ -10,27 +10,35 @@ size_t vector_span_length(const struct vector_span *span) {
   return (span->end - span->begin) / span->size;
 }
 void vector_span_push_back(struct vector_span *span, const struct buffer *buf) {
-  struct buffer dst;
-  buffer_init(&dst, span->end, span->size);
-  buffer_memcpy(&dst, buf);
+  if (buf) {
+    struct buffer dst;
+    buffer_init(&dst, span->end, span->size);
+    buffer_memcpy(&dst, buf);
+  }
   span->end += span->size;
 }
 void vector_span_pop_back(struct vector_span *span, struct buffer *buf) {
-  struct buffer src;
   span->end -= span->size;
-  buffer_init(&src, span->end, span->size);
-  buffer_memcpy(buf, &src);
+  if (buf) {
+    struct buffer src;
+    buffer_init(&src, span->end, span->size);
+    buffer_memcpy(buf, &src);
+  }
 }
 void vector_span_push_front(struct vector_span *span,
                             const struct buffer *buf) {
-  struct buffer dst;
   span->begin -= span->size;
-  buffer_init(&dst, span->begin, span->size);
-  buffer_memcpy(&dst, buf);
+  if (buf) {
+    struct buffer dst;
+    buffer_init(&dst, span->begin, span->size);
+    buffer_memcpy(&dst, buf);
+  }
 }
 void vector_span_pop_front(struct vector_span *span, struct buffer *buf) {
-  struct buffer src;
-  buffer_init(&src, span->begin, span->size);
-  buffer_memcpy(buf, &src);
+  if (buf) {
+    struct buffer src;
+    buffer_init(&src, span->begin, span->size);
+    buffer_memcpy(buf, &src);
+  }
   span->begin += span->size;
 }
