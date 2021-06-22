0102f32ad13797ff57c691165560de7d41ab2ba8
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-22 21:32:55 +0900

    Add vec_ptr_new

--- src/vector/vec_ptr.c
+++ src/vector/vec_ptr.c
@@ -1,1 +1,18 @@
 #include "vec_ptr.h"
+
+#include "utility/buffer.h"
+#include "vec.h"
+
+static struct vec_ptr *vec_ptr_malloc(void) {
+  struct buffer buf;
+  BUFFER_MALLOC(&buf, struct vec_ptr);
+  return (struct vec_ptr *)buf.ptr;
+}
+
+struct vec_ptr *vec_ptr_new(void) {
+  enum { initial_count = 8 };
+  struct vec_ptr *self = vec_ptr_malloc();
+  vec_init(&self->vec, sizeof(void *));
+  vec_alloc(&self->vec, initial_count);
+  return self;
+}
--- src/vector/vec_ptr.h
+++ src/vector/vec_ptr.h
@@ -7,4 +7,6 @@ struct vec_ptr {
   struct vec vec;
 };
 
+struct vec_ptr *vec_ptr_new(void);
+
 #endif /* INCLUDE_GUARD_163637DD_0D83_468B_88CC_AEFD8917410E */
