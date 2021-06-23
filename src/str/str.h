63d97d4534cbf3f4b0db28642ed1a2d4e9a20c27
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-23 23:46:09 +0900

    Add str_remove_{pre,suf}fix

--- src/str/str.h
+++ src/str/str.h
@@ -9,5 +9,7 @@ struct str {
 };
 
 void str_init(struct str *, const char *);
+void str_remove_prefix(struct str *, index_t);
+void str_remove_suffix(struct str *, index_t);
 
 #endif /* INCLUDE_GUARD_D25BBF12_114D_4DCE_BC2A_1ADD984F469C */
