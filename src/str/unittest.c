ca157079e792d9a81c7e3b12c7f25716052a15f6
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-24 00:15:43 +0900

    Update str_unittest

--- src/str/unittest.c
+++ src/str/unittest.c
@@ -1,3 +1,32 @@
 #include "unittest.h"
 
-void str_unittest(void) {}
+#include <assert.h>
+
+#include "str.h"
+
+#define str_unittest_eq(lhs, rhs)     \
+  do {                                \
+    assert(str_cmp(&lhs, &rhs) == 0); \
+    assert(str_cmp(&rhs, &lhs) == 0); \
+  } while (0)
+#define str_unittest_lt(lhs, rhs)    \
+  do {                               \
+    assert(str_cmp(&lhs, &rhs) < 0); \
+    assert(str_cmp(&rhs, &lhs) > 0); \
+  } while (0)
+
+void str_unittest(void) {
+  struct str str, prefix, suffix;
+  str_init(&str, "0123456789");
+  str_init(&prefix, "01234");
+  str_init(&suffix, "56789");
+  str_unittest_eq(str, str);
+  str_unittest_lt(prefix, str);
+  str_unittest_lt(str, suffix);
+  str_unittest_lt(prefix, suffix);
+  str_remove_prefix(&str, 5);
+  str_unittest_eq(str, suffix);
+  str_remove_prefix(&str, 5);
+  str_unittest_lt(str, prefix);
+  str_unittest_lt(str, suffix);
+}
