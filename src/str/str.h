143d224b12e0476ed2014c106774902973926371
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-23 21:00:31 +0900

    Add struct str

--- src/str/str.h
+++ src/str/str.h
@@ -1,3 +1,11 @@
 #ifndef INCLUDE_GUARD_D25BBF12_114D_4DCE_BC2A_1ADD984F469C
 #define INCLUDE_GUARD_D25BBF12_114D_4DCE_BC2A_1ADD984F469C
+
+#include "util/type.h"
+
+struct str {
+  const char *ptr;
+  index_t len;
+};
+
 #endif /* INCLUDE_GUARD_D25BBF12_114D_4DCE_BC2A_1ADD984F469C */
