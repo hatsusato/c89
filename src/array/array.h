3bd26dad12926c327019c160037c998e6250bf15
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-24 14:56:50 +0900

    Add struct array

--- src/array/array.h
+++ src/array/array.h
@@ -1,6 +1,12 @@
 #ifndef INCLUDE_GUARD_9AEC46FD_3C00_49FC_AB18_C05CB1E03D70
 #define INCLUDE_GUARD_9AEC46FD_3C00_49FC_AB18_C05CB1E03D70
 
-struct array;
+#include "util/buffer.h"
+
+struct array {
+  align_t align;
+  struct buffer buf;
+  index_t len;
+};
 
 #endif /* INCLUDE_GUARD_9AEC46FD_3C00_49FC_AB18_C05CB1E03D70 */
