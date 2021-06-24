eff2c4958baedac52355d32caa25e3cf5d60c8cc
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-24 17:11:23 +0900

    Update array_unittest

--- src/array/unittest.c
+++ src/array/unittest.c
@@ -1,3 +1,40 @@
 #include "unittest.h"
 
-void array_unittest(void) {}
+#include <assert.h>
+
+#include "array.h"
+
+#define array_unittest_push(array, val)                  \
+  do {                                                   \
+    struct buffer buf;                                   \
+    buffer_init(&buf, &val, sizeof(int));                \
+    array_insert(&array, array_length(&array), 1, &buf); \
+  } while (false)
+#define array_unittest_range(array, index, begin, end) \
+  do {                                                 \
+    int i, j;                                          \
+    for (i = index, j = begin; j < end; i++, j++) {    \
+      int *p = array_at(&array, i);                    \
+      assert(*p == j);                                 \
+    }                                                  \
+  } while (0)
+
+void array_unittest(void) {
+  struct array array;
+  struct buffer buf;
+  int i;
+  buffer_malloc(&buf, 100 * sizeof(int));
+  array_init(&array, sizeof(int), &buf);
+  assert(array_align(&array) == sizeof(int));
+  assert(array_capacity(&array) == 100);
+  assert(array_length(&array) == 0);
+  for (i = 0; i < 100; i++) {
+    array_unittest_push(array, i);
+  }
+  assert(array_length(&array) == 100);
+  array_unittest_range(array, 0, 0, 100);
+  array_remove(&array, 0, 100);
+  assert(array_capacity(&array) == 100);
+  assert(array_length(&array) == 0);
+  buffer_free(&buf);
+}
