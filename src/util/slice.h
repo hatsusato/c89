cd612487bdcc703542f42a83dd8767d5118aeb30
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-25 23:46:28 +0900

    Add struct slice

---
+++ src/util/slice.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDE_GUARD_1070D04D_4250_4AE0_B426_C897597C75CD
+#define INCLUDE_GUARD_1070D04D_4250_4AE0_B426_C897597C75CD
+
+#include "type.h"
+
+struct slice {
+  align_t align;
+  const byte_t *ptr;
+  index_t len;
+};
+
+#endif /* INCLUDE_GUARD_1070D04D_4250_4AE0_B426_C897597C75CD */
