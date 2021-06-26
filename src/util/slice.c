7f83b31bd4ca5558b2096f9aeb48933dbefc665f
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-26 23:59:07 +0900

    Update slice_at

--- src/util/slice.c
+++ src/util/slice.c
@@ -25,6 +25,12 @@ size_t slice_size(const struct slice *self) {
   return self->len * self->align;
 }
 const void *slice_at(const struct slice *self, index_t index) {
-  assert(0 <= index);
-  return (index < self->len) ? self->ptr + index * self->align : NULL;
+  if (index < 0) {
+    index += self->len;
+  }
+  if (0 <= index && index < self->len) {
+    return self->ptr + index * self->align;
+  } else {
+    return NULL;
+  }
 }
