8467f3055ff4cc8e490ed621f9dbbe6607f4036e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-27 12:16:06 +0900

    Add assertions in buffer

--- src/util/buffer.c
+++ src/util/buffer.c
@@ -7,11 +7,17 @@
 #include "util.h"
 
 void buffer_init(struct buffer *self, void *ptr, size_t size) {
+  assert(ptr || size == 0);
   self->ptr = ptr;
   self->size = size;
 }
 void *buffer_at(const struct buffer *self, size_t index) {
-  return (index < self->size) ? self->ptr + index : NULL;
+  if (index < self->size) {
+    assert(self->ptr);
+    return self->ptr + index;
+  } else {
+    return NULL;
+  }
 }
 size_t buffer_size(const struct buffer *self) {
   return self->size;
@@ -25,21 +31,22 @@ void buffer_free(struct buffer *self) {
   buffer_init(self, NULL, 0);
 }
 bool_t buffer_is_null(const struct buffer *self) {
-  assert(self->ptr || self->size == 0);
   return !self->ptr;
 }
 void buffer_memcpy(struct buffer *self, const struct buffer *buf) {
   assert(self->size >= buf->size);
-  if (!buffer_is_null(self) && !buffer_is_null(buf) && buf->size > 0) {
+  if (self->ptr && buf->ptr) {
     memcpy(self->ptr, buf->ptr, buf->size);
   }
 }
 void buffer_slice(struct buffer *self, size_t offset, size_t size) {
+  assert(self->ptr);
   assert(offset + size <= self->size);
   self->ptr += offset;
   self->size = size;
 }
 void buffer_slide(struct buffer *self, size_t src, size_t dst, size_t size) {
+  assert(self->ptr);
   assert(src + size <= self->size);
   assert(dst + size <= self->size);
   memmove(self->ptr + dst, self->ptr + src, size);
