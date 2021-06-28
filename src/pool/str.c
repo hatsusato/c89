cac7246c7fbd866950cd93bfa5b4244b5092aa43
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-28 20:52:47 +0900

    Add pool_str_table_insert

--- src/pool/str.c
+++ src/pool/str.c
@@ -4,9 +4,16 @@
 #include "util/buffer.h"
 #include "vec/vec.h"
 
+static void pool_str_table_insert(struct pool_str *self, const char *ptr,
+                                  index_t len) {
+  struct str str;
+  str_init(&str, ptr, len);
+  vec_push(&self->table, &str);
+}
+
 void pool_str_new(struct pool_str *self) {
   pool_new(&self->pool);
-  vec_new(&self->table, sizeof(const char *));
+  vec_new(&self->table, sizeof(struct str));
 }
 void pool_str_delete(struct pool_str *self) {
   pool_delete(&self->pool);
@@ -14,6 +21,10 @@ void pool_str_delete(struct pool_str *self) {
 }
 const char *pool_str_insert(struct pool_str *self, const struct str *str) {
   struct buffer buf;
-  buffer_init(&buf, (void *)str_ptr(str), str_length(str));
-  return pool_insert(&self->pool, &buf);
+  const char *ptr = str_ptr(str);
+  index_t len = str_length(str);
+  buffer_init(&buf, (void *)ptr, len);
+  ptr = pool_insert(&self->pool, &buf);
+  pool_str_table_insert(self, ptr, len);
+  return ptr;
 }
