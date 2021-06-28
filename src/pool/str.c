b868eab4217fced7344243f7035c4c26a0be9c2b
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-28 16:31:02 +0900

    Add pool_str_{insert,push}

--- src/pool/str.c
+++ src/pool/str.c
@@ -1,6 +1,7 @@
 #include "str.h"
 
 #include "pool.h"
+#include "str/str.h"
 #include "vec/vec.h"
 
 static void pool_str_new_pool_malloc(struct vec *self) {
@@ -11,6 +12,12 @@ static void pool_str_new_pool_malloc(struct vec *self) {
 }
 static void pool_str_delete_pool_free(void *self) {
   pool_free(self);
+}
+static const char *pool_str_push(struct vec *self, const struct str *str) {
+  struct pool *pool = vec_at(self, vec_length(self) - 1);
+  struct buffer buf;
+  buffer_init(&buf, (void *)str_ptr(str), str_length(str));
+  return pool_insert(pool, &buf);
 }
 
 void pool_str_new(struct pool_str *self) {
@@ -23,4 +30,12 @@ void pool_str_delete(struct pool_str *self) {
   vec_delete(&self->pool, pool_str_delete_pool_free);
   vec_delete(&self->table, NULL);
   vec_delete(&self->big, NULL);
+}
+const char *pool_str_insert(struct pool_str *self, const struct str *str) {
+  const char *ptr = pool_str_push(&self->pool, str);
+  if (!ptr) {
+    pool_str_new_pool_malloc(&self->pool);
+    ptr = pool_str_push(&self->pool, str);
+  }
+  return ptr;
 }
--- src/pool/str.h
+++ src/pool/str.h
@@ -3,11 +3,14 @@
 
 #include "vec/type.h"
 
+struct str;
+
 struct pool_str {
   struct vec pool, table, big;
 };
 
 void pool_str_new(struct pool_str *);
 void pool_str_delete(struct pool_str *);
+const char *pool_str_insert(struct pool_str *, const struct str *);
 
 #endif /* INCLUDE_GUARD_D300EE6C_9C63_45F4_9599_FB398D4BE800 */
