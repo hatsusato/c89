2762e8d1c287e647f841095641e7baa9f38f54fd
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-28 16:53:09 +0900

    Add pool_str_big_{malloc,free}

--- src/pool/str.c
+++ src/pool/str.c
@@ -1,5 +1,8 @@
 #include "str.h"
 
+#include <stdlib.h>
+#include <string.h>
+
 #include "pool.h"
 #include "str/str.h"
 #include "vec/vec.h"
@@ -12,6 +15,16 @@ static void pool_str_new_pool_malloc(struct vec *self) {
 }
 static void pool_str_delete_pool_free(void *self) {
   pool_free(self);
+}
+static const char *pool_str_big_malloc(const struct str *str) {
+  index_t len = str_length(str);
+  char *ptr = malloc(len + 1);
+  strncpy(ptr, str_ptr(str), str_length(str));
+  ptr[len] = 0;
+  return ptr;
+}
+static void pool_str_big_free(void *self) {
+  free(*(void **)self);
 }
 static const char *pool_str_push(struct vec *self, const struct str *str) {
   struct pool *pool = vec_at(self, vec_length(self) - 1);
@@ -29,7 +42,7 @@ void pool_str_new(struct pool_str *self) {
 void pool_str_delete(struct pool_str *self) {
   vec_delete(&self->pool, pool_str_delete_pool_free);
   vec_delete(&self->table, NULL);
-  vec_delete(&self->big, NULL);
+  vec_delete(&self->big, pool_str_big_free);
 }
 const char *pool_str_insert(struct pool_str *self, const struct str *str) {
   const char *ptr = pool_str_push(&self->pool, str);
