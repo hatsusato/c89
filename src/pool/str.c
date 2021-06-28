049ff56053aacd91e6d86d5f98ccf6946ec53da5
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-28 16:02:30 +0900

    Replace pool_str_new_pool

--- src/pool/str.c
+++ src/pool/str.c
@@ -3,13 +3,10 @@
 #include "pool.h"
 #include "vec/vec.h"
 
-enum { pool_str_initial_capacity = 8, pool_str_block_size = 4096 };
-
-static void pool_str_new_pool(struct vec *self) {
+static void pool_str_new_pool_malloc(struct vec *self) {
+  enum { pool_str_block_size = 4096 };
   struct pool pool;
   pool_malloc(&pool, pool_str_block_size);
-  vec_init(self, sizeof(struct pool));
-  vec_malloc(self, pool_str_initial_capacity);
   vec_push(self, &pool);
 }
 static void pool_str_delete_pool(struct vec *self) {
@@ -21,12 +18,10 @@ static void pool_str_delete_pool(struct vec *self) {
 }
 
 void pool_str_new(struct pool_str *self) {
-  enum { initial_capacity = 8 };
-  pool_str_new_pool(&self->pool);
-  vec_init(&self->table, sizeof(const char *));
-  vec_malloc(&self->table, initial_capacity);
-  vec_init(&self->big, sizeof(const char *));
-  vec_malloc(&self->big, initial_capacity);
+  vec_new(&self->pool, sizeof(struct pool));
+  pool_str_new_pool_malloc(&self->pool);
+  vec_new(&self->table, sizeof(const char *));
+  vec_new(&self->big, sizeof(const char *));
 }
 void pool_str_delete(struct pool_str *self) {
   pool_str_delete_pool(&self->pool);
