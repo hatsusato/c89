9f737feddbafe6e7f4fd64775ab53faa0beedca3
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-28 17:14:36 +0900

    Add pool_block_insert

--- src/pool/block.c
+++ src/pool/block.c
@@ -9,3 +9,14 @@ void pool_block_free(struct pool_block *self) {
   buffer_free(&self->buf);
   self->offset = 0;
 }
+const void *pool_block_insert(struct pool_block *self,
+                              const struct buffer *buf) {
+  const void *ptr = NULL;
+  size_t size = buffer_size(buf);
+  if (self->offset + size <= buffer_size(&self->buf)) {
+    buffer_memcpy(&self->buf, self->offset, buf);
+    ptr = buffer_at(&self->buf, self->offset);
+    self->offset += size;
+  }
+  return ptr;
+}
--- src/pool/block.h
+++ src/pool/block.h
@@ -10,5 +10,6 @@ struct pool_block {
 
 void pool_block_malloc(struct pool_block *);
 void pool_block_free(struct pool_block *);
+const void *pool_block_insert(struct pool_block *, const struct buffer *);
 
 #endif /* INCLUDE_GUARD_2234C8F0_274D_4287_A43D_9D9C08D851B7 */
