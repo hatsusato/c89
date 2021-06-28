e7abcb789694af219b5a9a4c837b07bc62e2520d
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-28 18:13:54 +0900

    Add struct pool.{small,big}

--- src/pool/pool.c
+++ src/pool/pool.c
@@ -1,12 +1,34 @@
 #include "pool.h"
 
+#include <stdlib.h>
+
+#include "block.h"
+#include "vec/ptr.h"
+#include "vec/vec.h"
+
+static void pool_small_free(void *self) {
+  pool_block_free(self);
+}
+static void pool_big_free(void *self) {
+  free(self);
+}
+
 void pool_malloc(struct pool *self, size_t size) {
+  struct pool_block block;
   buffer_malloc(&self->buf, size);
   self->offset = 0;
+  vec_new(&self->small, sizeof(struct pool_block));
+  pool_block_malloc(&block);
+  vec_push(&self->small, &block);
+  vec_ptr_new(&self->big);
 }
 void pool_free(struct pool *self) {
   buffer_free(&self->buf);
   self->offset = 0;
+  vec_map(&self->small, pool_small_free);
+  vec_delete(&self->small);
+  vec_ptr_map(&self->big, pool_big_free);
+  vec_ptr_delete(&self->big);
 }
 const void *pool_insert(struct pool *self, const struct buffer *buf) {
   const void *ptr = NULL;
--- src/pool/pool.h
+++ src/pool/pool.h
@@ -2,10 +2,13 @@
 #define INCLUDE_GUARD_FD4CDA16_28C2_418A_84F7_67EFEEE7B0B6
 
 #include "util/buffer.h"
+#include "vec/type.h"
 
 struct pool {
   struct buffer buf;
   size_t offset;
+  struct vec small;
+  struct vec_ptr big;
 };
 
 void pool_malloc(struct pool *, size_t);
