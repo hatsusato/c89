3160bb70848a34752813f7c10ca1b9366bf91473
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-28 15:30:42 +0900

    Add pool_str_{new,delete}_pool

--- src/pool/str.c
+++ src/pool/str.c
@@ -3,25 +3,33 @@
 #include "pool.h"
 #include "vec/vec.h"
 
-void pool_str_new(struct pool_str *self) {
-  enum { initial_capacity = 8, pool_size = 4096 };
+enum { pool_str_initial_capacity = 8, pool_str_block_size = 4096 };
+
+static void pool_str_new_pool(struct vec *self) {
   struct pool pool;
-  vec_init(&self->pool, sizeof(struct pool));
-  vec_malloc(&self->pool, initial_capacity);
-  pool_malloc(&pool, pool_size);
-  vec_push(&self->pool, &pool);
+  pool_malloc(&pool, pool_str_block_size);
+  vec_init(self, sizeof(struct pool));
+  vec_malloc(self, pool_str_initial_capacity);
+  vec_push(self, &pool);
+}
+static void pool_str_delete_pool(struct vec *self) {
+  index_t index, len = vec_length(self);
+  for (index = 0; index < len; index++) {
+    pool_free(vec_at(self, index));
+  }
+  vec_free(self);
+}
+
+void pool_str_new(struct pool_str *self) {
+  enum { initial_capacity = 8 };
+  pool_str_new_pool(&self->pool);
   vec_init(&self->table, sizeof(const char *));
   vec_malloc(&self->table, initial_capacity);
   vec_init(&self->big, sizeof(const char *));
   vec_malloc(&self->big, initial_capacity);
 }
 void pool_str_delete(struct pool_str *self) {
-  index_t index;
-  struct pool *it = vec_at(&self->pool, 0);
-  for (index = 0; index < vec_length(&self->pool); index++, it++) {
-    pool_free(it);
-  }
-  vec_free(&self->pool);
+  pool_str_delete_pool(&self->pool);
   vec_free(&self->table);
   vec_free(&self->big);
 }
