649ff09fc917b6613896131dfd73ff6c4f5cec59
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-28 16:02:50 +0900

    Replace pool_str_delete_pool

--- src/pool/str.c
+++ src/pool/str.c
@@ -9,12 +9,8 @@ static void pool_str_new_pool_malloc(struct vec *self) {
   pool_malloc(&pool, pool_str_block_size);
   vec_push(self, &pool);
 }
-static void pool_str_delete_pool(struct vec *self) {
-  index_t index, len = vec_length(self);
-  for (index = 0; index < len; index++) {
-    pool_free(vec_at(self, index));
-  }
-  vec_free(self);
+static void pool_str_delete_pool_free(void *self) {
+  pool_free(self);
 }
 
 void pool_str_new(struct pool_str *self) {
@@ -24,7 +20,7 @@ void pool_str_new(struct pool_str *self) {
   vec_new(&self->big, sizeof(const char *));
 }
 void pool_str_delete(struct pool_str *self) {
-  pool_str_delete_pool(&self->pool);
-  vec_free(&self->table);
-  vec_free(&self->big);
+  vec_delete(&self->pool, pool_str_delete_pool_free);
+  vec_delete(&self->table, NULL);
+  vec_delete(&self->big, NULL);
 }
