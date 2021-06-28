7ee55550086553ff33d542276e8f8417d2da9e24
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-28 17:09:24 +0900

    Add pool_block_{malloc,free}

--- src/pool/block.c
+++ src/pool/block.c
@@ -1,1 +1,11 @@
 #include "block.h"
+
+void pool_block_malloc(struct pool_block *self) {
+  enum { pool_block_size = 4096 };
+  buffer_malloc(&self->buf, pool_block_size);
+  self->offset = 0;
+}
+void pool_block_free(struct pool_block *self) {
+  buffer_free(&self->buf);
+  self->offset = 0;
+}
--- src/pool/block.h
+++ src/pool/block.h
@@ -8,4 +8,7 @@ struct pool_block {
   size_t offset;
 };
 
+void pool_block_malloc(struct pool_block *);
+void pool_block_free(struct pool_block *);
+
 #endif /* INCLUDE_GUARD_2234C8F0_274D_4287_A43D_9D9C08D851B7 */
