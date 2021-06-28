9d764d98ab61552d2a27bedc3bfeb674bad426d8
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-28 13:46:53 +0900

    Add sturct pool_str

---
+++ src/pool/str.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_GUARD_D300EE6C_9C63_45F4_9599_FB398D4BE800
+#define INCLUDE_GUARD_D300EE6C_9C63_45F4_9599_FB398D4BE800
+
+#include "vec/type.h"
+
+struct pool_str {
+  struct vec pool, table, big;
+};
+
+#endif /* INCLUDE_GUARD_D300EE6C_9C63_45F4_9599_FB398D4BE800 */
