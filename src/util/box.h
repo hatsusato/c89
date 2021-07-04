1cd4e1e7edb0519cb43f4b51229734981b65b723
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-04 14:13:01 +0900

    Add struct box

---
+++ src/util/box.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_GUARD_B04970AB_F75D_4E4A_B3A9_B765648D10EE
+#define INCLUDE_GUARD_B04970AB_F75D_4E4A_B3A9_B765648D10EE
+
+#include "util/type.h"
+
+struct box {
+  void *ptr;
+  size_t size;
+};
+
+#endif /* INCLUDE_GUARD_B04970AB_F75D_4E4A_B3A9_B765648D10EE */
