789a9504de16c16f09ef3fb1135d7ff0e84c35b3
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-04 17:23:08 +0900

    Fix pool_unittest

--- src/pool/unittest.c
+++ src/pool/unittest.c
@@ -5,43 +5,40 @@
 #include "pool.h"
 #include "str.h"
 #include "str/str.h"
+#include "util/box.h"
 #include "util/buffer.h"
 #include "vec/vec.h"
 
-#define pool_unittest_set_buffer(buf, len)      \
-  do {                                          \
-    int i;                                      \
-    buffer_malloc(buf, sizeof(int) * len);      \
-    for (i = 0; i < len; i++) {                 \
-      int *p = buffer_at(buf, sizeof(int) * i); \
-      *p = i;                                   \
-    }                                           \
-  } while (false)
-#define pool_unittest_check(p, len) \
-  do {                              \
-    int i;                          \
-    for (i = 0; i < len; i++) {     \
-      assert(p[i] == i);            \
-    }                               \
-  } while (false)
+const int *pool_unittest_set(struct pool *pool, int len) {
+  int i;
+  struct box box;
+  struct buffer buf;
+  const int *p;
+  box_new(&box, sizeof(int) * len);
+  box_buffer(&box, &buf);
+  for (i = 0; i < len; i++) {
+    int *p = buffer_at(&buf, sizeof(int) * i);
+    *p = i;
+  }
+  p = pool_insert(pool, &buf);
+  box_delete(&box);
+  return p;
+}
+void pool_unittest_check(const int *p, int len) {
+  int i;
+  for (i = 0; i < len; i++) {
+    assert(p[i] == i);
+  }
+}
 
 void pool_unittest(void) {
   struct pool pool;
-  struct buffer buf;
-  const int *p10, *p100, *p1000;
+  int i, j;
   pool_new(&pool);
-  pool_unittest_set_buffer(&buf, 10);
-  p10 = pool_insert(&pool, &buf);
-  buffer_free(&buf);
-  pool_unittest_set_buffer(&buf, 100);
-  p100 = pool_insert(&pool, &buf);
-  buffer_free(&buf);
-  pool_unittest_set_buffer(&buf, 1000);
-  p1000 = pool_insert(&pool, &buf);
-  buffer_free(&buf);
-  pool_unittest_check(p10, 10);
-  pool_unittest_check(p100, 100);
-  pool_unittest_check(p1000, 1000);
+  for (i = 0, j = 1; i < 16; i++, j *= 2) {
+    const int *p = pool_unittest_set(&pool, j);
+    pool_unittest_check(p, i);
+  }
   pool_delete(&pool);
 }
 
