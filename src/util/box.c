27be4cee4791782946add67c82ca0a2c6c886e74
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-04 14:41:19 +0900

    Add box_{new,delete}

--- src/util/box.c
+++ src/util/box.c
@@ -1,1 +1,13 @@
 #include "box.h"
+
+#include <stdlib.h>
+
+void box_new(struct box *box, size_t size) {
+  box->ptr = malloc(size);
+  box->size = size;
+}
+void box_delete(struct box *box) {
+  free(box->ptr);
+  box->ptr = NULL;
+  box->size = 0;
+}
--- src/util/box.h
+++ src/util/box.h
@@ -8,4 +8,7 @@ struct box {
   size_t size;
 };
 
+void box_new(struct box *, size_t);
+void box_delete(struct box *);
+
 #endif /* INCLUDE_GUARD_B04970AB_F75D_4E4A_B3A9_B765648D10EE */
