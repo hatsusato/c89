e620f5212d9b19043307f5d5d25fa78e3332b752
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-09 23:23:30 +0900

    Remove struct box

--- src/util/box.c
+++ src/util/box.c
@@ -9,25 +9,6 @@ struct box_data {
   byte_t data[1];
 };
 
-void box_new(struct box *box, size_t size) {
-  box->ptr = malloc(size);
-  box->size = size;
-}
-void box_delete(struct box *box) {
-  free(box->ptr);
-  box->ptr = NULL;
-  box->size = 0;
-}
-void box_buffer(struct box *box, struct buffer *buf) {
-  buffer_init(buf, box->ptr, box->size);
-}
-void *box_ptr(const struct box *box) {
-  return box->ptr;
-}
-size_t box_size(const struct box *box) {
-  return box->size;
-}
-
 struct box_data *box_data_new(size_t size) {
   struct box_data *box = malloc(sizeof(size_t) + size);
   box->size = size;
--- src/util/box.h
+++ src/util/box.h
@@ -6,17 +6,6 @@
 struct box_data;
 struct buffer;
 
-struct box {
-  void *ptr;
-  size_t size;
-};
-
-void box_new(struct box *, size_t);
-void box_delete(struct box *);
-void box_buffer(struct box *, struct buffer *);
-void *box_ptr(const struct box *);
-size_t box_size(const struct box *);
-
 struct box_data *box_data_new(size_t);
 void box_data_delete(struct box_data *);
 void box_data_buffer(struct box_data *, struct buffer *);
