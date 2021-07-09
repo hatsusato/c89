0ad69b3b075cff8fe65a5d0a23580ab497213f5e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-10 02:37:24 +0900

    Add array_inner

--- src/array/array.c
+++ src/array/array.c
@@ -8,6 +8,9 @@
 #include "util/buffer.h"
 #include "util/util.h"
 
+static struct slice *array_inner(struct array *self) {
+  return &self->slice;
+}
 static void array_slide(struct array *self, index_t index, index_t count) {
   void *ptr = array_at(self, 0);
   if (ptr) {
@@ -21,23 +24,23 @@ static void array_slide(struct array *self, index_t index, index_t count) {
     buffer_init(&buf, ptr, max * align);
     buffer_slide(&buf, from, to, offset);
   }
-  slice_resize(&self->slice, count);
+  slice_resize(array_inner(self), count);
 }
 
 void array_init(struct array *self, align_t align, void *ptr) {
-  slice_init(&self->slice, align, ptr, 0);
+  slice_init(array_inner(self), align, ptr, 0);
 }
 const struct slice *array_slice(const struct array *self) {
   return &self->slice;
 }
 align_t array_align(const struct array *self) {
-  return slice_align(&self->slice);
+  return slice_align(array_slice(self));
 }
 index_t array_length(const struct array *self) {
-  return slice_length(&self->slice);
+  return slice_length(array_slice(self));
 }
 void *array_at(struct array *self, index_t index) {
-  return (void *)slice_at(&self->slice, index);
+  return (void *)slice_at(array_inner(self), index);
 }
 void array_insert(struct array *self, index_t offset,
                   const struct slice *slice) {
@@ -46,7 +49,7 @@ void array_insert(struct array *self, index_t offset,
   assert(array_align(self) == slice_align(slice));
   array_slide(self, offset, slice_length(slice));
   slice_buffer((struct slice *)slice, &src);
-  slice_buffer(&self->slice, &dst);
+  slice_buffer((struct slice *)array_slice(self), &dst);
   buffer_copy(&dst, offset * array_align(self), &src);
 }
 void array_remove(struct array *self, index_t offset, index_t length) {
