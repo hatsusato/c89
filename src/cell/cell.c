d160509ed7de74dfd93c8a9482bd03a79c5508c3
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-10 16:13:08 +0900

    Add cell_new

--- src/cell/cell.c
+++ src/cell/cell.c
@@ -1,7 +1,17 @@
 #include "cell.h"
 
+#include "pool/pool.h"
 #include "type.h"
-#include "util/type.h"
+#include "util/box.h"
+
+const struct cell *cell_new(struct pool *pool, const void *car,
+                            const void *cdr) {
+  struct box *box = box_new(sizeof(struct cell), 1);
+  struct cell *cell = pool_insert(pool, box);
+  cell->car = car;
+  cell->cdr = cdr;
+  return cell;
+}
 
 const struct cell *cell_nil(void) {
   static struct cell cell = {NULL, NULL};
