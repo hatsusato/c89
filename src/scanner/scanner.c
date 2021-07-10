4ef34ad9de54ddf63f8d5a9d4550839ebbcc2f1e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-10 22:53:39 +0900

    Fix scanner_init

--- src/scanner/scanner.c
+++ src/scanner/scanner.c
@@ -6,19 +6,12 @@
 #include "type.h"
 #include "util/box.h"
 
-static yyscan_t scanner_init(struct scanner *self, struct pool *pool) {
-  yyscan_t yyscan;
-  struct box *box;
-  if (yylex_init(&yyscan)) {
-    return NULL;
-  }
-  box = box_new(sizeof(struct pool_str), 1);
+static void scanner_init(struct scanner *self, struct pool *pool) {
+  struct box *box = box_new(sizeof(struct pool_str), 1);
   self->pool = pool;
   self->table = box_get(box);
   pool_str_init(self->table, pool);
   self->ast = NULL;
-  yyset_extra(self, yyscan);
-  return yyscan;
 }
 static void scanner_finish(struct scanner *self) {
   pool_str_finish(self->table);
@@ -26,14 +19,14 @@ static void scanner_finish(struct scanner *self) {
 }
 
 const struct cell *scanner_parse(struct pool *pool) {
-  struct scanner scanner;
-  yyscan_t yyscan = scanner_init(&scanner, pool);
+  struct scanner scanner = {NULL, NULL, NULL};
+  yyscan_t yyscan = yyscan_new(&scanner);
   if (yyscan) {
+    scanner_init(&scanner, pool);
     if (yyparse(yyscan)) {
       scanner.ast = NULL;
     }
     scanner_finish(&scanner);
-    yylex_destroy(yyscan);
   }
   return scanner.ast;
 }
