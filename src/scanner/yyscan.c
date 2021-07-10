2f70045468504a69fb3bc022a40c735ccd51c223
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-10 16:51:47 +0900

    Update yyerror

--- src/scanner/yyscan.c
+++ src/scanner/yyscan.c
@@ -1,10 +1,12 @@
 #include "yyscan.h"
 
+#include <stdio.h>
+
 #include "util/util.h"
 
 void yyerror(yyscan_t yyscanner, const char *msg) {
   UTIL_UNUSED(yyscanner);
-  UTIL_UNUSED(msg);
+  fprintf(stderr, "%s\n", msg);
 }
 int yyscan_is_typedef(yyscan_t yyscanner, const char *symbol) {
   UTIL_UNUSED(yyscanner);
