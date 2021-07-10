cad5f0afa4c2ae2cd8b6f531b486cdd03f81682d
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-10 22:46:51 +0900

    Add yyscan_{new,delete}

--- src/scanner/yyscan.c
+++ src/scanner/yyscan.c
@@ -2,14 +2,26 @@
 
 #include <stdio.h>
 
+#include "parser.tab.h"
 #include "util/util.h"
 
 void yyerror(yyscan_t yyscanner, const char *msg) {
   UTIL_UNUSED(yyscanner);
   fprintf(stderr, "%s\n", msg);
 }
-int yyscan_is_typedef(yyscan_t yyscanner, const char *symbol) {
-  UTIL_UNUSED(yyscanner);
+yyscan_t yyscan_new(struct scanner *scanner) {
+  yyscan_t self;
+  if (yylex_init(&self)) {
+    return NULL;
+  }
+  yyset_extra(scanner, self);
+  return self;
+}
+void yyscan_delete(yyscan_t self) {
+  yylex_destroy(self);
+}
+int yyscan_is_typedef(yyscan_t self, const char *symbol) {
+  UTIL_UNUSED(self);
   UTIL_UNUSED(symbol);
   return 0;
 }
--- src/scanner/yyscan.h
+++ src/scanner/yyscan.h
@@ -4,6 +4,8 @@
 #include "yyscan_t.h"
 
 void yyerror(yyscan_t, const char *);
+yyscan_t yyscan_new(struct scanner *);
+void yyscan_delete(yyscan_t);
 int yyscan_is_typedef(yyscan_t, const char *);
 
 #endif /* INCLUDE_GUARD_9CD5F173_4C3F_436C_9085_C797766F802E */
