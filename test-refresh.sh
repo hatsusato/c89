257b867ee43741aca3e166cf3e0de90f2393eb4b
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-12 17:51:43 +0900

    Update test-refresh.sh

--- test-refresh.sh
+++ test-refresh.sh
@@ -2,5 +2,30 @@
 
 set -eu
 
-make -j --no-print-directory -C build
-valgrind ./build/main.out --unittest
+quiet=
+
+filter-make() {
+  if [[ $quiet ]]; then
+    cat - >/dev/null
+  else
+    cat -
+  fi
+}
+memcheck() {
+  local out grep
+  if [[ $quiet ]]; then
+    grep=(grep -e exit)
+  else
+    grep=(grep -e '.*')
+  fi
+  (
+    exec {out}>&1
+    valgrind --tool=memcheck --log-fd=$out ./build/main.out "$@"
+  ) | "${grep[@]}"
+}
+
+[[  ${1-} == -q  ]] && quiet=on
+cd "${BASH_SOURCE%/*}"
+make -j --no-print-directory -C build | filter-make
+memcheck --unittest
+memcheck <test/01.c
