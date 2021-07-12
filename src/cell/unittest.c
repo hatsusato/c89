e1c1a2ca332d71a9752c20bab715920ce9cf10d6
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-12 15:54:15 +0900

    Add cell_unittest

---
+++ src/cell/unittest.c
@@ -0,0 +1,30 @@
+#include "unittest.h"
+
+#include <assert.h>
+#include <string.h>
+
+#include "cell.h"
+#include "pool/pool.h"
+#include "pool/type.h"
+
+void cell_unittest(void) {
+  const char *a = "aaaaa";
+  struct pool pool;
+  const struct cell *cell;
+  int i;
+  pool_init(&pool);
+  cell = cell_nil();
+  for (i = 0; i < 5; i++) {
+    cell = cell_push(&pool, cell, cell_new_symbol(&pool, a + i));
+  }
+  for (i = 0; cell_is_cons(cell); cell = cell_cdr(cell)) {
+    const struct cell *car = cell_car(cell);
+    const char *symbol;
+    assert(cell_is_symbol(car));
+    symbol = cell_symbol(car);
+    assert(strcmp(a + i, symbol) == 0);
+    i++;
+  }
+  assert(cell_is_nil(cell));
+  pool_finish(&pool);
+}
--- src/unittest.c
+++ src/unittest.c
@@ -1,12 +1,14 @@
 #include "unittest.h"
 
 #include "array/unittest.h"
+#include "cell/unittest.h"
 #include "pool/unittest.h"
 #include "str/unittest.h"
 #include "vec/unittest.h"
 
 void unittest(void) {
   array_unittest();
+  cell_unittest();
   pool_unittest();
   pool_str_unittest();
   str_unittest();
