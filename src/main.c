6fadb7dec64849ff9b8eb5444d487a5e546133bb
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-12 17:30:29 +0900

    Use scanner_pool in main

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
+#include "pool/pool.h"
+#include "scanner/scanner.h"
 #include "unittest.h"
+#include "util/util.h"
 
 int main(int argc, char *argv[]) {
-  (void)argc;
-  (void)argv;
+  struct pool *pool = pool_new();
+  const struct cell *ast = scanner_parse(pool);
+  UTIL_UNUSED(argc);
+  UTIL_UNUSED(argv);
+  UTIL_UNUSED(ast);
   unittest();
+  pool_delete(pool);
   return 0;
 }
