ac3f40e89d570d44b753cbd98b8938171bfb4526
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-12 17:25:59 +0900

    Remove pool_{init,finish}

--- src/pool/pool.c
+++ src/pool/pool.c
@@ -11,19 +11,13 @@ static void pool_free(void *self) {
 struct pool *pool_new(void) {
   struct box *box = box_new(sizeof(struct pool), 1);
   struct pool *self = box_get(box);
-  pool_init(self);
+  vec_ptr_init(&self->vec);
   return self;
 }
 void pool_delete(struct pool *self) {
-  pool_finish(self);
-  box_release(self);
-}
-void pool_init(struct pool *self) {
-  vec_ptr_init(&self->vec);
-}
-void pool_finish(struct pool *self) {
   vec_ptr_map(&self->vec, pool_free);
   vec_ptr_finish(&self->vec);
+  box_release(self);
 }
 void *pool_insert(struct pool *self, struct box *box) {
   vec_ptr_push(&self->vec, box);
--- src/pool/pool.h
+++ src/pool/pool.h
@@ -6,8 +6,6 @@ struct pool;
 
 struct pool *pool_new(void);
 void pool_delete(struct pool *);
-void pool_init(struct pool *);
-void pool_finish(struct pool *);
 void *pool_insert(struct pool *, struct box *);
 
 #endif /* INCLUDE_GUARD_FD4CDA16_28C2_418A_84F7_67EFEEE7B0B6 */
