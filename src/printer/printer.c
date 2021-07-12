ec19b127f5d03a5ca85153efc6250b78de141886
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-12 23:27:06 +0900

    Add printer_shift

--- src/printer/printer.c
+++ src/printer/printer.c
@@ -1,7 +1,18 @@
 #include "printer.h"
 
+#include <stdio.h>
+
 #include "type.h"
 
+void printer_shift(struct printer *self) {
+  if (self->newline && self->fp) {
+    index_t i;
+    for (i = 0; i < self->indent; i++) {
+      fputc(' ', self->fp);
+    }
+  }
+  self->newline = false;
+}
 void printer_init(struct printer *self, void *fp) {
   self->fp = fp;
   self->indent = 0;
