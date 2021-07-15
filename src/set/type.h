a6e91ee064e4ad43a30b3b130f176a6a1f8e120e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-16 00:51:56 +0900

    Add struct set

---
+++ src/set/type.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDE_GUARD_5C0909A8_A60A_4157_B759_E43DD4529228
+#define INCLUDE_GUARD_5C0909A8_A60A_4157_B759_E43DD4529228
+
+#include "util/type.h"
+#include "vec/type.h"
+
+struct set {
+  struct vec vec;
+  cmp_t cmp;
+};
+
+#endif /* INCLUDE_GUARD_5C0909A8_A60A_4157_B759_E43DD4529228 */
