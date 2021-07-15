8057c7075ebe42670703497167e13d5ae597317e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-16 01:49:20 +0900

    Add set_set_dummy

--- src/set/set.c
+++ src/set/set.c
@@ -4,6 +4,7 @@
 
 #include "type.h"
 #include "util/box.h"
+#include "util/buffer.h"
 #include "vec/vec.h"
 
 struct set_elem {
@@ -15,6 +16,13 @@ int set_cmp(const void *lhs, const void *rhs) {
   const struct set_elem *l = lhs, *r = rhs;
   assert(l->cmp == r->cmp);
   return l->cmp(&l->elem, &r->elem);
+}
+void set_set_dummy(struct set *self, const void *ptr) {
+  struct buffer src, dst;
+  size_t size = box_size(self->dummy);
+  buffer_init(&src, (void *)ptr, size - sizeof(cmp_t));
+  box_buffer(self->dummy, &dst);
+  buffer_copy(&dst, sizeof(cmp_t), &src);
 }
 
 void set_init(struct set *self, align_t align, cmp_t cmp) {
