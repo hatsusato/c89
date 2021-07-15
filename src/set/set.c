b496f221477d75e0af4b06c080280d3f39ac468d
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-16 01:25:50 +0900

    Add struct set.dummy

--- src/set/set.c
+++ src/set/set.c
@@ -3,13 +3,24 @@
 #include <assert.h>
 
 #include "type.h"
+#include "util/box.h"
 #include "vec/vec.h"
 
-void set_init(struct set *self, align_t size, cmp_t cmp) {
+struct set_elem {
+  cmp_t cmp;
+  byte_t elem[1];
+};
+
+void set_init(struct set *self, align_t align, cmp_t cmp) {
+  size_t size = sizeof(cmp_t) + align;
+  struct set_elem *elem;
   assert(cmp);
-  vec_init(&self->vec, sizeof(struct set *) + size);
-  self->cmp = cmp;
+  vec_init(&self->vec, size);
+  self->dummy = box_new(size, 1);
+  elem = box_get(self->dummy);
+  elem->cmp = cmp;
 }
 void set_finish(struct set *self) {
   vec_finish(&self->vec);
+  box_delete(self->dummy);
 }
--- src/set/type.h
+++ src/set/type.h
@@ -1,12 +1,13 @@
 #ifndef INCLUDE_GUARD_5C0909A8_A60A_4157_B759_E43DD4529228
 #define INCLUDE_GUARD_5C0909A8_A60A_4157_B759_E43DD4529228
 
-#include "util/type.h"
 #include "vec/type.h"
+
+struct box;
 
 struct set {
   struct vec vec;
-  cmp_t cmp;
+  struct box *dummy;
 };
 
 #endif /* INCLUDE_GUARD_5C0909A8_A60A_4157_B759_E43DD4529228 */
