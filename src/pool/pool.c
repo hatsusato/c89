3b54f6231f9d3da5280ee1e652490ed67471b6eb
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-18 04:07:54 +0900

    Add struct pool.pool

--- src/pool/pool.c
+++ src/pool/pool.c
@@ -1,20 +1,29 @@
 #include "pool.h"
 
+#include "array/slice.h"
 #include "type.h"
 #include "util/box.h"
+#include "util/box_type.h"
 #include "vec/ptr.h"
+#include "vec/vec.h"
 
 static void pool_free(void *self) {
   box_delete(self);
+}
+static void pool_finish(const void *self) {
+  box_finish((void *)self);
 }
 
 struct pool *pool_new(void) {
   struct box *box = box_new(sizeof(struct pool), 1);
   struct pool *self = box_get(box);
+  vec_init(&self->pool, sizeof(struct box));
   vec_ptr_init(&self->vec);
   return self;
 }
 void pool_delete(struct pool *self) {
+  slice_map(vec_slice(&self->pool), pool_finish);
+  vec_finish(&self->pool);
   vec_ptr_map(&self->vec, pool_free);
   vec_ptr_finish(&self->vec);
   box_release(self);
