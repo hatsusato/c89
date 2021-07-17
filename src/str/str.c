62bbfeb2f7f8de5f373278d198ea717805300882
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-18 02:38:38 +0900

    Add str_new

--- src/str/str.c
+++ src/str/str.c
@@ -1,8 +1,29 @@
 #include "str.h"
 
-#include "util/type.h"
+#include "util/buffer.h"
+#include "util/util.h"
+#include "view.h"
 
 struct str {
   index_t len;
   const char *ptr;
 };
+
+static void str_init(struct str *self, const char *ptr) {
+  static const char zero = '\0';
+  struct buffer src, dst;
+  index_t len = self->len;
+  buffer_init(&dst, (void *)self->ptr, len + 1);
+  buffer_init(&src, (void *)ptr, len);
+  buffer_copy(&dst, 0, &src);
+  buffer_init(&src, (void *)&zero, 1);
+  buffer_copy(&dst, len, &src);
+}
+
+struct str *str_new(const struct str_view *view) {
+  struct str *self = util_malloc(sizeof(struct str), 1);
+  self->len = str_view_length(view);
+  self->ptr = util_malloc(1, self->len + 1);
+  str_init(self, str_view_ptr(view));
+  return self;
+}
--- src/str/str.h
+++ src/str/str.h
@@ -1,3 +1,9 @@
 #ifndef INCLUDE_GUARD_C6FD6DD6_BA8B_48CA_8964_FF358D6F9282
 #define INCLUDE_GUARD_C6FD6DD6_BA8B_48CA_8964_FF358D6F9282
+
+struct str;
+struct str_view;
+
+struct str *str_new(const struct str_view *);
+
 #endif /* INCLUDE_GUARD_C6FD6DD6_BA8B_48CA_8964_FF358D6F9282 */
