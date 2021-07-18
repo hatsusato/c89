578cf031a2120f287704e2bc7ec5c4db76d6fafa
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-18 16:40:27 +0900

    Remove struct pool.vec

--- src/pool/pool.c
+++ src/pool/pool.c
@@ -5,12 +5,8 @@
 #include "util/box.h"
 #include "util/box_type.h"
 #include "util/util.h"
-#include "vec/ptr.h"
 #include "vec/vec.h"
 
-static void pool_free(void *self) {
-  box_delete(self);
-}
 static void pool_finish(const void *self) {
   box_finish((void *)self);
 }
@@ -18,14 +14,11 @@ static void pool_finish(const void *self) {
 struct pool *pool_new(void) {
   struct pool *self = util_malloc(sizeof(struct pool), 1);
   vec_init(&self->pool, sizeof(struct box));
-  vec_ptr_init(&self->vec);
   return self;
 }
 void pool_delete(struct pool *self) {
   slice_map(vec_slice(&self->pool), pool_finish);
   vec_finish(&self->pool);
-  vec_ptr_map(&self->vec, pool_free);
-  vec_ptr_finish(&self->vec);
   util_free(self);
 }
 void *pool_insert(struct pool *self, align_t align, index_t count) {
--- src/pool/type.h
+++ src/pool/type.h
@@ -1,12 +1,10 @@
 #ifndef INCLUDE_GUARD_70E6EF5A_3885_49EF_9245_5EC21F7B08D1
 #define INCLUDE_GUARD_70E6EF5A_3885_49EF_9245_5EC21F7B08D1
 
-#include "vec/ptr_type.h"
 #include "vec/type.h"
 
 struct pool {
   struct vec pool;
-  struct vec_ptr vec;
 };
 
 #endif /* INCLUDE_GUARD_70E6EF5A_3885_49EF_9245_5EC21F7B08D1 */
