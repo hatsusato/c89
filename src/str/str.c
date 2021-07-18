c0dec6608548f1b26890c510bf985f53b23d45dd
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-18 15:35:58 +0900

    Remove struct str.ptr

--- src/str/str.c
+++ src/str/str.c
@@ -8,16 +8,12 @@
 
 struct str {
   struct box box;
-  const char *ptr;
 };
 
 static void str_init(struct str *self, const char *ptr) {
   static const char zero = '\0';
   struct buffer buf;
   index_t len = str_len(self);
-  buffer_init(&buf, self->ptr, len + 1);
-  buffer_copy(&buf, 0, ptr, len);
-  buffer_copy(&buf, len, &zero, 1);
   box_buffer(&self->box, &buf);
   buffer_copy(&buf, 0, ptr, len);
   buffer_copy(&buf, len, &zero, 1);
@@ -25,19 +21,16 @@ static void str_init(struct str *self, const char *ptr) {
 
 struct str *str_new(const struct str_view *view) {
   struct str *self = util_malloc(sizeof(struct str), 1);
-  index_t len = str_view_length(view);
-  self->ptr = util_malloc(1, len + 1);
-  box_init(&self->box, 1, len + 1);
+  box_init(&self->box, 1, str_view_length(view) + 1);
   str_init(self, str_view_ptr(view));
   return self;
 }
 void str_delete(struct str *self) {
   box_finish(&self->box);
-  util_free(self->ptr);
   util_free(self);
 }
 const char *str_ptr(const struct str *self) {
-  return self->ptr;
+  return box_ptr(&self->box);
 }
 index_t str_len(const struct str *self) {
   return box_size(&self->box) - 1;
