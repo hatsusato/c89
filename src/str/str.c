07d9e37b3951737fdf9fc23f4896959f64f24fa7
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-18 15:25:33 +0900

    Add struct str.box

--- src/str/str.c
+++ src/str/str.c
@@ -1,10 +1,13 @@
 #include "str.h"
 
+#include "util/box.h"
+#include "util/box_type.h"
 #include "util/buffer.h"
 #include "util/util.h"
 #include "view.h"
 
 struct str {
+  struct box box;
   index_t len;
   const char *ptr;
 };
@@ -16,16 +19,21 @@ static void str_init(struct str *self, const char *ptr) {
   buffer_init(&buf, self->ptr, len + 1);
   buffer_copy(&buf, 0, ptr, len);
   buffer_copy(&buf, len, &zero, 1);
+  box_buffer(&self->box, &buf);
+  buffer_copy(&buf, 0, ptr, len);
+  buffer_copy(&buf, len, &zero, 1);
 }
 
 struct str *str_new(const struct str_view *view) {
   struct str *self = util_malloc(sizeof(struct str), 1);
   self->len = str_view_length(view);
   self->ptr = util_malloc(1, self->len + 1);
+  box_init(&self->box, 1, self->len + 1);
   str_init(self, str_view_ptr(view));
   return self;
 }
 void str_delete(struct str *self) {
+  box_finish(&self->box);
   util_free(self->ptr);
   util_free(self);
 }
