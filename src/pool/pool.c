501b9ae73a8fc90a320511c8d07093bdc644d7fa
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-18 15:40:33 +0900

    Update pool_{new,delete}

--- src/pool/pool.c
+++ src/pool/pool.c
@@ -4,6 +4,7 @@
 #include "type.h"
 #include "util/box.h"
 #include "util/box_type.h"
+#include "util/util.h"
 #include "vec/ptr.h"
 #include "vec/vec.h"
 
@@ -15,8 +16,7 @@ static void pool_finish(const void *self) {
 }
 
 struct pool *pool_new(void) {
-  struct box *box = box_new(sizeof(struct pool), 1);
-  struct pool *self = box_get(box);
+  struct pool *self = util_malloc(sizeof(struct pool), 1);
   vec_init(&self->pool, sizeof(struct box));
   vec_ptr_init(&self->vec);
   return self;
@@ -26,7 +26,7 @@ void pool_delete(struct pool *self) {
   vec_finish(&self->pool);
   vec_ptr_map(&self->vec, pool_free);
   vec_ptr_finish(&self->vec);
-  box_release(self);
+  util_free(self);
 }
 void *pool_insert(struct pool *self, struct box *box) {
   vec_ptr_push(&self->vec, box);
