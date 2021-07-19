5d9ba2e4952cadd3521955af0f8d15722dfd50b6
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-07-19 14:57:04 +0900

    Add set_elem_{init,finish,cmp}

--- src/set/set.c
+++ src/set/set.c
@@ -5,26 +5,33 @@
 #include "type.h"
 #include "util/box.h"
 #include "util/buffer.h"
+#include "util/util.h"
 #include "vec/vec.h"
 
 struct set_elem {
   cmp_t cmp;
-  void *ptr;
+  const void *ptr;
   byte_t elem[1];
 };
 
-static int set_cmp(const void *lhs, const void *rhs) {
+static void set_elem_init(struct set_elem *self, struct set *set,
+                          const void *ptr) {
+  struct buffer buf;
+  align_t align = set->align;
+  self->cmp = set->cmp;
+  self->ptr = util_malloc(align, 1);
+  buffer_init(&buf, self->ptr, align);
+  buffer_copy(&buf, 0, ptr, align);
+}
+static void set_elem_finish(void *ptr) {
+  struct set_elem *self = ptr;
+  util_free(self->ptr);
+}
+static int set_elem_cmp(const void *lhs, const void *rhs) {
   const struct set_elem *l = lhs, *r = rhs;
   assert(l->cmp == r->cmp);
-  return l->cmp(l->elem, r->elem);
-}
-static void set_set_dummy(struct set *self, const void *ptr) {
-  struct buffer buf;
-  box_buffer(self->dummy, &buf);
-  buffer_copy(&buf, sizeof(cmp_t) + sizeof(void *), ptr, self->align);
-}
-static const void *set_get_dummy(struct set *self) {
-  return box_get(self->dummy);
+  return l->cmp(l->ptr, r->ptr);
+  ;
 }
 
 void set_init(struct set *self, align_t align, cmp_t cmp) {
@@ -39,19 +46,23 @@ void set_init(struct set *self, align_t align, cmp_t cmp) {
   elem->cmp = cmp;
 }
 void set_finish(struct set *self) {
+  vec_map(&self->vec, set_elem_finish);
   vec_finish(&self->vec);
   box_delete(self->dummy);
 }
 void set_insert(struct set *self, const void *ptr) {
   if (!set_search(self, ptr)) {
-    set_set_dummy(self, ptr);
-    vec_push(&self->vec, set_get_dummy(self));
-    vec_sort(&self->vec, set_cmp);
+    struct set_elem elem;
+    set_elem_init(&elem, self, ptr);
+    vec_push(&self->vec, &elem);
+    vec_sort(&self->vec, set_elem_cmp);
   }
 }
 const void *set_search(struct set *self, const void *key) {
+  struct set_elem elem;
   const struct set_elem *found;
-  set_set_dummy(self, key);
-  found = vec_search(&self->vec, set_get_dummy(self), set_cmp);
-  return found ? found->elem : NULL;
+  elem.cmp = self->cmp;
+  elem.ptr = key;
+  found = vec_search(&self->vec, &elem, set_elem_cmp);
+  return found ? found->ptr : NULL;
 }
