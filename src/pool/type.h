db1afc544f679ca55a6cb2346464614bf78de2ab
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-23 21:10:17 +0900

    Add struct pool_any

--- src/pool/type.h
+++ src/pool/type.h
@@ -1,10 +1,15 @@
 #ifndef INCLUDE_GUARD_70E6EF5A_3885_49EF_9245_5EC21F7B08D1
 #define INCLUDE_GUARD_70E6EF5A_3885_49EF_9245_5EC21F7B08D1
 
+#include "vec/ptr_type.h"
 #include "vec/type.h"
 
 struct pool {
   struct vec pool;
+};
+
+struct pool_any {
+  struct vec_ptr pool;
 };
 
 #endif /* INCLUDE_GUARD_70E6EF5A_3885_49EF_9245_5EC21F7B08D1 */
