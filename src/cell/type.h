650e2b0693cd4759d2835b046ebf0398e8faa4a1
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-23 21:46:53 +0900

    Add struct cell_factory

--- src/cell/type.h
+++ src/cell/type.h
@@ -1,9 +1,15 @@
 #ifndef INCLUDE_GUARD_973F8446_963B_4832_B66D_D002FE861696
 #define INCLUDE_GUARD_973F8446_963B_4832_B66D_D002FE861696
 
+struct pool_any;
+
 struct cell {
   const void *car;
   const struct cell *cdr;
+};
+
+struct cell_factory {
+  struct pool_any *pool;
 };
 
 #endif /* INCLUDE_GUARD_973F8446_963B_4832_B66D_D002FE861696 */
