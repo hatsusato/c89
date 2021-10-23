c7ff5b23e026ac2ce387d0e671888d8299f289aa
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-23 23:31:39 +0900

    Add cell_factory_make

--- src/cell/factory.c
+++ src/cell/factory.c
@@ -1,7 +1,16 @@
 #include "factory.h"
 
+#include "pool/any.h"
 #include "type.h"
 #include "util/util.h"
+
+const struct cell *cell_factory_make(struct cell_factory *self, const void *car,
+                                     const void *cdr) {
+  struct cell *cons = pool_any_alloc(self->pool, sizeof(struct cell));
+  cons->car = car;
+  cons->cdr = cdr;
+  return cons;
+}
 
 struct cell_factory *cell_factory_new(struct pool_any *pool,
                                       struct set_symbol *set) {
