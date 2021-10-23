d573589e057aa4ad25f425672b667f8a51c927e2
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-23 21:33:16 +0900

    Add pool_any_{new,delete}

--- src/pool/any.c
+++ src/pool/any.c
@@ -1,1 +1,19 @@
 #include "any.h"
+
+#include "type.h"
+#include "util/util.h"
+#include "vec/ptr.h"
+
+struct pool_any *pool_any_new(void) {
+  struct pool_any *self = util_malloc(sizeof(struct pool_any), 1);
+  vec_ptr_init(&self->pool);
+  return self;
+}
+void pool_any_delete(struct pool_any *self) {
+  index_t i, length = vec_ptr_length(&self->pool);
+  for (i = 0; i < length; i++) {
+    void *elem = vec_ptr_at(&self->pool, i);
+    util_free(elem);
+  }
+  util_free(self);
+}
--- src/pool/any.h
+++ src/pool/any.h
@@ -1,3 +1,9 @@
 #ifndef INCLUDE_GUARD_76DFC357_C3ED_4072_88BC_20349BB0834A
 #define INCLUDE_GUARD_76DFC357_C3ED_4072_88BC_20349BB0834A
+
+struct pool_any;
+
+struct pool_any *pool_any_new(void);
+void pool_any_delete(struct pool_any *);
+
 #endif /* INCLUDE_GUARD_76DFC357_C3ED_4072_88BC_20349BB0834A */
