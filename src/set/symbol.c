b4d7e1c0fc3d588f8468754791592198540b84af
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-23 22:20:35 +0900

    Add set_symbol_{new,delete}

--- src/set/symbol.c
+++ src/set/symbol.c
@@ -1,1 +1,20 @@
 #include "symbol.h"
+
+#include "type.h"
+#include "util/util.h"
+#include "vec/ptr.h"
+
+struct set_symbol *set_symbol_new(void) {
+  struct set_symbol *self = util_malloc(sizeof(struct set_symbol), 1);
+  vec_ptr_init(self->vec);
+  return self;
+}
+void set_symbol_delete(struct set_symbol *self) {
+  index_t i, length = vec_ptr_length(self->vec);
+  for (i = 0; i < length; i++) {
+    const char *symbol = vec_ptr_at(self->vec, i);
+    util_free(symbol);
+  }
+  vec_ptr_finish(self->vec);
+  util_free(self);
+}
--- src/set/symbol.h
+++ src/set/symbol.h
@@ -1,3 +1,9 @@
 #ifndef INCLUDE_GUARD_AF5DEE63_3A1E_44DE_B166_B15DCBB95A37
 #define INCLUDE_GUARD_AF5DEE63_3A1E_44DE_B166_B15DCBB95A37
+
+struct set_symbol;
+
+struct set_symbol *set_symbol_new(void);
+void set_symbol_delete(struct set_symbol *);
+
 #endif /* INCLUDE_GUARD_AF5DEE63_3A1E_44DE_B166_B15DCBB95A37 */
