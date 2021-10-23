8ce809f503dcaa2c168b855b9ae52813df6b4675
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-23 21:53:27 +0900

    Add cell_factory_{new,delete}

--- src/cell/factory.c
+++ src/cell/factory.c
@@ -1,1 +1,13 @@
 #include "factory.h"
+
+#include "type.h"
+#include "util/util.h"
+
+struct cell_factory *cell_factory_new(struct pool_any *pool) {
+  struct cell_factory *self = util_malloc(sizeof(struct cell_factory), 1);
+  self->pool = pool;
+  return self;
+}
+void cell_factory_delete(struct cell_factory *self) {
+  util_free(self);
+}
--- src/cell/factory.h
+++ src/cell/factory.h
@@ -1,3 +1,10 @@
 #ifndef INCLUDE_GUARD_2279A9B0_1CB3_48E5_BEA4_AFE820D7CFD1
 #define INCLUDE_GUARD_2279A9B0_1CB3_48E5_BEA4_AFE820D7CFD1
+
+struct cell_factory;
+struct pool_any;
+
+struct cell_factory *cell_factory_new(struct pool_any *);
+void cell_factory_delete(struct cell_factory *);
+
 #endif /* INCLUDE_GUARD_2279A9B0_1CB3_48E5_BEA4_AFE820D7CFD1 */
