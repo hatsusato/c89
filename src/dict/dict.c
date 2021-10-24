41b92c4ed7256cdabe86864220c75bef52894deb
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-25 01:45:13 +0900

    Add dict_insert

--- src/dict/dict.c
+++ src/dict/dict.c
@@ -4,12 +4,28 @@
 #include "util/util.h"
 #include "vec/vec.h"
 
+struct dict_entry {
+  const char *key;
+  void *val;
+};
+
+static void dict_entry_free(void *entry) {
+  util_free(entry);
+}
+
 struct dict *dict_new(void) {
   struct dict *self = util_malloc(sizeof(struct dict), 1);
   self->vec = vec_new();
   return self;
 }
 void dict_delete(struct dict *self) {
+  vec_map(self->vec, dict_entry_free);
   vec_delete(self->vec);
   util_free(self);
+}
+void dict_insert(struct dict *self, const char *key, void *val) {
+  struct dict_entry *entry = util_malloc(sizeof(struct dict_entry), 1);
+  entry->key = key;
+  entry->val = val;
+  vec_push(self->vec, entry);
 }
--- src/dict/dict.h
+++ src/dict/dict.h
@@ -3,5 +3,6 @@
 
 struct dict *dict_new(void);
 void dict_delete(struct dict *);
+void dict_insert(struct dict *, const char *, void *);
 
 #endif /* INCLUDE_GUARD_8FD4DA21_88F9_44C7_9BDC_CEE87486F9A0 */
