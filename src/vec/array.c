a698db9e55b9dc67af371ebaf4df5b37a0f9d41e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-24 20:29:26 +0900

    Add vec_array_set

--- src/vec/array.c
+++ src/vec/array.c
@@ -9,3 +9,8 @@ void vec_array_init(struct vec_array *self, align_t align) {
   self->align = align;
   self->count = 0;
 }
+void vec_array_set(struct vec_array *self, void *ptr, index_t count) {
+  assert(count == (ptr ? count : 0));
+  self->ptr = ptr;
+  self->count = count;
+}
--- src/vec/array.h
+++ src/vec/array.h
@@ -6,5 +6,6 @@
 struct vec_array;
 
 void vec_array_init(struct vec_array *, align_t);
+void vec_array_set(struct vec_array *, void *, index_t);
 
 #endif /* INCLUDE_GUARD_C229D27C_DE8E_4AE2_A417_79D5A6C324D7 */
