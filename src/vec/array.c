537a8a846d1588ee582a12380feba3d0884305dd
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-24 20:24:02 +0900

    Add vec_array_init

--- src/vec/array.c
+++ src/vec/array.c
@@ -1,1 +1,11 @@
 #include "array.h"
+
+#include "type.h"
+#include "util/util.h"
+
+void vec_array_init(struct vec_array *self, align_t align) {
+  assert(0 < align);
+  self->ptr = NULL;
+  self->align = align;
+  self->count = 0;
+}
--- src/vec/array.h
+++ src/vec/array.h
@@ -1,6 +1,10 @@
 #ifndef INCLUDE_GUARD_C229D27C_DE8E_4AE2_A417_79D5A6C324D7
 #define INCLUDE_GUARD_C229D27C_DE8E_4AE2_A417_79D5A6C324D7
 
+#include "util/type.h"
+
 struct vec_array;
+
+void vec_array_init(struct vec_array *, align_t);
 
 #endif /* INCLUDE_GUARD_C229D27C_DE8E_4AE2_A417_79D5A6C324D7 */
