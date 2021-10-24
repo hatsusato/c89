3bef7efd67dec4d289754cc9855fb5fc35523912
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-25 00:55:57 +0900

    Remove struct vec

--- src/vec/type.h
+++ src/vec/type.h
@@ -1,20 +1,12 @@
 #ifndef INCLUDE_GUARD_05A3735D_C33C_4CD4_AF36_3968DD09F48F
 #define INCLUDE_GUARD_05A3735D_C33C_4CD4_AF36_3968DD09F48F
 
-#include "array/type.h"
-#include "util/box_type.h"
-
-struct box;
+#include "util/type.h"
 
 struct vec_array {
   byte_t *ptr;
   align_t align;
   index_t count;
-};
-
-struct vec {
-  struct array array;
-  struct box box;
 };
 
 struct vec_ptr {
