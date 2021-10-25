b01f202ff3e4e0a58a10e2f10e216ef3ef8c0298
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-26 00:57:49 +0900

    Update dict_insert

--- src/dict/dict.c
+++ src/dict/dict.c
@@ -16,11 +16,10 @@ static int dict_cmp(const void *lhs, const void *rhs) {
   const struct dict_entry *const *l = lhs, *const *r = rhs;
   return util_strcmp((*l)->key, (*r)->key);
 }
-static const struct dict_entry *dict_search(struct dict *self,
-                                            const char *key) {
+static struct dict_entry *dict_search(struct dict *self, const char *key) {
   struct dict_entry entry = {NULL, NULL};
   entry.key = key;
-  return vec_search(self->vec, &entry, dict_cmp);
+  return (void *)vec_search(self->vec, &entry, dict_cmp);
 }
 
 struct dict *dict_new(void) {
@@ -34,11 +33,16 @@ void dict_delete(struct dict *self) {
   util_free(self);
 }
 void dict_insert(struct dict *self, const char *key, void *val) {
-  struct dict_entry *entry = util_malloc(sizeof(struct dict_entry), 1);
-  entry->key = key;
-  entry->val = val;
-  vec_push(self->vec, entry);
-  vec_sort(self->vec, dict_cmp);
+  struct dict_entry *entry = dict_search(self, key);
+  if (entry) {
+    entry->val = val;
+  } else {
+    entry = util_malloc(sizeof(struct dict_entry), 1);
+    entry->key = key;
+    entry->val = val;
+    vec_push(self->vec, entry);
+    vec_sort(self->vec, dict_cmp);
+  }
 }
 void *dict_find(struct dict *self, const char *key) {
   const struct dict_entry *found = dict_search(self, key);
