51fed1e382a099e626ddd39d33a97e13c6a2fdec
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-26 02:38:13 +0900

    Add array subdirectory

--- src/vec/type.h
+++ src/vec/type.h
@@ -1,13 +1,8 @@
 #ifndef INCLUDE_GUARD_05A3735D_C33C_4CD4_AF36_3968DD09F48F
 #define INCLUDE_GUARD_05A3735D_C33C_4CD4_AF36_3968DD09F48F
 
+#include "array/type.h"
 #include "util/type.h"
-
-struct vec_array {
-  byte_t *ptr;
-  align_t align;
-  index_t count;
-};
 
 struct vec {
   struct vec_array array;
