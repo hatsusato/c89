dfeca0f8cb67e1cb0393a24a1d6c7fbf9d1f9584
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-27 00:34:41 +0900

    Add typedef_contains

--- src/scanner/typedef.c
+++ src/scanner/typedef.c
@@ -1,1 +1,22 @@
 #include "typedef.h"
+
+#include "cell/cell.h"
+#include "cell/visitor.h"
+#include "dict/dict.h"
+#include "util/util.h"
+
+static void typedef_set_bool(const struct cell *cell, void *extra) {
+  bool_t *flag = extra;
+  *flag = true;
+  UTIL_UNUSED(cell);
+}
+
+bool_t typedef_contains(const struct cell *decl) {
+  bool_t contains = false;
+  struct dict *visitors = dict_new();
+  struct cell_visitor_wrapper wrapper = {typedef_set_bool};
+  dict_insert(visitors, "typedef", &wrapper);
+  cell_visitor(cell_at(decl, 1), visitors, &contains);
+  dict_delete(visitors);
+  return contains;
+}
--- src/scanner/typedef.h
+++ src/scanner/typedef.h
@@ -1,6 +1,10 @@
 #ifndef INCLUDE_GUARD_CE86B38E_9EFD_4EB5_99FA_CA123C9ACB68
 #define INCLUDE_GUARD_CE86B38E_9EFD_4EB5_99FA_CA123C9ACB68
 
-#include "yyscan_t.h"
+#include "util/type.h"
+
+struct cell;
+
+bool_t typedef_contains(const struct cell *);
 
 #endif /* INCLUDE_GUARD_CE86B38E_9EFD_4EB5_99FA_CA123C9ACB68 */
