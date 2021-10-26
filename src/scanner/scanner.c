5eb71d37d698f30cd6e3185d65f213b6c4291221
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-26 21:39:27 +0900

    Add struct scanner.typedefs

--- src/scanner/scanner.c
+++ src/scanner/scanner.c
@@ -1,16 +1,19 @@
 #include "scanner.h"
 
 #include "cell/factory.h"
+#include "set/set.h"
 #include "type.h"
 #include "util/util.h"
 #include "yyscan.h"
 
 const struct cell *scanner_parse(struct pool *pool, struct set *symbols) {
-  struct scanner scanner = {NULL, NULL};
+  struct scanner scanner = {NULL, NULL, NULL};
   yyscan_t yyscan = yyscan_new(&scanner);
   if (yyscan) {
     scanner.factory = cell_factory_new(pool, symbols);
+    scanner.typedefs = set_new();
     yyscan_parse(yyscan);
+    set_delete(scanner.typedefs);
     cell_factory_delete(scanner.factory);
   }
   yyscan_delete(yyscan);
