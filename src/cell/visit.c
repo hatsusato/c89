59ec37404aa61d540e335ef54dce717d7ed7847a
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-26 21:04:45 +0900

    Add cell_visit

--- src/cell/visit.c
+++ src/cell/visit.c
@@ -1,1 +1,28 @@
 #include "visit.h"
+
+#include "cell.h"
+#include "dict/dict.h"
+#include "util/util.h"
+
+static cell_visitor_t cell_visit_match(const struct cell *car,
+                                       const struct dict *visitors) {
+  void *visitor = NULL;
+  if (cell_is_symbol(car)) {
+    visitor = dict_find((struct dict *)visitors, cell_symbol(car));
+  }
+  return (cell_visitor_t)visitor;
+}
+
+void cell_visit(const struct cell *it, const struct dict *visitors,
+                void *extra) {
+  if (cell_is_list(it)) {
+    cell_visitor_t visitor = cell_visit_match(cell_car(it), visitors);
+    if (visitor) {
+      visitor(it, extra);
+    } else {
+      for (; cell_is_cons(it); it = cell_cdr(it)) {
+        cell_visit(cell_car(it), visitors, extra);
+      }
+    }
+  }
+}
--- src/cell/visit.h
+++ src/cell/visit.h
@@ -2,6 +2,9 @@
 #define INCLUDE_GUARD_1EE525F3_DB92_4F96_8208_3D730F3BA78F
 
 struct cell;
+struct dict;
 typedef void (*cell_visitor_t)(const struct cell *, void *);
+
+void cell_visit(const struct cell *, const struct dict *, void *);
 
 #endif /* INCLUDE_GUARD_1EE525F3_DB92_4F96_8208_3D730F3BA78F */
