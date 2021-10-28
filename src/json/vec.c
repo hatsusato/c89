6a974d4f3dd4130a06e0cc54784feb9a0e2d280a
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-28 23:27:14 +0900

    Add json_vec_reserve
    and json_vec_capacity_ceil

--- src/json/vec.c
+++ src/json/vec.c
@@ -1,7 +1,23 @@
 #include "vec.h"
 
+#include "array.h"
 #include "type.h"
 #include "util/util.h"
+
+static index_t json_vec_capacity_ceil(index_t capacity) {
+  enum { initial_capacity = 8 };
+  index_t ceil = initial_capacity;
+  while (ceil < capacity) {
+    ceil *= 2;
+  }
+  return ceil;
+}
+void json_vec_reserve(struct json_vec *self, index_t capacity) {
+  if (self->capacity < capacity) {
+    self->capacity = json_vec_capacity_ceil(capacity);
+    json_array_resize(&self->array, self->capacity);
+  }
+}
 
 struct json_vec *json_vec_new(void) {
   struct json_vec *self = util_malloc(sizeof(struct json_vec));
