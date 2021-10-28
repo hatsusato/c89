90c5f4f6374b319d4323a0ac2a6d5aab41b5b4e3
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-29 01:39:26 +0900

    Add json_factory_{new,delete}

--- src/json/factory.c
+++ src/json/factory.c
@@ -1,1 +1,13 @@
 #include "factory.h"
+
+#include "type.h"
+#include "util/util.h"
+
+struct json_factory *json_factory_new(struct pool *pool) {
+  struct json_factory *self = util_malloc(sizeof(struct json_factory));
+  self->pool = pool;
+  return self;
+}
+void json_factory_delete(struct json_factory *self) {
+  util_free(self);
+}
