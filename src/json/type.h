011e05d02a044838b4d976e027e202411d91840a
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-28 22:56:24 +0900

    Add struct json_array

--- src/json/type.h
+++ src/json/type.h
@@ -1,6 +1,13 @@
 #ifndef INCLUDE_GUARD_B6B56A7D_817F_4841_A842_6C0985520EBD
 #define INCLUDE_GUARD_B6B56A7D_817F_4841_A842_6C0985520EBD
 
+#include "util/type.h"
+
 struct json;
+
+struct json_array {
+  struct json *base;
+  index_t count;
+};
 
 #endif /* INCLUDE_GUARD_B6B56A7D_817F_4841_A842_6C0985520EBD */
