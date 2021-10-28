68f9a8502bb0b5a7328070245255f825c5fdee3f
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-28 23:16:46 +0900

    Add json_array_sort

--- src/json/array.c
+++ src/json/array.c
@@ -1,13 +1,15 @@
 #include "array.h"
 
+#include <stdlib.h>
 #include <string.h>
 
 #include "type.h"
 #include "util/util.h"
 
+static const align_t align = sizeof(struct json);
+
 void json_array_insert(struct json_array *self, const struct json *base,
                        index_t count) {
-  static const align_t align = sizeof(struct json);
   if (0 < count) {
     assert(self->base && base);
     memcpy(self->base + align * self->count, base, align * count);
@@ -15,10 +17,14 @@ void json_array_insert(struct json_array *self, const struct json *base,
   }
 }
 void json_array_resize(struct json_array *self, index_t count) {
-  static const align_t align = sizeof(struct json);
   struct json_array tmp = {NULL, 0};
   tmp.base = util_malloc_array(align, count);
   json_array_insert(&tmp, self->base, UTIL_MIN(self->count, count));
   UTIL_SWAP(struct json_array, self, &tmp);
   util_free(tmp.base);
+}
+void json_array_sort(struct json_array *self, cmp_t cmp) {
+  if (self->base) {
+    qsort(self->base, self->count, align, cmp);
+  }
 }
--- src/json/array.h
+++ src/json/array.h
@@ -8,5 +8,6 @@ struct json_array;
 
 void json_array_insert(struct json_array *, const struct json *, index_t);
 void json_array_resize(struct json_array *, index_t);
+void json_array_sort(struct json_array *, cmp_t);
 
 #endif /* INCLUDE_GUARD_CDB9EC8A_96C8_4113_8118_8E0D2E8316D8 */
