07bf23e481f76ecbe1b4fcd2cf561e3b2dc6479f
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-28 09:21:42 +0900

    Add struct cell_visitor

--- src/cell/type.h
+++ src/cell/type.h
@@ -1,6 +1,7 @@
 #ifndef INCLUDE_GUARD_973F8446_963B_4832_B66D_D002FE861696
 #define INCLUDE_GUARD_973F8446_963B_4832_B66D_D002FE861696
 
+struct dict;
 struct pool;
 struct set;
 
@@ -12,6 +13,12 @@ struct cell {
 struct cell_factory {
   struct pool *pool;
   struct set *set;
+};
+
+struct cell_visitor {
+  struct pool *wrappers;
+  struct dict *visitors;
+  void *extra;
 };
 
 #endif /* INCLUDE_GUARD_973F8446_963B_4832_B66D_D002FE861696 */
