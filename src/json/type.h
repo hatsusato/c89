c0ed7852b2b9f0fc489a31994a6c0d760020bb98
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-28 23:22:11 +0900

    Add struct json_vec

--- src/json/type.h
+++ src/json/type.h
@@ -3,14 +3,21 @@
 
 #include "util/type.h"
 
+struct json_vec;
+
 struct json {
   const char *key;
-  void *val;
+  struct json_vec *val;
 };
 
 struct json_array {
   struct json *base;
   index_t count;
+};
+
+struct json_vec {
+  struct json_array array;
+  index_t capacity;
 };
 
 #endif /* INCLUDE_GUARD_B6B56A7D_817F_4841_A842_6C0985520EBD */
