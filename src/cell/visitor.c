3ed3ad7e18593f5cc3cf644e7825486888dd4bc8
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-28 09:23:18 +0900

    Add cell_visitor_{new,delete}

--- src/cell/visitor.c
+++ src/cell/visitor.c
@@ -2,6 +2,8 @@
 
 #include "cell.h"
 #include "dict/dict.h"
+#include "pool/pool.h"
+#include "type.h"
 #include "util/util.h"
 
 static cell_visitor_t cell_visitor_extract(const struct cell *car,
@@ -29,4 +31,17 @@ bool_t cell_visitor_noop(const struct cell *self, void *extra) {
   UTIL_UNUSED(self);
   UTIL_UNUSED(extra);
   return true;
+}
+
+struct cell_visitor *cell_visitor_new(void *extra) {
+  struct cell_visitor *self = util_malloc(sizeof(struct cell_visitor));
+  self->wrappers = pool_new();
+  self->visitors = dict_new();
+  self->extra = extra;
+  return self;
+}
+void cell_visitor_delete(struct cell_visitor *self) {
+  dict_delete(self->visitors);
+  pool_delete(self->wrappers);
+  util_free(self);
 }
--- src/cell/visitor.h
+++ src/cell/visitor.h
@@ -4,6 +4,7 @@
 #include "util/type.h"
 
 struct cell;
+struct cell_visitor;
 struct dict;
 typedef bool_t (*cell_visitor_t)(const struct cell *, void *);
 struct cell_visitor_wrapper {
@@ -12,5 +13,8 @@ struct cell_visitor_wrapper {
 
 void cell_visitor(const struct cell *, const struct dict *, void *);
 bool_t cell_visitor_noop(const struct cell *, void *);
+
+struct cell_visitor *cell_visitor_new(void *);
+void cell_visitor_delete(struct cell_visitor *);
 
 #endif /* INCLUDE_GUARD_1EE525F3_DB92_4F96_8208_3D730F3BA78F */
