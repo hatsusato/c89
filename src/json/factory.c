c96911bb56073ba58961950bfd0388d6d05c170b
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-29 01:44:13 +0900

    Add json_factory_alloc

--- src/json/factory.c
+++ src/json/factory.c
@@ -1,7 +1,16 @@
 #include "factory.h"
 
+#include "pool/pool.h"
 #include "type.h"
 #include "util/util.h"
+
+struct json *json_factory_alloc(struct json_factory *self, enum json_tag tag) {
+  struct json *json = pool_alloc(self->pool, sizeof(struct json));
+  json->tag = tag;
+  json->str = NULL;
+  json->vec = NULL;
+  return json;
+}
 
 struct json_factory *json_factory_new(struct pool *pool) {
   struct json_factory *self = util_malloc(sizeof(struct json_factory));
