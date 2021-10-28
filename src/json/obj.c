d7f53a12450d393d50a645daa6450bd6bd42bf70
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-29 00:34:48 +0900

    Add json_obj_insert

--- src/json/obj.c
+++ src/json/obj.c
@@ -4,6 +4,11 @@
 #include "util/util.h"
 #include "vec.h"
 
+static int json_obj_cmp(const void *lhs, const void *rhs) {
+  const struct json_pair *l = lhs, *r = rhs;
+  return util_strcmp(l->key, r->key);
+}
+
 struct json *json_obj_new(void) {
   struct json *self = util_malloc(sizeof(struct json));
   self->str = NULL;
@@ -13,4 +18,13 @@ struct json *json_obj_new(void) {
 void json_obj_delete(struct json *self) {
   json_vec_delete(self->vec);
   util_free(self);
+}
+void json_obj_insert(struct json *self, const char *key, struct json *val) {
+  struct json_pair *pair = json_vec_search(self->vec, key, NULL, json_obj_cmp);
+  if (pair) {
+    pair->val = val;
+  } else {
+    json_vec_push(self->vec, key, val);
+    json_vec_sort(self->vec, json_obj_cmp);
+  }
 }
--- src/json/obj.h
+++ src/json/obj.h
@@ -3,5 +3,6 @@
 
 struct json *json_obj_new(void);
 void json_obj_delete(struct json *);
+void json_obj_insert(struct json *, const char *, struct json *);
 
 #endif /* INCLUDE_GUARD_1A3501D5_6CCC_4A43_9369_088AA3991F18 */
