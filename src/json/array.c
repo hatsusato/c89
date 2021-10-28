8ec390af9c889851fc11fe95737b515c5f470134
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-28 23:09:50 +0900

    Add json_array_insert

--- src/json/array.c
+++ src/json/array.c
@@ -1,1 +1,16 @@
 #include "array.h"
+
+#include <string.h>
+
+#include "type.h"
+#include "util/util.h"
+
+void json_array_insert(struct json_array *self, const struct json *base,
+                       index_t count) {
+  static const align_t align = sizeof(struct json);
+  if (0 < count) {
+    assert(self->base && base);
+    memcpy(self->base + align * self->count, base, align * count);
+    self->count += count;
+  }
+}
--- src/json/array.h
+++ src/json/array.h
@@ -1,6 +1,10 @@
 #ifndef INCLUDE_GUARD_CDB9EC8A_96C8_4113_8118_8E0D2E8316D8
 #define INCLUDE_GUARD_CDB9EC8A_96C8_4113_8118_8E0D2E8316D8
 
+#include "util/type.h"
+
+struct json;
 struct json_array;
 
+void json_array_insert(struct json_array *, const struct json *, index_t);
 #endif /* INCLUDE_GUARD_CDB9EC8A_96C8_4113_8118_8E0D2E8316D8 */
