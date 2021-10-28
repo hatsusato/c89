27e86dcbe0d3321929c4ae800219f42be233d84f
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-28 23:25:13 +0900

    Add json_vec_{new,delete}

--- src/json/vec.c
+++ src/json/vec.c
@@ -1,1 +1,15 @@
 #include "vec.h"
+
+#include "type.h"
+#include "util/util.h"
+
+struct json_vec *json_vec_new(void) {
+  struct json_vec *self = util_malloc(sizeof(struct json_vec));
+  self->array.base = NULL;
+  self->array.count = self->capacity = 0;
+  return self;
+}
+void json_vec_delete(struct json_vec *self) {
+  util_free(self->array.base);
+  util_free(self);
+}
--- src/json/vec.h
+++ src/json/vec.h
@@ -3,4 +3,7 @@
 
 struct json_vec;
 
+struct json_vec *json_vec_new(void);
+void json_vec_delete(struct json_vec *);
+
 #endif /* INCLUDE_GUARD_28ECEBB7_B3F3_4129_987F_AA9D1F2203BF */
