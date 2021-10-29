d546a97a4c8107dda34e6ce7c56a168f605bc81b
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-29 09:38:45 +0900

    Add json_arr_{get,set,push}

--- src/json/json.c
+++ src/json/json.c
@@ -8,6 +8,23 @@ struct json *json_null(void) {
   static struct json null = {JSON_TAG_NULL, NULL, NULL};
   return &null;
 }
+struct json *json_arr_get(struct json *self, index_t key) {
+  struct json_pair *pair;
+  assert(self->tag == JSON_TAG_ARR);
+  pair = json_vec_at(self->vec, key);
+  return pair ? pair->val : NULL;
+}
+void json_arr_set(struct json *self, index_t key, struct json *val) {
+  struct json_pair *pair;
+  assert(self->tag == JSON_TAG_ARR);
+  pair = json_vec_at(self->vec, key);
+  assert(pair);
+  pair->val = val;
+}
+void json_arr_push(struct json *self, struct json *val) {
+  assert(self->tag == JSON_TAG_ARR);
+  json_vec_push(self->vec, NULL, val);
+}
 struct json *json_obj_get(struct json *self, const char *key) {
   struct json_pair *pair;
   assert(self->tag == JSON_TAG_OBJ);
--- src/json/json.h
+++ src/json/json.h
@@ -1,9 +1,14 @@
 #ifndef INCLUDE_GUARD_5E299705_D2B2_4EFA_AD2B_575DB876526E
 #define INCLUDE_GUARD_5E299705_D2B2_4EFA_AD2B_575DB876526E
 
+#include "util/type.h"
+
 struct json;
 
 struct json *json_null(void);
+struct json *json_arr_get(struct json *, index_t);
+void json_arr_set(struct json *, index_t, struct json *);
+void json_arr_push(struct json *, struct json *);
 struct json *json_obj_get(struct json *, const char *);
 void json_obj_set(struct json *, const char *, struct json *);
 
