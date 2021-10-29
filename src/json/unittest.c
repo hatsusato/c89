02fe4118e1a583e3204134892bd81ac964bbda5a
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-29 11:45:07 +0900

    Add json_arr_unittest

--- src/json/unittest.c
+++ src/json/unittest.c
@@ -4,6 +4,33 @@
 #include "json.h"
 #include "util/util.h"
 
+void json_arr_unittest(void) {
+  struct json_factory *factory = json_factory_new();
+  struct json *arr = json_factory_arr(factory);
+  index_t i, count = 100;
+  char(*str)[2] = util_malloc_array(sizeof(char[2]), count);
+  for (i = 0; i < count; i++) {
+    str[i][0] = 'a' + (i * 100) % 26;
+    str[i][1] = 0;
+  }
+  assert(!json_arr_get(arr, 0));
+  for (i = 0; i < count; i++) {
+    struct json *val = json_factory_arr(factory);
+    json_arr_push(arr, val);
+  }
+  for (i = 0; i < count; i++) {
+    assert(json_arr_get(arr, i));
+    json_arr_set(arr, i, json_factory_str(factory, str[i]));
+  }
+  for (i = 0; i < count; i++) {
+    struct json *elem = json_arr_get(arr, i);
+    assert(elem && json_is_str(elem));
+    assert(util_streq(json_str_get(elem), str[i]));
+  }
+  util_free(str);
+  json_factory_delete(factory);
+}
+
 void json_obj_unittest(void) {
   struct json_factory *factory = json_factory_new();
   struct json *obj = json_factory_obj(factory);
@@ -28,5 +55,6 @@ void json_obj_unittest(void) {
 }
 
 void json_unittest(void) {
+  json_arr_unittest();
   json_obj_unittest();
 }
