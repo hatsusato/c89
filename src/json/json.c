caaa6c6beb7eb63cb8cb97bb601c79c2b6ade29b
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-29 09:28:17 +0900

    Add json_obj_{get,set}

--- src/json/json.c
+++ src/json/json.c
@@ -1,8 +1,26 @@
 #include "json.h"
 
 #include "type.h"
+#include "util/util.h"
+#include "vec.h"
 
 struct json *json_null(void) {
   static struct json null = {JSON_TAG_NULL, NULL, NULL};
   return &null;
+}
+struct json *json_obj_get(struct json *self, const char *key) {
+  struct json_pair *pair;
+  assert(self->tag == JSON_TAG_OBJ);
+  pair = json_vec_find(self->vec, key);
+  return pair ? pair->val : NULL;
+}
+void json_obj_set(struct json *self, const char *key, struct json *val) {
+  struct json_pair *pair;
+  assert(self->tag == JSON_TAG_OBJ);
+  pair = json_vec_find(self->vec, key);
+  if (pair) {
+    pair->val = val;
+  } else {
+    json_vec_push(self->vec, key, val);
+  }
 }
--- src/json/json.h
+++ src/json/json.h
@@ -4,5 +4,7 @@
 struct json;
 
 struct json *json_null(void);
+struct json *json_obj_get(struct json *, const char *);
+void json_obj_set(struct json *, const char *, struct json *);
 
 #endif /* INCLUDE_GUARD_5E299705_D2B2_4EFA_AD2B_575DB876526E */
