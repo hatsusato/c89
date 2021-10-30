5464691cc31a89ef3085a2aea94556dde1517bb7
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-30 14:36:56 +0900

    Rename json_print_*

--- src/json/printer.c
+++ src/json/printer.c
@@ -5,17 +5,16 @@
 #include "type.h"
 #include "util/util.h"
 
-static void json_print_rec(struct json *json, struct printer *printer);
-static void json_print_null(struct json *json, struct printer *printer) {
+static void json_printer_null(struct json *json, struct printer *printer) {
   UTIL_UNUSED(json);
   assert(json->tag == JSON_TAG_NULL);
   printer_print(printer, "null");
 }
-static void json_print_str(struct json *json, struct printer *printer) {
+static void json_printer_str(struct json *json, struct printer *printer) {
   assert(json->tag == JSON_TAG_STR);
   printer_print(printer, "\"%s\"", json_str_get(json));
 }
-static void json_print_arr(struct json *json, struct printer *printer) {
+static void json_printer_arr(struct json *json, struct printer *printer) {
   index_t i, count;
   assert(json->tag == JSON_TAG_ARR);
   count = json_count(json);
@@ -23,18 +22,18 @@ static void json_print_arr(struct json *json, struct printer *printer) {
   if (0 < count) {
     printer_indent(printer, 2);
     printer_newline(printer);
-    json_print_rec(json_arr_get(json, 0), printer);
+    json_printer_print(json_arr_get(json, 0), printer);
     for (i = 1; i < count; i++) {
       printer_print(printer, ",");
       printer_newline(printer);
-      json_print_rec(json_arr_get(json, i), printer);
+      json_printer_print(json_arr_get(json, i), printer);
     }
     printer_indent(printer, -2);
     printer_newline(printer);
   }
   printer_print(printer, "]");
 }
-static void json_print_obj(struct json *json, struct printer *printer) {
+static void json_printer_obj(struct json *json, struct printer *printer) {
   index_t i, count;
   assert(json->tag == JSON_TAG_OBJ);
   count = json_count(json);
@@ -43,38 +42,38 @@ static void json_print_obj(struct json *json, struct printer *printer) {
     printer_indent(printer, 2);
     printer_newline(printer);
     printer_print(printer, "\"%s\": ", json_obj_key(json, 0));
-    json_print_rec(json_obj_val(json, 0), printer);
+    json_printer_print(json_obj_val(json, 0), printer);
     for (i = 1; i < count; i++) {
       printer_print(printer, ",");
       printer_newline(printer);
       printer_print(printer, "\"%s\": ", json_obj_key(json, i));
-      json_print_rec(json_obj_val(json, i), printer);
+      json_printer_print(json_obj_val(json, i), printer);
     }
     printer_indent(printer, -2);
     printer_newline(printer);
   }
   printer_print(printer, "}");
 }
-static void json_print_rec(struct json *json, struct printer *printer) {
+
+void json_printer_print(struct json *json, struct printer *printer) {
   switch (json->tag) {
   case JSON_TAG_NULL:
-    json_print_null(json, printer);
+    json_printer_null(json, printer);
     break;
   case JSON_TAG_STR:
-    json_print_str(json, printer);
+    json_printer_str(json, printer);
     break;
   case JSON_TAG_ARR:
-    json_print_arr(json, printer);
+    json_printer_arr(json, printer);
     break;
   case JSON_TAG_OBJ:
-    json_print_obj(json, printer);
+    json_printer_obj(json, printer);
     break;
   default:
     break;
   }
 }
-
 void json_print(struct json *json, struct printer *printer) {
-  json_print_rec(json, printer);
+  json_printer_print(json, printer);
   printer_newline(printer);
 }
--- src/json/printer.h
+++ src/json/printer.h
@@ -4,6 +4,7 @@
 struct json;
 struct printer;
 
+void json_printer_print(struct json *, struct printer *);
 void json_print(struct json *, struct printer *);
 
 #endif /* INCLUDE_GUARD_D0DB261A_1139_4CC7_A9C5_00F0E9573E21 */
