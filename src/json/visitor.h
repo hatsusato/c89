1c63d20f12cada31286ec9ce3e640cca893ed780
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-30 21:18:48 +0900

    Add struct json_visitor

---
+++ src/json/visitor.h
@@ -0,0 +1,17 @@
+#ifndef INCLUDE_GUARD_BAC01668_22C9_47DA_ACA9_033FC7005651
+#define INCLUDE_GUARD_BAC01668_22C9_47DA_ACA9_033FC7005651
+
+struct json;
+struct json_factory;
+struct json_visitor;
+
+typedef struct json *(*json_callback_t)(struct json_visitor *, struct json *);
+
+struct json_visitor {
+  json_callback_t callback;
+  const char *key;
+  struct json *extra;
+  struct json_factory *factory;
+};
+
+#endif /* INCLUDE_GUARD_BAC01668_22C9_47DA_ACA9_033FC7005651 */
