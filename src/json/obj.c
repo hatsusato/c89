6f938b0fbc4e40c9027209938cab4e50f8636498
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-31 13:32:00 +0900

    Add json_obj_{insert,get,has}

--- src/json/obj.c
+++ src/json/obj.c
@@ -1,5 +1,6 @@
 #include "obj.h"
 
+#include "json.h"
 #include "util/util.h"
 #include "vec.h"
 
@@ -15,4 +16,20 @@ struct json_obj *json_obj_new(void) {
 void json_obj_delete(struct json_obj *self) {
   json_vec_delete(self->vec);
   util_free(self);
+}
+void json_obj_insert(struct json_obj *self, const char *key, struct json *val) {
+  struct json_pair *pair = json_vec_find(self->vec, key);
+  if (pair) {
+    pair->val = val;
+  } else {
+    json_vec_push(self->vec, key, val);
+  }
+}
+struct json *json_obj_get(struct json_obj *self, const char *key) {
+  struct json_pair *pair = json_vec_find(self->vec, key);
+  return pair ? pair->val : json_null();
+}
+bool_t json_obj_has(struct json_obj *self, const char *key) {
+  struct json_pair *pair = json_vec_find(self->vec, key);
+  return pair ? true : false;
 }
--- src/json/obj.h
+++ src/json/obj.h
@@ -1,10 +1,15 @@
 #ifndef INCLUDE_GUARD_ABC752EE_C142_4BE7_8DB6_61F2C2E61D10
 #define INCLUDE_GUARD_ABC752EE_C142_4BE7_8DB6_61F2C2E61D10
 
+#include "util/type.h"
+
 struct json;
 struct json_obj;
 
 struct json_obj *json_obj_new(void);
 void json_obj_delete(struct json_obj *);
+void json_obj_insert(struct json_obj *, const char *, struct json *);
+struct json *json_obj_get(struct json_obj *, const char *);
+bool_t json_obj_has(struct json_obj *, const char *);
 
 #endif /* INCLUDE_GUARD_ABC752EE_C142_4BE7_8DB6_61F2C2E61D10 */
