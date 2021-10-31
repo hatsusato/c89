2813bcffa18e88cedd745c63bc0f0a3ca3791fc7
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-31 13:09:19 +0900

    Add json_obj_{new,delete}

--- src/json/obj.c
+++ src/json/obj.c
@@ -1,7 +1,18 @@
 #include "obj.h"
 
-struct json_vec;
+#include "util/util.h"
+#include "vec.h"
 
 struct json_obj {
   struct json_vec *vec;
 };
+
+struct json_obj *json_obj_new(void) {
+  struct json_obj *self = util_malloc(sizeof(struct json_obj));
+  self->vec = json_vec_new();
+  return self;
+}
+void json_obj_delete(struct json_obj *self) {
+  json_vec_delete(self->vec);
+  util_free(self);
+}
