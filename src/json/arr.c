a7464364e767ed6e0517309e8635a917e0bd4861
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-31 12:33:22 +0900

    Add json_arr_{new,delete}

--- src/json/arr.c
+++ src/json/arr.c
@@ -1,7 +1,18 @@
 #include "arr.h"
 
-struct json_vec;
+#include "util/util.h"
+#include "vec.h"
 
 struct json_arr {
   struct json_vec *vec;
 };
+
+struct json_arr *json_arr_new(void) {
+  struct json_arr *self = util_malloc(sizeof(struct json_arr));
+  self->vec = json_vec_new();
+  return self;
+}
+void json_arr_delete(struct json_arr *self) {
+  json_vec_delete(self->vec);
+  util_free(self);
+}
--- src/json/arr.h
+++ src/json/arr.h
@@ -3,4 +3,7 @@
 
 struct json_arr;
 
+struct json_arr *json_arr_new(void);
+void json_arr_delete(struct json_arr *);
+
 #endif /* INCLUDE_GUARD_5A572308_20F6_491E_A3CD_83B5A0D2B526 */
