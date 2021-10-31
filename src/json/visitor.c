b8380323649c95c177a3afa5fb12081ba394d17b
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-01 05:06:00 +0900

    Support conversion in json_visitor_visit

--- src/json/visitor.c
+++ src/json/visitor.c
@@ -5,11 +5,22 @@
 #include "type.h"
 #include "util/util.h"
 
+struct json_visitor_extra {
+  struct json_visitor *visitor;
+  struct json_obj *parent;
+};
+
 static void json_visitor_visit_obj(struct json *json,
                                    struct json_map_extra *extra) {
-  struct json_visitor *visitor = extra->extra;
+  struct json_visitor_extra *tmp = extra->extra;
+  struct json_visitor *visitor = tmp->visitor;
+  struct json_obj *parent = tmp->parent;
   if (util_streq(visitor->key, extra->key)) {
-    visitor->visitor(visitor, json);
+    visitor->result = NULL;
+    visitor->visitor(visitor, val);
+    if (visitor->result) {
+      json_obj_insert(parent, extra->key, visitor->result);
+    }
   }
   json_visitor_visit(visitor, json);
 }
@@ -20,11 +31,14 @@ static void json_visitor_visit_arr(struct json *json,
 }
 
 void json_visitor_visit(struct json_visitor *self, struct json *json) {
+  struct json_visitor_extra extra;
   struct json_map map;
   switch (json->tag) {
   case JSON_TAG_OBJ:
+    extra.visitor = self;
+    extra.parent = json_as_obj(json);
     map.map = json_visitor_visit_obj;
-    map.extra = self;
+    map.extra = &extra;
     json_obj_foreach(json_as_obj(json), &map);
     break;
   case JSON_TAG_ARR:
