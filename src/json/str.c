b58986e891c4b41276e31ddd4cf9ad9e0e378897
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-10-31 10:59:42 +0900

    Add json_str_{new,delete,get,set}

--- src/json/str.c
+++ src/json/str.c
@@ -1,5 +1,22 @@
 #include "str.h"
 
+#include "util/util.h"
+
 struct json_str {
   const char *str;
 };
+
+struct json_str *json_str_new(const char *str) {
+  struct json_str *self = util_malloc(sizeof(struct json_str));
+  self->str = str;
+  return self;
+}
+void json_str_delete(struct json_str *self) {
+  util_free(self);
+}
+const char *json_str_get(struct json_str *self) {
+  return self->str;
+}
+void json_str_set(struct json_str *self, const char *str) {
+  self->str = str;
+}
--- src/json/str.h
+++ src/json/str.h
@@ -3,4 +3,9 @@
 
 struct json_str;
 
+struct json_str *json_str_new(const char *);
+void json_str_delete(struct json_str *);
+const char *json_str_get(struct json_str *);
+void json_str_set(struct json_str *, const char *);
+
 #endif /* INCLUDE_GUARD_20801795_2D19_46F1_86E4_7C2726AB7AEF */
