8c884b079ade9b275af67ce0fd98bd2315f808e2
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-02 02:26:14 +0900

    Modify type of struct json_factory.pool

--- src/json/factory.c
+++ src/json/factory.c
@@ -6,7 +6,8 @@
 #include "vec.h"
 
 struct json_factory {
-  struct json_vec *pool, *symbol;
+  struct json_vec *symbol;
+  struct json_arr *pool;
 };
 
 static void json_factory_free_pool(const char *key, struct json *val,
@@ -24,17 +25,17 @@ static void json_factory_free_symbol(const char *key, struct json *val,
 
 struct json_factory *json_factory_new(void) {
   struct json_factory *self = util_malloc(sizeof(struct json_factory));
-  self->pool = json_vec_new();
   self->symbol = json_vec_new();
+  self->pool = json_arr_new();
   return self;
 }
 void json_factory_delete(struct json_factory *self) {
   struct json_map map_pool = {json_factory_free_pool, NULL};
   struct json_map map_symbol = {json_factory_free_symbol, NULL};
+  json_arr_foreach(self->pool, &map_pool);
+  json_arr_delete(self->pool);
   json_vec_foreach(self->symbol, &map_symbol);
   json_vec_delete(self->symbol);
-  json_vec_foreach(self->pool, &map_pool);
-  json_vec_delete(self->pool);
   util_free(self);
 }
 const char *json_factory_symbol(struct json_factory *self, const char *symbol) {
@@ -50,21 +51,21 @@ const char *json_factory_symbol(struct json_factory *self, const char *symbol) {
 }
 struct json *json_factory_int(struct json_factory *self, int num) {
   struct json *json = json_new_int(num);
-  json_vec_push(self->pool, NULL, json);
+  json_arr_push(self->pool, json);
   return json;
 }
 struct json *json_factory_str(struct json_factory *self, const char *str) {
   struct json *json = json_new_str(str);
-  json_vec_push(self->pool, NULL, json);
+  json_arr_push(self->pool, json);
   return json;
 }
 struct json *json_factory_arr(struct json_factory *self) {
   struct json *json = json_new_arr();
-  json_vec_push(self->pool, NULL, json);
+  json_arr_push(self->pool, json);
   return json;
 }
 struct json *json_factory_obj(struct json_factory *self) {
   struct json *json = json_new_obj();
-  json_vec_push(self->pool, NULL, json);
+  json_arr_push(self->pool, json);
   return json;
 }
