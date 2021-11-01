49983587b9a716512ae575d2155b8d5ba1166806
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-01 13:31:28 +0900

    Remove json_factory_alloc

--- src/json/factory.c
+++ src/json/factory.c
@@ -30,15 +30,6 @@ static void json_factory_free(const char *key, struct json *val, void *extra) {
   UTIL_UNUSED(key);
   UTIL_UNUSED(extra);
 }
-static struct json *json_factory_alloc(struct json_factory *self,
-                                       enum json_tag tag) {
-  struct json *json = util_malloc(sizeof(struct json));
-  json->tag = tag;
-  json->str = NULL;
-  json->arr = NULL;
-  json_vec_push(self->pool, NULL, json);
-  return json;
-}
 
 struct json_factory *json_factory_new(void) {
   struct json_factory *self = util_malloc(sizeof(struct json_factory));
@@ -52,18 +43,18 @@ void json_factory_delete(struct json_factory *self) {
   util_free(self);
 }
 struct json *json_factory_str(struct json_factory *self, const char *str) {
-  struct json *json = json_factory_alloc(self, JSON_TAG_STR);
+  struct json *json = json_new_str(str);
   assert(str);
-  json->str = json_str_new(str);
+  json_vec_push(self->pool, NULL, json);
   return json;
 }
 struct json *json_factory_arr(struct json_factory *self) {
-  struct json *json = json_factory_alloc(self, JSON_TAG_ARR);
-  json->arr = json_arr_new();
+  struct json *json = json_new_arr();
+  json_vec_push(self->pool, NULL, json);
   return json;
 }
 struct json *json_factory_obj(struct json_factory *self) {
-  struct json *json = json_factory_alloc(self, JSON_TAG_OBJ);
-  json->obj = json_obj_new();
+  struct json *json = json_new_obj();
+  json_vec_push(self->pool, NULL, json);
   return json;
 }
