c7c128fc5e843df94a624b9b97f7d155f64d1db4
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-01 13:14:12 +0900

    Move enum json_tag

--- src/json/type.h
+++ src/json/type.h
@@ -1,14 +1,13 @@
 #ifndef INCLUDE_GUARD_B6B56A7D_817F_4841_A842_6C0985520EBD
 #define INCLUDE_GUARD_B6B56A7D_817F_4841_A842_6C0985520EBD
 
+#include "tag.h"
 #include "util/type.h"
 
 struct json_arr;
 struct json_obj;
 struct json_str;
 struct json_vec;
-
-enum json_tag { JSON_TAG_NULL, JSON_TAG_STR, JSON_TAG_ARR, JSON_TAG_OBJ };
 
 struct json {
   enum json_tag tag;
