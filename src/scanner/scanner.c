13e74444a0140965c89953b68412d0338d84f846
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-02 03:19:04 +0900

    Update scanner_json_{push,set}

--- src/scanner/scanner.c
+++ src/scanner/scanner.c
@@ -38,16 +38,18 @@ YYSCAN_TYPE scanner_json_obj(YYSCAN_EXTRA self) {
   return json_factory_obj(self->factory);
 }
 YYSCAN_TYPE scanner_json_push(YYSCAN_TYPE json, YYSCAN_TYPE val) {
-  struct json_arr *arr = json_as_arr(json);
-  assert(arr);
-  json_arr_push(arr, val);
+  if (json_is_arr(json)) {
+    struct json_arr *arr = json_as_arr(json);
+    json_arr_push(arr, val);
+  }
   return json;
 }
 YYSCAN_TYPE scanner_json_set(YYSCAN_TYPE json, const char *key,
                              YYSCAN_TYPE val) {
-  struct json_obj *obj = json_as_obj(json);
-  assert(obj);
-  json_obj_insert(obj, key, val);
+  if (json_is_obj(json)) {
+    struct json_obj *obj = json_as_obj(json);
+    json_obj_insert(obj, key, val);
+  }
   return json;
 }
 YYSCAN_TYPE scanner_json_binop(YYSCAN_EXTRA self, YYSCAN_TYPE lhs,
