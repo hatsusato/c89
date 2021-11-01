224b6e11dedce68b9f256d4c376f8618f5f8304f
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-02 01:57:34 +0900

    Add json_int_{new,delete}

--- src/json/int.c
+++ src/json/int.c
@@ -1,5 +1,16 @@
 #include "int.h"
 
+#include "util/util.h"
+
 struct json_int {
   int num;
 };
+
+struct json_int *json_int_new(int num) {
+  struct json_int *self = util_malloc(sizeof(struct json_int));
+  self->num = num;
+  return self;
+}
+void json_int_delete(struct json_int *self) {
+  util_free(self);
+}
--- src/json/int.h
+++ src/json/int.h
@@ -1,3 +1,9 @@
 #ifndef INCLUDE_GUARD_D056FAB2_6588_4A0F_9DF2_D1FDBC097051
 #define INCLUDE_GUARD_D056FAB2_6588_4A0F_9DF2_D1FDBC097051
+
+struct json_int;
+
+struct json_int *json_int_new(int);
+void json_int_delete(struct json_int *);
+
 #endif /* INCLUDE_GUARD_D056FAB2_6588_4A0F_9DF2_D1FDBC097051 */
