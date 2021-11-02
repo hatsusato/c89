6cf02867d85bc7aac0980c39972c4036fb30230e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-02 13:54:58 +0900

    Add json_callback_{new,delete}

--- src/json/callback.c
+++ src/json/callback.c
@@ -1,6 +1,20 @@
 #include "callback.h"
 
+#include "obj.h"
+#include "util/util.h"
+
 struct json_callback {
   json_callback_t callback;
   struct json_obj *args;
 };
+
+struct json_callback *json_callback_new(json_callback_t callback) {
+  struct json_callback *self = util_malloc(sizeof(struct json_callback));
+  self->callback = callback;
+  self->args = json_obj_new();
+  return self;
+}
+void json_callback_delete(struct json_callback *self) {
+  json_obj_delete(self->args);
+  util_free(self);
+}
--- src/json/callback.h
+++ src/json/callback.h
@@ -1,8 +1,12 @@
 #ifndef INCLUDE_GUARD_15A786EB_987B_4284_9538_D41F3B730C9C
 #define INCLUDE_GUARD_15A786EB_987B_4284_9538_D41F3B730C9C
 
+struct json_callback;
 struct json_obj;
 
 typedef void (*json_callback_t)(struct json_obj *);
+
+struct json_callback *json_callback_new(json_callback_t);
+void json_callback_delete(struct json_callback *);
 
 #endif /* INCLUDE_GUARD_15A786EB_987B_4284_9538_D41F3B730C9C */
