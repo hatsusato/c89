a58e6b87d829b8b40feb07b9a68c78113e2c1ef0
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-02 11:50:51 +0900

    Add json_any_{new,delete}

--- src/json/any.c
+++ src/json/any.c
@@ -1,5 +1,16 @@
 #include "any.h"
 
+#include "util/util.h"
+
 struct json_any {
   void *any;
 };
+
+struct json_any *json_any_new(void *any) {
+  struct json_any *self = util_malloc(sizeof(struct json_any));
+  self->any = any;
+  return self;
+}
+void json_any_delete(struct json_any *self) {
+  util_free(self);
+}
--- src/json/any.h
+++ src/json/any.h
@@ -1,3 +1,9 @@
 #ifndef INCLUDE_GUARD_77726E87_251A_41CB_9277_CB6E6EBFAA3A
 #define INCLUDE_GUARD_77726E87_251A_41CB_9277_CB6E6EBFAA3A
+
+struct json_any;
+
+struct json_any *json_any_new(void *);
+void json_any_delete(struct json_any *);
+
 #endif /* INCLUDE_GUARD_77726E87_251A_41CB_9277_CB6E6EBFAA3A */
