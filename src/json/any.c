df81614fe731cba96e4ae71813cff4d7a262c21c
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-02 11:57:24 +0900

    Add json_any_print

--- src/json/any.c
+++ src/json/any.c
@@ -1,5 +1,6 @@
 #include "any.h"
 
+#include "printer.h"
 #include "util/util.h"
 
 struct json_any {
@@ -19,4 +20,8 @@ void *json_any_get(struct json_any *self) {
 }
 void json_any_set(struct json_any *self, void *any) {
   self->any = any;
+}
+void json_any_print(struct json_any *self, struct json_printer *printer) {
+  json_printer_str(printer, "<any>");
+  UTIL_UNUSED(self);
 }
--- src/json/any.h
+++ src/json/any.h
@@ -2,10 +2,12 @@
 #define INCLUDE_GUARD_77726E87_251A_41CB_9277_CB6E6EBFAA3A
 
 struct json_any;
+struct json_printer;
 
 struct json_any *json_any_new(void *);
 void json_any_delete(struct json_any *);
 void *json_any_get(struct json_any *);
 void json_any_set(struct json_any *, void *);
+void json_any_print(struct json_any *, struct json_printer *);
 
 #endif /* INCLUDE_GUARD_77726E87_251A_41CB_9277_CB6E6EBFAA3A */
