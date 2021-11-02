b77c85303a476b1621a94d353e3e3e449fa34c80
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-02 11:53:37 +0900

    Add json_any_{get,set}

--- src/json/any.c
+++ src/json/any.c
@@ -14,3 +14,9 @@ struct json_any *json_any_new(void *any) {
 void json_any_delete(struct json_any *self) {
   util_free(self);
 }
+void *json_any_get(struct json_any *self) {
+  return self->any;
+}
+void json_any_set(struct json_any *self, void *any) {
+  self->any = any;
+}
--- src/json/any.h
+++ src/json/any.h
@@ -5,5 +5,7 @@ struct json_any;
 
 struct json_any *json_any_new(void *);
 void json_any_delete(struct json_any *);
+void *json_any_get(struct json_any *);
+void json_any_set(struct json_any *, void *);
 
 #endif /* INCLUDE_GUARD_77726E87_251A_41CB_9277_CB6E6EBFAA3A */
