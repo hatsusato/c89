2766193db3220b36fd97f95dc57ff2ae94e71016
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-03 18:02:08 +0900

    Remove dict subdirectory

--- src/dict/dict.h
+++
@@ -1,9 +0,0 @@
-#ifndef INCLUDE_GUARD_8FD4DA21_88F9_44C7_9BDC_CEE87486F9A0
-#define INCLUDE_GUARD_8FD4DA21_88F9_44C7_9BDC_CEE87486F9A0
-
-struct dict *dict_new(void);
-void dict_delete(struct dict *);
-void dict_insert(struct dict *, const char *, void *);
-void *dict_find(struct dict *, const char *);
-
-#endif /* INCLUDE_GUARD_8FD4DA21_88F9_44C7_9BDC_CEE87486F9A0 */
--- src/unittest.c
+++ src/unittest.c
@@ -1,14 +1,12 @@
 #include "unittest.h"
 
 #include "array/unittest.h"
-#include "dict/unittest.h"
 #include "json/unittest.h"
 #include "set/unittest.h"
 #include "vec/unittest.h"
 
 void unittest(void) {
   array_unittest();
-  dict_unittest();
   json_unittest();
   set_unittest();
   vec_unittest();
