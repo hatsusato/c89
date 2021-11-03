79184d5f02cfd4cc8dcaffcaad39352d7d604d84
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-03 18:03:33 +0900

    Remove set subdirectory

--- src/unittest.c
+++ src/unittest.c
@@ -2,12 +2,10 @@
 
 #include "array/unittest.h"
 #include "json/unittest.h"
-#include "set/unittest.h"
 #include "vec/unittest.h"
 
 void unittest(void) {
   array_unittest();
   json_unittest();
-  set_unittest();
   vec_unittest();
 }
