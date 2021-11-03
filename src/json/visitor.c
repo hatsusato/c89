7b321e855641cd9789dc4c38bf6210f21bb08d0b
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-03 17:14:16 +0900

    Remove struct json_visitor

--- src/json/visitor.c
+++ src/json/visitor.c
@@ -4,45 +4,6 @@
 #include "map.h"
 #include "util/util.h"
 
-struct json_visitor {
-  json_visitor_t visitor;
-  const char *key;
-  struct json *result;
-  struct json *parent;
-  void *extra;
-};
-
-static void json_visitor_recurse(struct json_visitor *, struct json *);
-static void json_visitor_map(struct json_map *map) {
-  struct json_visitor *visitor = map->extra;
-  if (map->is_obj && util_streq(visitor->key, map->key)) {
-    visitor->result = NULL;
-    visitor->visitor(map->val, visitor->extra);
-    if (visitor->result) {
-      json_obj_insert(json_as_obj(visitor->parent), map->key, visitor->result);
-    }
-  }
-  json_visitor_recurse(visitor, map->val);
-}
-static void json_visitor_recurse(struct json_visitor *self, struct json *json) {
-  struct json_map map;
-  map.map = json_visitor_map;
-  map.extra = self;
-  if (json_is_obj(json)) {
-    self->parent = json;
-    json_obj_foreach(json_as_obj(json), &map);
-  } else if (json_is_arr(json)) {
-    json_arr_foreach(json_as_arr(json), &map);
-  }
-}
-void json_visitor_visit(json_visitor_t visitor, const char *key,
-                        struct json *json, void *extra) {
-  struct json_visitor self = {NULL, NULL, NULL, NULL, NULL};
-  self.visitor = visitor;
-  self.key = key;
-  self.extra = extra;
-  json_visitor_recurse(&self, json);
-}
 static void json_visitor2_map(struct json_map *map) {
   json_visitor2_visit(map->extra, map->val);
 }
--- src/json/visitor.h
+++ src/json/visitor.h
@@ -4,7 +4,6 @@
 struct json;
 struct json_visitor2;
 
-typedef void (*json_visitor_t)(struct json *, void *);
 typedef void (*json_visitor2_t)(struct json_visitor2 *, struct json *);
 
 struct json_visitor2 {
@@ -12,7 +11,6 @@ struct json_visitor2 {
   void *extra;
 };
 
-void json_visitor_visit(json_visitor_t, const char *, struct json *, void *);
 void json_visitor2_visit(struct json_visitor2 *, struct json *);
 
 #endif /* INCLUDE_GUARD_BAC01668_22C9_47DA_ACA9_033FC7005651 */
