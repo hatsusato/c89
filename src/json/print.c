ae4a4c5f33ed6670dfb9539923a247c71a316055
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-03 14:50:37 +0900

    Replace json_print_{arr,obj}_map with json_print_map

--- src/json/print.c
+++ src/json/print.c
@@ -2,6 +2,7 @@
 
 #include "closure.h"
 #include "json.h"
+#include "map.h"
 #include "printer/printer.h"
 #include "type.h"
 #include "util/util.h"
@@ -17,69 +18,39 @@ static void json_print_int(struct printer *self, struct json_int *num) {
 static void json_print_str(struct printer *self, struct json_str *str) {
   printer_print(self, "\"%s\"", json_str_get(str));
 }
-static void json_print_arr_map(struct json_closure *closure) {
-  struct json *args = json_closure_args(closure);
-  struct json *printer = json_get(args, "printer");
-  struct json *index = json_get(args, "index");
-  struct json *val = json_get(args, "val");
-  struct printer *self = json_get_any(printer);
-  assert(json_is_int(index));
-  if (0 < json_int_get(json_as_int(index))) {
+static void json_print_map(struct json_map *map) {
+  struct printer *self = map->extra;
+  if (0 < map->index) {
     printer_print(self, ",");
   }
   printer_newline(self);
-  json_print_recurse(self, val);
+  if (map->is_obj) {
+    printer_print(self, "\"%s\": ", map->key);
+  }
+  json_print_recurse(self, map->val);
 }
 static void json_print_arr(struct printer *self, struct json_arr *arr) {
   printer_print(self, "[");
   printer_indent(self, 2);
   if (0 < json_arr_count(arr)) {
-    struct json_closure *map = json_closure_new(json_print_arr_map);
-    struct json *printer = json_new_any(self);
-    struct json *index = json_new_int(0);
-    json_closure_insert(map, "printer", printer);
-    json_closure_insert(map, "index", index);
-    json_arr_map(arr, map);
+    struct json_map map;
+    map.map = json_print_map;
+    map.extra = self;
+    json_arr_foreach(arr, &map);
     printer_newline(self);
-    json_delete(index);
-    json_delete(printer);
-    json_closure_delete(map);
   }
   printer_indent(self, -2);
   printer_print(self, "]");
-}
-static void json_print_obj_map(struct json_closure *closure) {
-  struct json *args = json_closure_args(closure);
-  struct json *printer = json_get(args, "printer");
-  struct json *index = json_get(args, "index");
-  struct json *key = json_get(args, "key");
-  struct json *val = json_get(args, "val");
-  struct printer *self = json_get_any(printer);
-  assert(json_is_int(index));
-  if (0 < json_int_get(json_as_int(index))) {
-    printer_print(self, ",");
-  }
-  printer_newline(self);
-  printer_print(self, "\"%s\": ", json_get_str(key));
-  json_print_recurse(self, val);
 }
 static void json_print_obj(struct printer *self, struct json_obj *obj) {
   printer_print(self, "{");
   printer_indent(self, 2);
   if (0 < json_obj_count(obj)) {
-    struct json_closure *map = json_closure_new(json_print_obj_map);
-    struct json *printer = json_new_any(self);
-    struct json *index = json_new_int(0);
-    struct json *key = json_new_str("");
-    json_closure_insert(map, "printer", printer);
-    json_closure_insert(map, "index", index);
-    json_closure_insert(map, "key", key);
-    json_obj_map(obj, map);
+    struct json_map map;
+    map.map = json_print_map;
+    map.extra = self;
+    json_obj_foreach(obj, &map);
     printer_newline(self);
-    json_delete(key);
-    json_delete(index);
-    json_delete(printer);
-    json_closure_delete(map);
   }
   printer_indent(self, -2);
   printer_print(self, "}");
