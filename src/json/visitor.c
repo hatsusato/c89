bb9848955922077663730344292fe3cf691fa302
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-03 14:45:35 +0900

    Replace json_visitor_visit_{obj,arr} with json_visitor_map

--- src/json/visitor.c
+++ src/json/visitor.c
@@ -8,37 +8,30 @@ struct json_visitor {
   json_visitor_t visitor;
   const char *key;
   struct json *result;
-  struct json_obj *parent;
+  struct json *parent;
   void *extra;
 };
 
 static void json_visitor_recurse(struct json_visitor *, struct json *);
-static void json_visitor_visit_obj(struct json_map *map) {
+static void json_visitor_map(struct json_map *map) {
   struct json_visitor *visitor = map->extra;
-  if (util_streq(visitor->key, map->key)) {
+  if (map->is_obj && util_streq(visitor->key, map->key)) {
     visitor->result = NULL;
     visitor->visitor(map->val, visitor->extra);
     if (visitor->result) {
-      json_obj_insert(visitor->parent, map->key, visitor->result);
+      json_obj_insert(json_as_obj(visitor->parent), map->key, visitor->result);
     }
   }
   json_visitor_recurse(visitor, map->val);
 }
-static void json_visitor_visit_arr(struct json_map *map) {
-  json_visitor_recurse(map->extra, map->val);
-}
 static void json_visitor_recurse(struct json_visitor *self, struct json *json) {
+  struct json_map map;
+  map.map = json_visitor_map;
+  map.extra = self;
   if (json_is_obj(json)) {
-    struct json_obj *obj = json_as_obj(json);
-    struct json_map map;
-    map.map = json_visitor_visit_obj;
-    map.extra = self;
-    self->parent = obj;
-    json_obj_foreach(obj, &map);
+    self->parent = json;
+    json_obj_foreach(json_as_obj(json), &map);
   } else if (json_is_arr(json)) {
-    struct json_map map;
-    map.map = json_visitor_visit_arr;
-    map.extra = self;
     json_arr_foreach(json_as_arr(json), &map);
   }
 }
