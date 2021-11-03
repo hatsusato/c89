dc2a63b8a0f62e055c122fae40e294ae131363c4
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-03 20:01:35 +0900

    Add compile

--- src/main.c
+++ src/main.c
@@ -13,19 +13,22 @@ bool_t is_unittest(int argc, char *argv[]) {
   }
   return false;
 }
+void compile(void) {
+  struct json_factory *factory = json_factory_new();
+  struct json *json = scanner_parse(factory);
+  if (json_is_null(json)) {
+    util_error("ERROR: failed to parse");
+  } else {
+    json_print(json);
+  }
+  json_factory_delete(factory);
+}
 
 int main(int argc, char *argv[]) {
   if (is_unittest(argc, argv)) {
     unittest();
   } else {
-    struct json_factory *factory = json_factory_new();
-    struct json *json = scanner_parse(factory);
-    if (json_is_null(json)) {
-      util_error("ERROR: failed to parse");
-    } else {
-      json_print(json);
-    }
-    json_factory_delete(factory);
+    compile();
   }
   return 0;
 }
