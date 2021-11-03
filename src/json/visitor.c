c9501de54c0d6d8fd4f3e5a14f5dc3e3632d1176
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-03 16:19:18 +0900

    Add struct json_visitor2

--- src/json/visitor.c
+++ src/json/visitor.c
@@ -43,3 +43,17 @@ void json_visitor_visit(json_visitor_t visitor, const char *key,
   self.extra = extra;
   json_visitor_recurse(&self, json);
 }
+static void json_visitor2_map(struct json_map *map) {
+  json_visitor2_visit(map->extra, map->val);
+}
+void json_visitor2_visit(struct json_visitor2 *self, struct json *json) {
+  struct json_map map;
+  map.map = json_visitor2_map;
+  map.extra = self;
+  self->visitor(self, json);
+  if (json_is_obj(json)) {
+    json_obj_foreach(json_as_obj(json), &map);
+  } else if (json_is_arr(json)) {
+    json_arr_foreach(json_as_arr(json), &map);
+  }
+}
--- src/json/visitor.h
+++ src/json/visitor.h
@@ -2,9 +2,17 @@
 #define INCLUDE_GUARD_BAC01668_22C9_47DA_ACA9_033FC7005651
 
 struct json;
+struct json_visitor2;
 
 typedef void (*json_visitor_t)(struct json *, void *);
+typedef void (*json_visitor2_t)(struct json_visitor2 *, struct json *);
+
+struct json_visitor2 {
+  json_visitor2_t visitor;
+  void *extra;
+};
 
 void json_visitor_visit(json_visitor_t, const char *, struct json *, void *);
+void json_visitor2_visit(struct json_visitor2 *, struct json *);
 
 #endif /* INCLUDE_GUARD_BAC01668_22C9_47DA_ACA9_033FC7005651 */
