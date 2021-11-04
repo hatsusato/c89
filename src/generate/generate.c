8e5bb6d97faa1aef9846081ea83d1d7b0eb58075
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-04 14:17:15 +0900

    Add generate_header

--- src/generate/generate.c
+++ src/generate/generate.c
@@ -5,6 +5,11 @@
 #include "printer/printer.h"
 #include "util/util.h"
 
+static void generate_header(struct printer *printer) {
+  printer_print(printer, "target triple = ");
+  printer_quote(printer, "x86_64-unknown-linux-gnu");
+  printer_newline(printer);
+}
 static void generate_function(struct json_map *map) {
   struct printer *printer = map->extra;
   struct json *name = json_get(map->val, "name");
@@ -22,9 +27,10 @@ void generate(struct json *json) {
   struct printer *printer = printer_new_stdout();
   struct json *module = json_get(json, "module");
   struct json_map map;
-  assert(json_is_arr(module));
+  generate_header(printer);
   map.map = generate_function;
   map.extra = printer;
+  assert(json_is_arr(module));
   json_arr_foreach(json_as_arr(module), &map);
   printer_del(printer);
 }
