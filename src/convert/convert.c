a56b4bc11028ddbc9f93cb6ff6cbd097ccf7c1a5
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-04 13:21:32 +0900

    Add convert_get_identifier

--- src/convert/convert.c
+++ src/convert/convert.c
@@ -1,11 +1,22 @@
 #include "convert.h"
 
+#include "json/json.h"
+#include "scanner/symbol.h"
 #include "util/util.h"
 
 struct convert {
   struct json_factory *factory;
 };
 
+struct json *convert_get_identifier(struct json *json) {
+  while (!json_is_null(json)) {
+    json = json_get(json, SYMBOL_DIRECT_DECLARATOR);
+    if (json_has(json, SYMBOL_IDENTIFIER)) {
+      return json_get(json, SYMBOL_IDENTIFIER);
+    }
+  }
+  return json;
+}
 void convert(struct json_factory *factory, struct json *json) {
   UTIL_UNUSED(factory);
   UTIL_UNUSED(json);
--- src/convert/convert.h
+++ src/convert/convert.h
@@ -4,6 +4,7 @@
 struct json;
 struct json_factory;
 
+struct json *convert_get_identifier(struct json *json);
 void convert(struct json_factory *, struct json *);
 
 #endif /* INCLUDE_GUARD_1F1337DE_895F_45EE_959F_A1D190BEC347 */
