787faf670acfc262c54ae72244660a1171d39b3e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-06 21:33:25 +0900

    Add json_set_{new,del}

--- src/json/set.c
+++ src/json/set.c
@@ -1,7 +1,18 @@
 #include "set.h"
 
-struct json_obj;
+#include "json.h"
+#include "util/util.h"
 
 struct json_set {
   struct json_obj *set;
 };
+
+struct json_set *json_set_new(void) {
+  struct json_set *self = util_malloc(sizeof(struct json_set));
+  self->set = json_obj_new();
+  return self;
+}
+void json_set_del(struct json_set *self) {
+  json_obj_del(self->set);
+  util_free(self);
+}
--- src/json/set.h
+++ src/json/set.h
@@ -3,4 +3,7 @@
 
 struct json_set;
 
+struct json_set *json_set_new(void);
+void json_set_del(struct json_set *);
+
 #endif /* INCLUDE_GUARD_0A2DDE84_20A0_4C2B_92E9_4482494817FF */
