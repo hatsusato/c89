a0f7aa75da09cfdae24cca6747cf705f905e258e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-06 14:01:22 +0900

    Remove struct json_get_extra.found

--- src/json/util.c
+++ src/json/util.c
@@ -3,11 +3,11 @@
 #include "json.h"
 #include "print.h"
 #include "tag.h"
+#include "util/util.h"
 #include "visitor.h"
 
 struct json_get_extra {
   const char *key;
-  bool_t found;
   struct json *result;
 };
 
@@ -24,19 +24,16 @@ bool_t json_has(struct json *self, const char *key) {
 }
 static void json_get_visitor(struct json_visitor *visitor, struct json *json) {
   struct json_get_extra *extra = json_visit_extra(visitor);
-  if (!extra->found && json_is_obj(json)) {
-    struct json_obj *obj = json_as_obj(json);
-    if (json_obj_has(obj, extra->key)) {
-      extra->result = json_obj_get(obj, extra->key);
-      extra->found = true;
-    }
+  if (json_has(json, extra->key)) {
+    assert(json_is_obj(json));
+    extra->result = json_obj_get(json_as_obj(json), extra->key);
+    json_visit_finish(visitor);
   }
   json_visit_foreach(visitor, json);
 }
 struct json *json_get(struct json *self, const char *key) {
   struct json_get_extra extra;
   extra.key = key;
-  extra.found = false;
   extra.result = json_null();
   json_visit(json_get_visitor, &extra, self);
   return extra.result;
