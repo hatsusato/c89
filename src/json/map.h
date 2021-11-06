2e0fc0ec6d2870be2a7f2bc0c27cc20dc8ff49df
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-06 12:55:21 +0900

    Move json_map_t

--- src/json/map.h
+++ src/json/map.h
@@ -1,12 +1,10 @@
 #ifndef INCLUDE_GUARD_B3786200_8182_4167_B88A_47D1C9AAB284
 #define INCLUDE_GUARD_B3786200_8182_4167_B88A_47D1C9AAB284
 
+#include "map_type.h"
 #include "util/type.h"
 
 struct json;
-struct json_map;
-
-typedef void (*json_map_t)(struct json_map *);
 
 struct json_map {
   json_map_t map;
