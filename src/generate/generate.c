fd5576c1da5cbacbe31290550ff36a347773a5ee
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-07 01:48:10 +0900

    Update generate_function

--- src/generate/generate.c
+++ src/generate/generate.c
@@ -1,10 +1,8 @@
 #include "generate.h"
 
-#include "json/json.h"
 #include "json/map.h"
 #include "json/util.h"
 #include "printer/printer.h"
-#include "util/util.h"
 
 static void generate_header(struct printer *printer) {
   printer_print(printer, "target triple = ");
@@ -13,13 +11,14 @@ static void generate_header(struct printer *printer) {
 }
 static void generate_function(struct json_map *map) {
   struct printer *printer = json_map_extra(map);
-  struct json *name = json_get(json_map_val(map), "name");
-  assert(json_is_str(name));
+  struct json *func = json_map_val(map);
+  struct json *name = json_get(func, "name");
+  struct json *ret = json_get(func, "ret");
   printer_newline(printer);
   printer_print(printer, "define i32 @%s() ", json_get_str(name));
   printer_open(printer, "{");
   printer_newline(printer);
-  printer_print(printer, "ret i32 0");
+  printer_print(printer, "ret i32 %s", json_get_str(ret));
   printer_newline(printer);
   printer_close(printer, "}");
   printer_newline(printer);
