14302ad51a300e35357d57e8ae3a46405d78e129
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-06 13:50:39 +0900

    Add json_visit_finish

--- src/json/visitor.c
+++ src/json/visitor.c
@@ -8,21 +8,31 @@
 struct json_visitor {
   json_visitor_t visitor;
   void *extra;
+  bool_t finished;
 };
 
 void json_visit(json_visitor_t visitor, void *extra, struct json *json) {
   struct json_visitor self;
   self.visitor = visitor;
   self.extra = extra;
+  self.finished = false;
   visitor(&self, json);
 }
 void *json_visit_extra(struct json_visitor *self) {
   return self->extra;
 }
+void json_visit_finish(struct json_visitor *self) {
+  self->finished = true;
+}
 static void json_visit_map(struct json_map *map) {
   struct json_visitor *self = json_map_extra(map);
   self->visitor(self, json_map_val(map));
+  if (self->finished) {
+    json_map_finish(map);
+  }
 }
 void json_visit_foreach(struct json_visitor *self, struct json *json) {
-  json_foreach(json, json_visit_map, self);
+  if (!self->finished) {
+    json_foreach(json, json_visit_map, self);
+  }
 }
--- src/json/visitor.h
+++ src/json/visitor.h
@@ -8,6 +8,7 @@ typedef void (*json_visitor_t)(struct json_visitor *, struct json *);
 
 void json_visit(json_visitor_t, void *, struct json *);
 void *json_visit_extra(struct json_visitor *);
+void json_visit_finish(struct json_visitor *);
 void json_visit_foreach(struct json_visitor *, struct json *);
 
 #endif /* INCLUDE_GUARD_BAC01668_22C9_47DA_ACA9_033FC7005651 */
