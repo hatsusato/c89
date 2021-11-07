91b62974979faeb27fffaa62930531d7e1b0b484
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-08 02:00:25 +0900

    Add convert_declaration
    and convert_init_declarator{,_list}

--- src/convert/declaration.c
+++ src/convert/declaration.c
@@ -1,1 +1,27 @@
 #include "declaration.h"
+
+#include "extra.h"
+#include "json/json.h"
+#include "json/map.h"
+#include "util/symbol.h"
+
+static void convert_init_declarator(struct convert_extra *self,
+                                    struct json *json) {
+  struct json *identifier = json_find_identifier(json);
+  struct json *instr = convert_extra_new_instr("alloca");
+  json_insert(instr, "name", identifier);
+  convert_extra_push_instr(self, instr);
+  json_del(instr);
+}
+static void convert_init_declarator_list(struct json_map *map) {
+  struct convert_extra *self = json_map_extra(map);
+  struct json *json = json_map_val(map);
+  convert_init_declarator(self, json);
+}
+
+void convert_declaration(struct convert_extra *self, struct json *json) {
+  if (json_has(json, SYMBOL_INIT_DECLARATOR_LIST)) {
+    json_foreach(json_get(json, SYMBOL_INIT_DECLARATOR_LIST),
+                 convert_init_declarator_list, self);
+  }
+}
--- src/convert/declaration.h
+++ src/convert/declaration.h
@@ -2,5 +2,8 @@
 #define INCLUDE_GUARD_02C87D77_338D_4B87_A1A3_478C810729AE
 
 struct convert_extra;
+struct json;
+
+void convert_declaration(struct convert_extra *, struct json *);
 
 #endif /* INCLUDE_GUARD_02C87D77_338D_4B87_A1A3_478C810729AE */
