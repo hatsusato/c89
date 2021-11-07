c42f19c71d51879c70b4b8d64db903ee0e252160
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-08 01:02:08 +0900

    Arrange generate_header

--- src/generate/generate.c
+++ src/generate/generate.c
@@ -4,11 +4,6 @@
 #include "json/map.h"
 #include "printer/printer.h"
 
-static void generate_header(struct printer *printer) {
-  printer_print(printer, "target triple = ");
-  printer_quote(printer, "x86_64-unknown-linux-gnu");
-  printer_newline(printer);
-}
 static void generate_function(struct json_map *map) {
   struct printer *printer = json_map_extra(map);
   struct json *func = json_map_val(map);
@@ -23,10 +18,15 @@ static void generate_function(struct json_map *map) {
   printer_close(printer, "}");
   printer_newline(printer);
 }
+static void generate_header(struct printer *printer) {
+  printer_print(printer, "target triple = ");
+  printer_quote(printer, "x86_64-unknown-linux-gnu");
+  printer_newline(printer);
+}
+
 void generate(struct json *json) {
   struct printer *printer = printer_new_stdout();
-  struct json *module = json_get(json, "module");
   generate_header(printer);
-  json_foreach(module, generate_function, printer);
+  json_foreach(json_get(json, "module"), generate_function, printer);
   printer_del(printer);
 }
