83041a9aaab2e4f2f4fff8b0f50df2f093381f8f
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-08 01:09:15 +0900

    Add generate_{instr,block}

--- src/generate/generate.c
+++ src/generate/generate.c
@@ -4,17 +4,28 @@
 #include "json/map.h"
 #include "printer/printer.h"
 
+static void generate_instr(struct json_map *map) {
+  struct printer *printer = json_map_extra(map);
+  struct json *instr = json_map_val(map);
+  struct json *tag = json_get(instr, "instr");
+  struct json *val = json_get(instr, "value");
+  printer_print(printer, "%s i32 %s", json_get_str(tag), json_get_str(val));
+  printer_newline(printer);
+}
+static void generate_block(struct json_map *map) {
+  struct printer *printer = json_map_extra(map);
+  struct json *block = json_map_val(map);
+  json_foreach(json_get(block, "block"), generate_instr, printer);
+}
 static void generate_function(struct json_map *map) {
   struct printer *printer = json_map_extra(map);
   struct json *func = json_map_val(map);
   struct json *name = json_get(func, "name");
-  struct json *ret = json_get(func, "ret");
   printer_newline(printer);
   printer_print(printer, "define i32 @%s() ", json_get_str(name));
   printer_open(printer, "{");
   printer_newline(printer);
-  printer_print(printer, "ret i32 %s", json_get_str(ret));
-  printer_newline(printer);
+  json_foreach(json_get(func, "function"), generate_block, printer);
   printer_close(printer, "}");
   printer_newline(printer);
 }
