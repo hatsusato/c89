9286ad2970874b215811041eaa8d824694ab33cf
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-08 13:20:13 +0900

    Update generate_reg

--- src/generate/instruction.c
+++ src/generate/instruction.c
@@ -4,7 +4,11 @@
 #include "printer/printer.h"
 #include "util/util.h"
 
-static void generate_reg(struct printer *printer, struct json *json) {
+static void generate_register(struct printer *printer, struct json *json,
+                              const char *key) {
+  if (key && json_has(json, key)) {
+    json = json_get(json, key);
+  }
   if (json_has(json, "reg")) {
     struct json *reg = json_get(json, "reg");
     printer_print(printer, "%%%d", json_int_get(json_as_int(reg)));
@@ -15,36 +19,36 @@ static void generate_reg(struct printer *printer, struct json *json) {
 /* Terminator Instructions */
 static void generate_ret(struct printer *printer, struct json *json) {
   printer_print(printer, "ret i32 ");
-  generate_reg(printer, json_get(json, "value"));
+  generate_register(printer, json, "value");
 }
 /* Unary Operations */
 /* Binary Operations */
 static void generate_add(struct printer *printer, struct json *json) {
-  generate_reg(printer, json);
+  generate_register(printer, json, NULL);
   printer_print(printer, " = add nsw i32 ");
-  generate_reg(printer, json_get(json, "lhs"));
+  generate_register(printer, json, "lhs");
   printer_print(printer, ", ");
-  generate_reg(printer, json_get(json, "rhs"));
+  generate_register(printer, json, "rhs");
 }
 /* Bitwise Binary Operations */
 /* Vector Operations */
 /* Aggregate Operations */
 /* Memory Access and Addressing Operations */
 static void generate_alloca(struct printer *printer, struct json *json) {
-  generate_reg(printer, json);
+  generate_register(printer, json, NULL);
   printer_print(printer, " = alloca i32, align 4");
 }
 static void generate_load(struct printer *printer, struct json *json) {
-  generate_reg(printer, json);
+  generate_register(printer, json, NULL);
   printer_print(printer, " = load i32, i32* ");
-  generate_reg(printer, json_get(json, "pointer"));
+  generate_register(printer, json, "pointer");
   printer_print(printer, ", align 4");
 }
 static void generate_store(struct printer *printer, struct json *json) {
   printer_print(printer, "store i32 ");
-  generate_reg(printer, json_get(json, "value"));
+  generate_register(printer, json, "value");
   printer_print(printer, ", i32* ");
-  generate_reg(printer, json_get(json, "pointer"));
+  generate_register(printer, json, "pointer");
   printer_print(printer, ", align 4");
 }
 /* Conversion Operations */
