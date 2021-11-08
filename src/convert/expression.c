37b62a62cf6eb177de944cd08e272b45481b492e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-08 10:48:17 +0900

    Add convert_{identifier,integer_constant}

--- src/convert/expression.c
+++ src/convert/expression.c
@@ -3,7 +3,17 @@
 #include "extra.h"
 #include "json/json.h"
 #include "util/symbol.h"
+#include "util/util.h"
 
+static struct json *convert_identifier(struct convert_extra *self,
+                                       struct json *json) {
+  return convert_extra_lookup_symbol(self, json);
+}
+static struct json *convert_integer_constant(struct convert_extra *self,
+                                             struct json *json) {
+  UTIL_UNUSED(self);
+  return json;
+}
 static struct json *convert_assignment_expression(struct convert_extra *self,
                                                   struct json *json) {
   struct json *value = convert_expression(self, json_get(json, "rhs"));
@@ -17,13 +27,15 @@ static struct json *convert_assignment_expression(struct convert_extra *self,
 }
 
 struct json *convert_expression(struct convert_extra *self, struct json *json) {
-  if (json_has(json, SYMBOL_INTEGER_CONSTANT)) {
-    return json_get(json, SYMBOL_INTEGER_CONSTANT);
+  if (json_has(json, SYMBOL_IDENTIFIER)) {
+    return convert_identifier(self, json_get(json, SYMBOL_IDENTIFIER));
+  } else if (json_has(json, SYMBOL_INTEGER_CONSTANT)) {
+    return convert_integer_constant(self,
+                                    json_get(json, SYMBOL_INTEGER_CONSTANT));
   } else if (json_has(json, SYMBOL_ASSIGNMENT_EXPRESSION)) {
     return convert_assignment_expression(
         self, json_get(json, SYMBOL_ASSIGNMENT_EXPRESSION));
   } else {
     return json;
   }
-  (void)self;
 }
