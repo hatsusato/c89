f8c3a6925c4a60357f62729a2e48f31b79e51fe2
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-08 10:39:23 +0900

    Add generate_store

--- src/generate/instruction.c
+++ src/generate/instruction.c
@@ -10,8 +10,12 @@ static void generate_print(struct printer *printer, struct json *obj,
   printer_print(printer, "%s", json_get_str(str));
 }
 static void generate_reg(struct printer *printer, struct json *json) {
-  struct json *reg = json_get(json, "reg");
-  printer_print(printer, "%%%d", json_int_get(json_as_int(reg)));
+  if (json_has(json, "reg")) {
+    struct json *reg = json_get(json, "reg");
+    printer_print(printer, "%%%d", json_int_get(json_as_int(reg)));
+  } else {
+    printer_print(printer, "%s", json_get_str(json));
+  }
 }
 static void generate_ret(struct printer *printer, struct json *json) {
   printer_print(printer, "ret i32 ");
@@ -21,12 +25,21 @@ static void generate_alloca(struct printer *printer, struct json *json) {
   generate_reg(printer, json);
   printer_print(printer, " = alloca i32, align 4");
 }
+static void generate_store(struct printer *printer, struct json *json) {
+  printer_print(printer, "store i32 ");
+  generate_reg(printer, json_get(json, "value"));
+  printer_print(printer, ", i32* ");
+  generate_reg(printer, json_get(json, "pointer"));
+  printer_print(printer, ", align 4");
+}
 void generate_instruction(struct printer *printer, struct json *json) {
   const char *tag = json_get_str(json_get(json, "instr"));
   if (util_streq(tag, "ret")) {
     generate_ret(printer, json);
   } else if (util_streq(tag, "alloca")) {
     generate_alloca(printer, json);
+  } else if (util_streq(tag, "store")) {
+    generate_store(printer, json);
   } else {
     json_print(json);
     return;
