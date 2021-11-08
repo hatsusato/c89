4c5fa74c34925b87da0a95a2a9861fc964058ddb
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-08 10:05:31 +0900

    Add struct generate_numbering

--- src/generate/generate.c
+++ src/generate/generate.c
@@ -5,10 +5,29 @@
 #include "json/map.h"
 #include "printer/printer.h"
 
+struct generate_numbering {
+  index_t index;
+};
+
 static void generate_print(struct printer *printer, struct json *obj,
                            const char *key) {
   struct json *str = json_get(obj, key);
   printer_print(printer, "%s", json_get_str(str));
+}
+static void generate_numbering_instr(struct json_map *map) {
+  struct generate_numbering *numbering = json_map_extra(map);
+  struct json *instr = json_map_val(map);
+  struct json *reg = json_new_int(numbering->index++);
+  json_insert(instr, "reg", reg);
+  json_del(reg);
+}
+static void generate_numbering_block(struct json_map *map) {
+  struct generate_numbering *numbering = json_map_extra(map);
+  struct json *block = json_map_val(map);
+  struct json *label = json_new_int(numbering->index++);
+  json_insert(block, "label", label);
+  json_del(label);
+  json_foreach(json_get(block, "block"), generate_numbering_instr, numbering);
 }
 static void generate_function_map(struct json_map *map) {
   struct printer *printer = json_map_extra(map);
@@ -18,13 +37,17 @@ static void generate_function_map(struct json_map *map) {
 static void generate_function(struct json_map *map) {
   struct printer *printer = json_map_extra(map);
   struct json *func = json_map_val(map);
+  struct json *blocks = json_get(func, "function");
+  struct generate_numbering numbering;
+  numbering.index = 0;
+  json_foreach(blocks, generate_numbering_block, &numbering);
   printer_newline(printer);
   printer_print(printer, "define i32 @");
   generate_print(printer, func, "name");
   printer_print(printer, "() ");
   printer_open(printer, "{");
   printer_newline(printer);
-  json_foreach(json_get(func, "function"), generate_function_map, printer);
+  json_foreach(blocks, generate_function_map, printer);
   printer_close(printer, "}");
   printer_newline(printer);
 }
