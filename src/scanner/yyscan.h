4e5d6af380863c5eb7dff683c921a980434b4793
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-08 19:33:12 +0900

    Remove YYSCAN_TAG

--- src/scanner/yyscan.h
+++ src/scanner/yyscan.h
@@ -15,7 +15,6 @@
 #define YYSCAN_PUSH(arr, val) yyscan_json_push(arr, val)
 #define YYSCAN_INSERT(obj, key, val) yyscan_json_insert(obj, key, val)
 #define YYSCAN_BINOP(obj, lhs, op, rhs) yyscan_json_binop(obj, lhs, op, rhs)
-#define YYSCAN_TAG(key, val) YYSCAN_INSERT(YYSCAN_OBJ(), key, val)
 #define YYSCAN_REGISTER(decl) \
   scanner_register_typedef(yyget_extra(YYSCAN_PARAM), decl)
 #define YYSCAN_SET_TOP(top) scanner_set_top(yyget_extra(YYSCAN_PARAM), top)
