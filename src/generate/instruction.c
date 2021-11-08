7590be54e42cb324bedda70f8a7a342c3ccde226
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-08 10:08:09 +0900

    Add generate_reg

--- src/generate/instruction.c
+++ src/generate/instruction.c
@@ -9,12 +9,16 @@ static void generate_print(struct printer *printer, struct json *obj,
   struct json *str = json_get(obj, key);
   printer_print(printer, "%s", json_get_str(str));
 }
+static void generate_reg(struct printer *printer, struct json *json) {
+  struct json *reg = json_get(json, "reg");
+  printer_print(printer, "%%%d", json_int_get(json_as_int(reg)));
+}
 static void generate_ret(struct printer *printer, struct json *json) {
   printer_print(printer, "ret i32 ");
   generate_print(printer, json, "value");
 }
 static void generate_alloca(struct printer *printer, struct json *json) {
-  generate_print(printer, json, "reg");
+  generate_reg(printer, json);
   printer_print(printer, " = alloca i32, align 4");
 }
 void generate_instruction(struct printer *printer, struct json *json) {
