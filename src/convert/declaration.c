d00d8ef19c5bd86d5bd253006e17e19a17e345cd
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-09 11:10:20 +0900

    Add convert_initializer

--- src/convert/declaration.c
+++ src/convert/declaration.c
@@ -1,18 +1,37 @@
 #include "declaration.h"
 
+#include "expression.h"
 #include "json/json.h"
 #include "json/map.h"
 #include "type.h"
 #include "util/symbol.h"
 
+static struct json *convert_initializer(struct convert *, struct json *);
+
 static void convert_init_declarator(struct convert *self, struct json *json) {
   struct json *identifier = json_find_identifier(json);
   convert_push_symbol(self, identifier);
+  if (json_has(json, SYMBOL_INITIALIZER)) {
+    struct json *value =
+        convert_initializer(self, json_get(json, SYMBOL_INITIALIZER));
+    struct json *pointer = convert_lookup_symbol(self, identifier);
+    struct json *instr = convert_push_instr(self, "store");
+    json_insert(instr, "value", value);
+    json_insert(instr, "pointer", pointer);
+  }
 }
 static void convert_init_declarator_list(struct json_map *map) {
   struct convert *self = json_map_extra(map);
   struct json *json = json_map_val(map);
   convert_init_declarator(self, json);
+}
+static struct json *convert_initializer(struct convert *self,
+                                        struct json *json) {
+  if (json_has(json, SYMBOL_ASSIGNMENT_EXPRESSION)) {
+    return convert_rvalue(self, json_get(json, SYMBOL_ASSIGNMENT_EXPRESSION));
+  } else {
+    return json;
+  }
 }
 static void convert_declaration_list(struct json_map *map) {
   struct convert *self = json_map_extra(map);
