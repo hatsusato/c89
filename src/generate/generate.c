0c4e9d2a801928cda6af8726b83391afc16947b7
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-10 01:31:54 +0900

    Add generate_global

--- src/generate/generate.c
+++ src/generate/generate.c
@@ -10,6 +10,14 @@ static void generate_header(struct printer *printer) {
   printer_quote(printer, "x86_64-unknown-linux-gnu");
   printer_newline(printer);
 }
+static void generate_global(struct json_map *map) {
+  struct printer *printer = json_map_extra(map);
+  struct json *global = json_map_val(map);
+  struct json *name = json_get(global, "name");
+  printer_print(printer, "@%s", json_get_str(name));
+  printer_print(printer, " = global i32 0, align 4");
+  printer_newline(printer);
+}
 static void generate_map(struct json_map *map) {
   struct printer *printer = json_map_extra(map);
   struct json *function = json_map_val(map);
@@ -19,7 +27,9 @@ static void generate_map(struct json_map *map) {
 void generate(struct json *json) {
   struct printer *printer = printer_new_stdout();
   struct json *functions = json_get(json, "module");
+  struct json *global = json_get(json, "global");
   generate_header(printer);
+  json_foreach(global, generate_global, printer);
   json_foreach(functions, generate_map, printer);
   printer_del(printer);
 }
