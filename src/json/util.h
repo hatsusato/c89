24cd6b8c017bc025d9378f551b3e7ba603c8d47b
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-10 03:16:35 +0900

    Add json_merge

--- src/json/util.h
+++ src/json/util.h
@@ -17,6 +17,7 @@ const char *json_get_str(struct json *);
 struct json *json_find_identifier(struct json *);
 struct json *json_front(struct json *);
 void json_append(struct json *, struct json *);
+void json_merge(struct json *, struct json *);
 struct json *json_take(struct json *, const char *);
 
 #endif /* INCLUDE_GUARD_0363FDA6_C88C_4B43_9B75_218C62E9A789 */
