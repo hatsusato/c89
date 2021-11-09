d4479c9a9d04affd0dc85e5dbb5c6d564654ced3
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-10 02:53:18 +0900

    Add generate_global_name

--- src/generate/global.c
+++ src/generate/global.c
@@ -7,13 +7,19 @@ struct json *generate_global_get(struct json *module) {
   return json_get(module, "global");
 }
 void generate_global(struct printer *printer, struct json *json) {
-  const char *name = json_get_str(json_get(json, "name"));
   struct json *init = json_get(json, "init");
   int val = 0;
   if (json_has(init, "immediate")) {
     struct json *immediate = json_get(init, "immediate");
     val = json_int_get(json_as_int(immediate));
   }
-  printer_print(printer, "@%s = global i32 %d, align 4", name, val);
+  generate_global_name(printer, json);
+  printer_print(printer, " = global i32 %d, align 4", val);
   printer_newline(printer);
+}
+void generate_global_name(struct printer *printer, struct json *json) {
+  struct json *name = json_get(json, "name");
+  if (json_is_str(name)) {
+    printer_print(printer, "@%s", json_get_str(name));
+  }
 }
