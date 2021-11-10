92e3d82e83a608572154a42bbaff3bec94ada95b
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-10 22:35:42 +0900

    Update ir_module_init_function

--- src/convert/function.c
+++ src/convert/function.c
@@ -1,10 +1,7 @@
 #include "function.h"
 
-#include "alloc.h"
-#include "ir/function.h"
 #include "ir/module.h"
 #include "json/json.h"
-#include "module.h"
 #include "statement.h"
 
 static struct json *convert_function_get(struct json *module) {
@@ -18,7 +15,6 @@ struct json *convert_function_get_blocks(struct json *module) {
 void convert_function_definition(struct json *module, struct json *json) {
   struct json *name = json_find_identifier(json);
   ir_module_init_function(module, name);
-  convert_push_block(module);
   convert_statement(module, json);
   ir_module_finish_function(module);
 }
