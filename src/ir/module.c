882b5d2ce7be2388b06f0cd9cd9e4b752d3412bb
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-10 15:22:42 +0900

    Add ir_module_{get,set}_table

--- src/ir/module.c
+++ src/ir/module.c
@@ -5,6 +5,14 @@
 #include "table.h"
 #include "value.h"
 
+static struct json *ir_module_get_table(struct json *module) {
+  return json_get(module, "table");
+}
+static void ir_module_set_table(struct json *module, struct json *table) {
+  json_insert(module, "table", table);
+  json_del(table);
+}
+
 struct json *ir_module_new(void) {
   struct json *module = json_new_obj();
   struct json *array = json_new_arr();
@@ -20,16 +28,14 @@ void ir_module_finish(struct json *module) {
   ir_global_finish(module);
 }
 void ir_module_push_scope(struct json *module) {
-  struct json *table = json_get(module, "table");
-  table = ir_table_push(table);
-  json_insert(module, "table", table);
-  json_del(table);
+  struct json *table = ir_module_get_table(module);
+  struct json *updated = ir_table_push(table);
+  ir_module_set_table(module, updated);
 }
 void ir_module_pop_scope(struct json *module) {
-  struct json *table = json_get(module, "table");
-  table = ir_table_pop(table);
-  json_insert(module, "table", table);
-  json_del(table);
+  struct json *table = ir_module_get_table(module);
+  struct json *updated = ir_table_pop(table);
+  ir_module_set_table(module, updated);
 }
 void ir_module_push_global(struct json *module, struct json *value) {
   if (ir_value_is_global(value)) {
