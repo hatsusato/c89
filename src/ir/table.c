b3a22d45484a2f233a198ffec4a6788bd2654109
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-10 14:03:12 +0900

    Remove ir_table_{take,set}

--- src/ir/table.c
+++ src/ir/table.c
@@ -2,13 +2,6 @@
 
 #include "json/json.h"
 
-static struct json *ir_table_take(struct json *module) {
-  return json_take(module, "table");
-}
-static void ir_table_set(struct json *module, struct json *table) {
-  json_insert(module, "table", table);
-}
-
 void ir_table_init(struct json *module) {
   struct json *table = json_new_obj();
   json_insert(module, "table", table);
@@ -16,15 +9,14 @@ void ir_table_init(struct json *module) {
 }
 void ir_table_push(struct json *module) {
   struct json *table = json_new_obj();
-  struct json *next = ir_table_take(module);
+  struct json *next = json_get(module, "table");
   json_insert(table, "$next", next);
-  ir_table_set(module, table);
+  json_insert(module, "table", table);
   json_del(table);
-  json_del(next);
 }
 void ir_table_pop(struct json *module) {
-  struct json *table = ir_table_take(module);
+  struct json *table = json_take(module, "table");
   struct json *next = json_get(table, "$next");
-  ir_table_set(module, next);
+  json_insert(module, "table", next);
   json_del(table);
 }
