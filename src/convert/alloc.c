372dca2c73961e9a76e04911b22ca3d58040bb61
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-10 21:15:50 +0900

    Update structure of alloc

--- src/convert/alloc.c
+++ src/convert/alloc.c
@@ -4,17 +4,20 @@
 #include "module.h"
 
 void convert_alloc_init(struct json *module) {
+  struct json *function = json_get(module, "function");
   struct json *alloc = json_new_arr();
-  json_insert(module, "alloc", alloc);
+  json_insert(function, "alloc", alloc);
   json_del(alloc);
 }
 struct json *convert_alloc_finish(struct json *module) {
-  struct json *alloc = json_take(module, "alloc");
-  json_insert(module, "alloc", json_null());
+  struct json *function = json_get(module, "function");
+  struct json *alloc = json_take(function, "alloc");
+  json_insert(function, "alloc", json_null());
   return alloc;
 }
 struct json *convert_alloc_push(struct json *module) {
-  struct json *alloc = json_get(module, "alloc");
+  struct json *function = json_get(module, "function");
+  struct json *alloc = json_get(function, "alloc");
   struct json *instr = convert_new_instr("alloca");
   json_push(alloc, instr);
   return instr;
