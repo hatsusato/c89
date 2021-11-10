dfd1ed09b05a7d4eed7658c294f4c9793c84e495
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-10 11:21:26 +0900

    Add ir_module_{init,finish}

--- src/ir/module.c
+++ src/ir/module.c
@@ -11,6 +11,12 @@ struct json *ir_module_new(void) {
   json_del(array);
   return module;
 }
+void ir_module_init(struct json *module) {
+  ir_global_init(module);
+}
+void ir_module_finish(struct json *module) {
+  ir_global_finish(module);
+}
 void ir_module_push_global(struct json *module, struct json *value) {
   if (ir_value_is_global(value)) {
     struct json *global = ir_global_get(module);
