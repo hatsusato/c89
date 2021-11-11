09dff39d1e4108d380592a258f767dc0619ea31c
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-11 16:47:27 +0900

    Modify test

--- test/02.c
+++ test/02.c
@@ -1,39 +1,49 @@
 int f1(void) {
-  int x;
-  x = 1;
-  if (x) {
-    x = x + 2;
-  }
-  x = x + 3;
-  return x;
-}
-int f2(void) {
-  int x;
-  x = 1;
+  int x = 1;
   if (x) {
     x = x + 2;
     if (x) {
       x = x + 3;
     }
+    x = x + 4;
   }
-  x = x + 4;
+  x = x + 5;
+  return x;
+}
+int f2(void) {
+  int x = 0;
+  if (x) {
+    x = x + 1;
+  } else {
+    x = x + 2;
+  }
+  x = x + 3;
   return x;
 }
 int f3(void) {
-  int x;
-  x = 1;
+  int x = 0;
   if (x) {
+    x = x + 1;
+  } else if (x) {
     x = x + 2;
   } else {
     x = x + 3;
   }
+  x = x + 4;
   return x;
 }
 int f4(void) {
-  int x = 1;
-  int y = x + 2;
-  int z = y + 3;
-  return z;
+  int x = 0;
+  if (x) {
+    x = x + 1;
+    if (x) {
+      x = x + 2;
+      return x + 3;
+    }
+    x = x + 4;
+  }
+  x = x + 5;
+  return x + 6;
 }
 int f5(void) {
   int x = 1;
