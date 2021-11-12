d68e3d8ec00021689f9ce9e96e42fc16554ca0b1
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-13 00:48:02 +0900

    Update json_is_{null,int,str,arr,obj}

--- src/json/new.c
+++ src/json/new.c
@@ -77,19 +77,19 @@ struct json *json_new_weak(struct json *json) {
   return json_alloc(JSON_TAG_WEAK, json_unwrap(json));
 }
 bool_t json_is_null(struct json *self) {
-  return json_tag(self) == JSON_TAG_NULL;
+  return json_tag(json_unwrap(self)) == JSON_TAG_NULL;
 }
 bool_t json_is_int(struct json *self) {
-  return json_tag(self) == JSON_TAG_INT;
+  return json_tag(json_unwrap(self)) == JSON_TAG_INT;
 }
 bool_t json_is_str(struct json *self) {
-  return json_tag(self) == JSON_TAG_STR;
+  return json_tag(json_unwrap(self)) == JSON_TAG_STR;
 }
 bool_t json_is_arr(struct json *self) {
-  return json_tag(self) == JSON_TAG_ARR;
+  return json_tag(json_unwrap(self)) == JSON_TAG_ARR;
 }
 bool_t json_is_obj(struct json *self) {
-  return json_tag(self) == JSON_TAG_OBJ;
+  return json_tag(json_unwrap(self)) == JSON_TAG_OBJ;
 }
 bool_t json_is_weak(struct json *self) {
   return json_tag(self) == JSON_TAG_WEAK;
