686c59691e35e1344eafa0001a0fd72729d92eef
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-13 00:08:53 +0900

    Add ir_block_{new,get}_terminator

--- src/ir/block.c
+++ src/ir/block.c
@@ -1,16 +1,26 @@
 #include "block.h"
 
+#include "instr.h"
 #include "json/json.h"
 #include "json/map.h"
 
 struct json *ir_block_new(void) {
   struct json *block = json_new_obj();
   json_set(block, "instructions", json_new_arr());
+  json_insert(block, "terminator", json_null());
   return block;
 }
 void ir_block_push_instr(struct json *block, struct json *instr) {
   struct json *array = json_get(block, "instructions");
   json_push(array, instr);
+}
+struct json *ir_block_new_terminator(struct json *block, const char *tag) {
+  struct json *terminator = ir_instr_new(tag);
+  json_insert(block, "terminator", terminator);
+  return terminator;
+}
+struct json *ir_block_get_terminator(struct json *block) {
+  return json_get(block, "terminator");
 }
 void ir_block_prepend(struct json *self, struct json *other) {
   struct json *array = json_new_arr();
