8967e4013ac1c857d780a4441085baf0fb642b5c
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-15 23:20:27 +0900

    Add convert_immediate_additive_expression

--- src/convert/immediate.c
+++ src/convert/immediate.c
@@ -5,17 +5,41 @@
 #include "util/symbol.h"
 #include "util/util.h"
 
+static void convert_immediate_insert(struct json *json, int val) {
+  json_set(json, "immediate", json_new_int(val));
+}
+static void convert_immediate_expression(struct json *);
+
 static void convert_immediate_primary_expression(struct json *json) {
   if (json_has(json, SYMBOL_INTEGER_CONSTANT)) {
     struct json *val = json_get(json, SYMBOL_INTEGER_CONSTANT);
     int num = util_atoi(json_get_str(val));
-    json_set(json, "immediate", json_new_int(num));
+    convert_immediate_insert(json, num);
+  }
+}
+static void convert_immediate_additive_expression(struct json *json) {
+  struct json *lhs = json_get(json, SYMBOL_ADDITIVE_EXPRESSION);
+  struct json *rhs = json_get(json, SYMBOL_MULTIPLICATIVE_EXPRESSION);
+  convert_immediate_expression(lhs);
+  convert_immediate_expression(rhs);
+  lhs = json_get(lhs, "immediate");
+  rhs = json_get(rhs, "immediate");
+  if (json_is_int(lhs) && json_is_int(rhs)) {
+    int lval = json_int_get(json_as_int(lhs));
+    int rval = json_int_get(json_as_int(rhs));
+    if (json_has(json, SYMBOL_PLUS)) {
+      convert_immediate_insert(json, lval + rval);
+    } else if (json_has(json, SYMBOL_MINUS)) {
+      convert_immediate_insert(json, lval - rval);
+    }
   }
 }
 static void convert_immediate_expression(struct json *json) {
   const char *tag = json_get_str(json_get(json, "tag"));
   if (util_streq(tag, SYMBOL_PRIMARY_EXPRESSION)) {
     convert_immediate_primary_expression(json);
+  } else if (util_streq(tag, SYMBOL_ADDITIVE_EXPRESSION)) {
+    convert_immediate_additive_expression(json);
   }
 }
 static void convert_immediate_visitor(struct json_visitor *visitor,
