03743adbe23d1e11a1c42b6d49910fe7edf989e4
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-16 20:53:37 +0900

    Modify test

--- test/03.c
+++ test/03.c
@@ -1,96 +1,136 @@
 int f1(void) {
-  int x = 1;
+  int x = 0;
   switch (x) {
   default:
+    x = 1;
     break;
   }
-  return x;
+  return 0;
 }
 int f2(void) {
-  int x = 1;
+  int x = 0;
   switch (x) {
-  default:
-    return x;
+  case 1:
+    x = 1;
+    break;
   }
+  return 0;
 }
 int f3(void) {
-  int x = 1;
+  int x = 0;
   switch (x) {
-  case 0:
-    return x;
+  case 1:
+    x = 1;
+    break;
+  case 2:
+    x = 2;
+    break;
+  case 3:
+    x = 3;
+    break;
   }
   return 0;
 }
 int f4(void) {
-  int x = 1;
+  int x = 0;
   switch (x) {
-  case 0:
+  case 1:
+    x = 1;
+    break;
+  case 2:
+    x = 2;
+    break;
+  default:
+    x = 5;
+    break;
+  case 3:
+    x = 3;
+    break;
+  case 4:
+    x = 4;
     break;
   }
   return 0;
 }
 int f5(void) {
-  int x = 1;
+  int x = 0;
   switch (x) {
-  case 0:
-    break;
   case 1:
+    x = 1;
+  case 2:
+  default:
+  case 3:
+    x = 3;
     break;
+  case 4:
+    x = 4;
   }
   return 0;
 }
 int f6(void) {
-  int x = 1;
+  int x = 0;
   switch (x) {
-  case 0:
-  case 1:
-    x = x + 1;
-    x = x + 1;
-  case 2:
-  case 3:
-    x = x + 1;
-    break;
+  default:
+    x = 1;
+    return x;
   }
-  return 0;
 }
 int f7(void) {
-  int x = 1;
+  int x = 0;
   switch (x) {
-  case 0:
   case 1:
-    x = x + 1;
-    x = x + 1;
-  case 2:
-  default:
-  case 3:
-    x = x + 1;
-    break;
+    x = 1;
+    return x;
   }
   return 0;
 }
 int f8(void) {
-  int x = 1;
+  int x = 0;
   switch (x) {
-  case 0:
-  default:
   case 1:
+    x = 1;
+    break;
+  case 2:
+    x = 2;
+    return x;
+  case 3:
+    x = 3;
+    break;
+  }
+  return 0;
+}
+int f9(void) {
+  int x = 0;
+  switch (x) {
+  case 1:
+    x = 1;
+    return x;
+  case 2:
+    x = 2;
+    return x;
+  default:
+    x = 5;
+    return x;
+  case 3:
+    x = 3;
+    return x;
+  case 4:
+    x = 4;
     return x;
   }
 }
-int f9(void) {
-  int x = 1;
+int f10(void) {
+  int x = 0;
   switch (x) {
-  case 0:
   case 1:
-    x = x + 1;
-    x = x + 1;
-    return x;
+    x = 1;
   case 2:
-    break;
   default:
   case 3:
-    x = x + 1;
-    break;
+    x = 3;
+    return x;
+  case 4:
+    x = 4;
   }
   return 0;
 }
