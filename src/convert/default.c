15b5b4f58d75f8db960dc5d0cfe46f4db5083397
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-19 23:07:10 +0900

    Add convert_default_labeled_statement

--- src/convert/default.c
+++ src/convert/default.c
@@ -1,8 +1,12 @@
 #include "default.h"
 
+#include "json/json.h"
 #include "json/visitor.h"
-#include "util/type.h"
+#include "util/symbol.h"
 
+bool_t convert_default_labeled_statement(struct json *json) {
+  return json_has(json, SYMBOL_DEFAULT);
+}
 static void convert_default_visitor(struct json_visitor *visitor,
                                     struct json *json) {
   json_visit_foreach(visitor, json);
