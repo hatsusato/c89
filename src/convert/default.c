39df3b3db48542ee1b1b3e49f7e1e7259c071416
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-11-19 22:46:17 +0900

    convert_default_visitor

--- src/convert/default.c
+++ src/convert/default.c
@@ -1,5 +1,13 @@
 #include "default.h"
 
+#include "json/visitor.h"
+#include "util/type.h"
+
+static void convert_default_visitor(struct json_visitor *visitor,
+                                    struct json *json) {
+  json_visit_foreach(visitor, json);
+}
+
 void convert_default(struct json *json) {
-  (void)json;
+  json_visit(convert_default_visitor, NULL, json);
 }
